7f8aa558a9ae2eba3bd9e69cd52c4a3bd21bb4a7
Alexandr Tarelkin <tarelkinaa@bk.ru>
2021-04-14 23:24:03 +0300

    network-based config

---
+++ utils/config_goerli.py
@@ -0,0 +1,6 @@
+ldo_token_address = '0x56340274fB5a72af1A3C6609061c451De7961Bd4'
+lido_dao_agent_address = '0x4333218072D5d7008546737786663c38B4D561A4'
+lido_dao_finance_address = '0x75c7b1D23f1cad7Fb4D60281d7069E46440BC179'
+lido_dao_voting_address = '0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db'
+lido_dao_token_manager_address = '0xDfe76d11b365f5e0023343A367f0b311701B3bc1'
+lido_dao_node_operators_registry = '0x9D4AF1Ee19Dad8857db3a45B0374c81c8A1C6320'
---
+++ utils/config_mainnet.py
@@ -0,0 +1,7 @@
+ldo_token_address = '0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32'
+lido_dao_acl_address = '0x9895F0F17cc1d1891b6f18ee0b483B6f221b37Bb'
+lido_dao_agent_address = '0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c'
+lido_dao_finance_address = '0xB9E5CBB9CA5b0d659238807E84D0176930753d86'
+lido_dao_voting_address = '0x2e59A20f205bB85a89C53f1936454680651E618e'
+lido_dao_token_manager_address = '0xf73a1260d222f447210581DDf212D915c09a3249'
+lido_dao_node_operators_registry = '0x55032650b14df07b85bF18A3a3eC8E0Af2e028d5'
