53373309c8df41e67ac5fdaa9a111ad6b8ec9c2d
Eugene Pshenichnyy <pshenichniy.eugene@gmail.com>
2021-07-13 17:27:45 +0300

    Update script which start a vote for withdrawal credentials change

--- scripts/set_withdrawal_credentials.py
+++ scripts/set_withdrawal_credentials.py
@@ -10,6 +10,8 @@ def set_console_globals(**kwargs):
     interface = kwargs['interface']
 
 
+import time
+from brownie.utils import color
 from utils.voting import create_vote
 from utils.evm_script import encode_call_script
 from utils.node_operators import encode_set_node_operator_staking_limit
@@ -21,17 +23,28 @@ from utils.config import (
     lido_dao_token_manager_address,
     lido_dao_node_operators_registry,
     lido_dao_steth_address,
-    get_deployer_account
+    get_deployer_account,
+    prompt_bool
 )
 
 
 from utils.withdrawal_credentials import (
     get_eth1_withdrawal_credentials,
-    encode_set_withdrawal_credentials
+    encode_set_withdrawal_credentials,
+    colorize_withdrawal_credentials
 )
 
 
+def pp(text, value):
+    print(text, color.highlight(value), end='')
+
+
+def fetch_node_operators(registry):
+    return [{**registry.getNodeOperator(i, True), **{'index': i}} for i in range(registry.getNodeOperatorsCount())]
+
+
 def set_withdrawal_credentials_vote(tx_params):
+    print('You are about ot launch a vote for withdrawal credentials change...')
     voting = interface.Voting(lido_dao_voting_address)
     token_manager = interface.TokenManager(lido_dao_token_manager_address)
     registry = interface.NodeOperatorsRegistry(lido_dao_node_operators_registry)
@@ -40,35 +53,73 @@ def set_withdrawal_credentials_vote(tx_params):
     lido_dao_withdrawal_contract_address = '0xb9d7934878b5fb9610b3fe8a5e441e8fad7e293f'
     withdrawal_contract = interface.WithdrawalContractProxy(lido_dao_withdrawal_contract_address)
 
+    node_operators = fetch_node_operators(registry)
+    node_operators = list(filter(lambda op: op['stakingLimit'] > op['usedSigningKeys'], node_operators))
+
     if (withdrawal_contract.proxy_getAdmin() != voting.address):
         raise Exception('withdrawal_contract is not in a valid state')
 
     new_withdrawal_credentials = get_eth1_withdrawal_credentials(withdrawal_contract.address)
 
-    evm_script = encode_call_script([
+    print()
+    pp('Using Lido/stETH contract at address', lido_dao_steth_address)
+    pp('Using NodeOperatorsRegistry at address', lido_dao_node_operators_registry)
+    pp('Using voting contract at address', voting.address)
+    print()
+    pp('Using withdrawal contract at address', lido_dao_withdrawal_contract_address)
+    pp('Withdrawal contract admin', withdrawal_contract.proxy_getAdmin())
+
+    print()
+    print('Node Operators whose staking limits should be reduced:')
+    for operator in node_operators:
+        index = operator['index']
+        name = operator['name']
+        used = operator['usedSigningKeys']
+        pp('{:<30}'.format(f'#{index} {name}'), str(used))
+
+    print()
+    print('New withdrawal credentials', colorize_withdrawal_credentials(new_withdrawal_credentials))
+
+    call_script = []
+
+    call_script.append(
         encode_set_withdrawal_credentials(
             withdrawal_credentials=new_withdrawal_credentials,
             lido=lido
-        ),
-        encode_set_node_operator_staking_limit(id=0, limit=2500, registry=registry),
-        encode_set_node_operator_staking_limit(id=1, limit=1000, registry=registry),
-        encode_set_node_operator_staking_limit(id=2, limit=3386, registry=registry),
-        encode_set_node_operator_staking_limit(id=3, limit=3073, registry=registry),
-        encode_set_node_operator_staking_limit(id=4, limit=3000, registry=registry),
-        encode_set_node_operator_staking_limit(id=5, limit=3073, registry=registry),
-        encode_set_node_operator_staking_limit(id=6, limit=100, registry=registry),
-        encode_set_node_operator_staking_limit(id=7, limit=1000, registry=registry),
-        encode_set_node_operator_staking_limit(id=9, limit=1500, registry=registry),
-    ])
+        )
+    )
+
+    for operator in node_operators:
+        index = operator['index']
+        used = operator['usedSigningKeys']
+
+        call_script.append(
+            encode_set_node_operator_staking_limit(
+                id=index,
+                limit=used,
+                registry=registry
+            )
+        )
+
+    print()
+    print('Callscript:')
+
+    for addr, action in call_script:
+        pp(addr, action)
+
+    print()
+
+    print('Does it look good?')
+    prompt_bool()
 
     return create_vote(
         voting=voting,
         token_manager=token_manager,
         vote_desc=(
             f'1) set withdrawal_credentials to {new_withdrawal_credentials}, '
-            f'2) set staking limits for all node operators to the current values'
+            f'2) reduce staking limits for node operators whose has approved keys but not used yet'
         ),
-        evm_script=evm_script,
+        evm_script=encode_call_script(call_script),
         tx_params=tx_params
     )
 
--- utils/withdrawal_credentials.py
+++ utils/withdrawal_credentials.py
@@ -9,7 +9,8 @@ def strip_byte_prefix(hexstr):
 
 
 def get_eth1_withdrawal_credentials(withdrawal_contract_address):
-    return '0x' + ETH1_ADDRESS_WITHDRAWAL_PREFIX + '00' * 11 + strip_byte_prefix(withdrawal_contract_address)
+    wc = '0x' + ETH1_ADDRESS_WITHDRAWAL_PREFIX + '00' * 11 + strip_byte_prefix(withdrawal_contract_address)
+    return wc.lower()
 
 
 def encode_set_withdrawal_credentials(withdrawal_credentials, lido):
@@ -17,3 +18,18 @@ def encode_set_withdrawal_credentials(withdrawal_credentials, lido):
         lido.address,
         lido.setWithdrawalCredentials.encode_input(withdrawal_credentials)
     )
+
+
+def colorize_withdrawal_credentials(withdrawal_credentials):
+    byte_prefix = withdrawal_credentials[:2]
+    prefix = withdrawal_credentials[2:4]
+    zero_pad = withdrawal_credentials[4:26]
+    address = withdrawal_credentials[26:]
+
+    hl_color = '\x1b[38;5;141m'
+    yellow_color = '\x1b[0;33m'
+    gray_color = '\x1b[0;m'
+    end_of_color = '\033[0m'
+
+    return f'{hl_color}{byte_prefix}{yellow_color}{prefix}{gray_color}{zero_pad}{hl_color}{address}{end_of_color}'
+
