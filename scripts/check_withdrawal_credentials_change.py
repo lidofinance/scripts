b046bd98a45c0d2296afff64fa763d40b5cd28a8
Eugene Pshenichnyy <pshenichniy.eugene@gmail.com>
2021-07-14 09:51:31 +0300

    Fix check script

--- scripts/check_withdrawal_credentials_change.py
+++ scripts/check_withdrawal_credentials_change.py
@@ -80,7 +80,7 @@ def run_checks(registry, lido, prev_withdrawal_credentials):
     print()
 
     withdrawal_credentials = lido.getWithdrawalCredentials()
-    pp('Current withdrawal credentials', prev_withdrawal_credentials)
+    pp('Current withdrawal credentials', withdrawal_credentials)
 
     assert withdrawal_credentials != prev_withdrawal_credentials
     ok('Withdrawal crenedtials has been changed')
