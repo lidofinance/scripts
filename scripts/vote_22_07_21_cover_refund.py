98040823414d56b88916dbfa5469610d0ac65cff
Victor Suzdalev <victor.suzdalev@gmail.com>
2021-07-22 19:11:35 +0300

    feat: cover refund omnibus

---
+++ scripts/vote_22_07_21_cover_refund.py
@@ -0,0 +1,87 @@
+try:
+    from brownie import interface
+except ImportError:
+    print("You're probably running inside Brownie console. Please call:")
+    print("set_console_globals(interface=interface)")
+
+
+def set_console_globals(**kwargs):
+    global interface
+    interface = kwargs['interface']
+
+
+import time
+from brownie.utils import color
+from utils.voting import create_vote
+from utils.evm_script import encode_call_script
+from utils.finance import encode_eth_transfer
+
+from utils.config import (
+    lido_dao_voting_address,
+    lido_dao_finance_address,
+    lido_dao_token_manager_address,
+    get_deployer_account,
+    prompt_bool
+)
+
+
+def pp(text, value):
+    print(text, color.highlight(str(value)), end='')
+
+
+def make_refund_call_script(refund_address, refund_in_wei, finance):
+    call_script = [
+        encode_eth_transfer(
+            recipient=refund_address,
+            amount=refund_in_wei,
+            reference=f'Cover refund',
+            finance=finance
+        )
+    ]
+    return call_script
+
+
+def start_vote(tx_params, silent=False):
+    refund_address = '0xD089cc83f5B803993E266ACEB929e52A993Ca2C8'
+    refund_in_wei = 79837990169609360000
+
+    finance = interface.Finance(lido_dao_finance_address)
+
+    if not silent:
+      print()
+      pp('Using finance contract at address', lido_dao_finance_address)
+      pp('Using voting contract at address', lido_dao_voting_address)
+      print()
+
+    if not silent:
+      print('Cover refund (ETH):')
+      pp('{:<30}'.format(refund_address), refund_in_wei / 10 ** 18)
+      print()
+
+    call_script = make_refund_call_script(refund_address, refund_in_wei, finance)
+
+    if not silent:
+      print('Callscript:')
+      for addr, action in call_script:
+          pp(addr, action)
+      print()
+
+    if not silent:
+      print('Does it look good?')
+      prompt_bool()
+
+    return create_vote(
+        voting=interface.Voting(lido_dao_voting_address),
+        token_manager=interface.TokenManager(lido_dao_token_manager_address),
+        vote_desc=(
+            f'Omnibus vote: 1) refund dev team for the cover purchase'
+        ),
+        evm_script=encode_call_script(call_script),
+        tx_params=tx_params
+    )
+
+
+def main():
+    (vote_id, _) = start_vote({'from': get_deployer_account()})
+    print(f'Vote created: {vote_id}')
+    time.sleep(5) # hack: waiting thread 2
---
+++ tests/test_22_07_21_cover_refund.py
@@ -0,0 +1,16 @@
+from scripts.vote_22_07_21_cover_refund import (start_vote)
+
+def test_set_operators_limit(ldo_holder, helpers, accounts, dao_voting):
+
+    refund_address = '0xD089cc83f5B803993E266ACEB929e52A993Ca2C8'
+    refund_acc = accounts.at(refund_address, force=True)
+    refund_balance_before = refund_acc.balance()
+
+    (vote_id, _) = start_vote({"from": ldo_holder}, silent=True)
+    print(f'Vote {vote_id} created')
+    helpers.execute_vote(vote_id=vote_id, accounts=accounts, dao_voting=dao_voting)
+    print(f'Vote {vote_id} executed')
+
+    refund_balance_after = refund_acc.balance()
+
+    assert refund_balance_after - refund_balance_before == 79837990169609360000
