af902df07d5ba1df4cf6185416aa964b7eb8f662
Dmitri Ivakhnenko <dmit.ivh@gmail.com>
2021-09-06 19:34:09 +0300

    Decoding: show trace for voting

--- utils/voting.py
+++ utils/voting.py
@@ -2,7 +2,8 @@ from utils.evm_script import encode_call_script, EMPTY_CALLSCRIPT
 from utils.config import ldo_token_address
 
 
-def create_vote(voting, token_manager, vote_desc, evm_script, tx_params):
+def create_vote(voting, token_manager, vote_desc, evm_script, tx_params,
+                verbose: bool = False):
     new_vote_script = encode_call_script([
         (voting.address,
          voting.newVote.encode_input(
@@ -10,5 +11,12 @@ def create_vote(voting, token_manager, vote_desc, evm_script, tx_params):
              vote_desc, False, False))
     ])
     tx = token_manager.forward(new_vote_script, tx_params)
+    if tx.revert_msg is not None:
+        print(tx.traceback)
+        return -1, tx
     vote_id = tx.events['StartVote']['voteId']
+
+    if verbose:
+        tx.call_trace()
+
     return (vote_id, tx)
