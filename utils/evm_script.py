ad2caff5cd3da9031bd918795a35b6934993dfac
Dmitri Ivakhnenko <dmit.ivh@gmail.com>
2021-09-06 19:03:41 +0300

    Decoding: update evm script decoding

--- utils/evm_script.py
+++ utils/evm_script.py
@@ -1,7 +1,8 @@
 import os
 import logging
 
-from typing import List, Union
+from functools import lru_cache
+from typing import List, Union, Optional
 from collections import defaultdict
 
 import eth_abi
@@ -10,18 +11,67 @@ from eth_typing.evm import HexAddress
 from brownie.utils import color
 
 
+def _get_latest_version(package_name: str) -> str:
+    import json
+    import urllib3
+
+    from distutils import version
+
+    url = f'https://pypi.org/pypi/{package_name}/json'
+
+    versions = list(json.loads(urllib3.PoolManager().request(
+        'GET', url
+    ).data)['releases'].keys())
+    versions.sort(key=version.StrictVersion)
+
+    return versions[-1]
+
+
 def _resolve_parser_dependency() -> None:
     parser_package_name = 'evmscript-parser'
 
-    import importlib.util
-    if importlib.util.find_spec('evmscript_parser') is None:
-        import sys
-        import subprocess
+    import sys
+    import subprocess
+    import pkg_resources
 
-        subprocess.check_call([
-            sys.executable, '-m', 'pip', 'install', '--user',
-            parser_package_name
-        ])
+    from distutils import version
+
+    try:
+        installed_version = version.StrictVersion(
+            pkg_resources.get_distribution(
+                parser_package_name
+            ).version
+        )
+        if os.getenv(
+                'UPGRADE-EVMSCRIPT-PARSER',
+                1
+        ):
+            latest_version = _get_latest_version(parser_package_name)
+            if latest_version > installed_version:
+                try:
+                    subprocess.run([
+                        sys.executable, '-m', 'pip', 'install',
+                        '--upgrade', '--user',
+                        parser_package_name
+                    ], stdout=sys.stdout, check=True, stderr=sys.stderr)
+                except subprocess.CalledProcessError:
+                    subprocess.run([
+                        sys.executable, '-m', 'pip3', 'install',
+                        '--upgrade', '--user',
+                        parser_package_name
+                    ], stdout=sys.stdout, check=True, stderr=sys.stderr)
+
+    except pkg_resources.DistributionNotFound:
+        try:
+            subprocess.run([
+                sys.executable, '-m', 'pip', 'install', '--user',
+                parser_package_name
+            ], stdout=sys.stdout, check=True, stderr=sys.stderr)
+        except subprocess.CalledProcessError:
+            subprocess.run([
+                sys.executable, '-m', 'pip3', 'install', '--user',
+                parser_package_name
+            ], stdout=sys.stdout, check=True, stderr=sys.stderr)
 
 
 _resolve_parser_dependency()
@@ -29,9 +79,9 @@ _resolve_parser_dependency()
 from evmscript_parser.core.parse import parse_script  # noqa
 from evmscript_parser.core.decode import Call  # noqa
 from evmscript_parser.core.decode import decode_function_call  # noqa
-from evmscript_parser.core.decode.ABI import ABIProviderEtherscanApi  # noqa
+from evmscript_parser.core.ABI import get_cached_combined  # noqa
 from evmscript_parser.core.exceptions import (
-    ParseStructureError,
+    ABILocalFileNotExisted, ParseStructureError,
     ABIEtherscanNetworkError, ABIEtherscanStatusCode  # noqa
 )
 
@@ -61,9 +111,24 @@ def encode_call_script(actions, spec_id=1):
     return result
 
 
+@lru_cache
+def get_abi_cache(api_key: str, net: str):
+    return get_cached_combined(
+        api_key, net, os.getenv(
+            'INTERFACES_DIRECTORY',
+            os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                '..',
+                'interfaces'
+            )
+        )
+    )
+
+
 def decode_evm_script(
         script: str, verbose: bool = True,
-        specific_net: str = 'mainnet', repeat_is_error: bool = True
+        specific_net: str = 'mainnet', repeat_is_error: bool = True,
+        interface: Optional[str] = None
 ) -> List[Union[str, Call]]:
     """Decode EVM script to human-readable format."""
     if verbose:
@@ -82,9 +147,7 @@ def decode_evm_script(
             )
         return [repr(err)]
 
-    abi_provider = ABIProviderEtherscanApi(
-        ETHERSCAN_API_KEY, specific_net, retries=3
-    )
+    abi_storage = get_abi_cache(ETHERSCAN_API_KEY, specific_net)
 
     calls = []
     called_contracts = defaultdict(lambda: defaultdict(dict))
@@ -92,12 +155,14 @@ def decode_evm_script(
         try:
             call_info = decode_function_call(
                 call.address, call.method_id,
-                call.encoded_call_data, abi_provider
+                call.encoded_call_data, abi_storage,
+                combined_key=True, interface_name=interface
             )
-        except ABIEtherscanNetworkError as err:
-            call_info = repr(err)
-
-        except ABIEtherscanStatusCode as err:
+        except (
+                ABIEtherscanNetworkError,
+                ABIEtherscanStatusCode,
+                ABILocalFileNotExisted
+        ) as err:
             call_info = repr(err)
 
         contract_calls = called_contracts[call.address][call.method_id]
