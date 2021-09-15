423a0dd10f6bff4076b4a792509ce47079a59f95
Alexandr Tarelkin <tarelkinaa@bk.ru>
2021-09-15 20:35:19 +0300

    tests: update balancer test

--- tests/test_2021_09_16_balancer.py
+++ tests/test_2021_09_16_balancer.py
@@ -13,29 +13,7 @@ def balancer_manager():
     return interface.BalancerReawardsManager(balancer_rewards_manager)
 
 
-def test_common(balancer_manager, ldo_holder, helpers, accounts, dao_voting):
-
-    assert balancer_manager.available_allocations() == 25000 * 10**18
-    print('\n')
-    print(time.ctime(chain.time()), 'allowance: ', balancer_manager.available_allocations())
-    print('                                ', 'rate: ', balancer_manager.rewards_limit_per_period())
-    chain.sleep(1631491200 - chain.time())  # Tuesday, 13 September 2021 
-    chain.mine()
-    print('\n')
-    print(time.ctime(chain.time()), 'allowance: ', balancer_manager.available_allocations())
-    print('                                ', 'rate: ', balancer_manager.rewards_limit_per_period())
-    assert balancer_manager.available_allocations() == 50000 * 10**18
-
-    chain.sleep(1631664000 - chain.time())  # Tuesday, 15 September 2021 
-    chain.mine()
-
-    balancer_allocator = balancer_manager.allocator()
-    balancer_manager.seed_allocations(100, '0x00', 25000 * 10**18, {"from": balancer_allocator})
-    assert balancer_manager.available_allocations() == 25000 * 10**18
-    print('\n')
-    print('Balancer allocates rewards')
-    print(time.ctime(chain.time()), 'allowance: ', balancer_manager.available_allocations())
-    print('                                ', 'rate: ', balancer_manager.rewards_limit_per_period())
+def test_2021_09_16_balancer(balancer_manager, ldo_holder, helpers, accounts, dao_voting):
 
 
     chain.sleep(1631750400 - chain.time())  # Tuesday, 17 September 2021 
