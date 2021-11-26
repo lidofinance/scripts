46321f9ef0618ca2d7bead7ec7b13342e9cd483e
Eugene Mamin <TheDZhon@gmail.com>
2021-11-26 13:52:50 +0300

    !fix: remove raise key limit vote item
    Also use EIP-1559 price model.

--- tests/test_2021_11_25.py
+++ tests/test_2021_11_25.py
@@ -3,22 +3,11 @@ Tests for voting 25/11/2021.
 """
 import pytest
 from collections import namedtuple
-from brownie import network
 
 from scripts.vote_2021_11_25 import start_vote
 
 Payout = namedtuple(
     'Payout', ['address', 'amount']
-)
-
-NodeOperatorIncLimit = namedtuple(
-    'NodeOperatorIncLimit', ['name', 'id', 'limit']
-)
-
-anyblock_an_limits = NodeOperatorIncLimit(
-    name='Anyblock Analytics',
-    id=12,
-    limit=1950
 )
 
 dao_ldo_holder_address = '0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c'
@@ -34,11 +23,9 @@ referral_payout = Payout(
     amount=140246.2696 * (10 ** 18)
 )
 
-def test_2021_11_25(helpers, accounts, lido, ldo_holder, dao_voting, ldo_token, node_operators_registry):
+def test_2021_11_25(helpers, accounts, lido, ldo_holder, dao_voting, ldo_token):
     multisig_balance_before = ldo_token.balanceOf(finance_multisig_address)
     dao_balance_before = ldo_token.balanceOf(dao_ldo_holder_address)
-
-    anyblock_an_limit_before = node_operators_registry.getNodeOperator (anyblock_an_limits.id, True)[3]
 
     vote_id, _ = start_vote({
         'from': ldo_holder
@@ -53,11 +40,3 @@ def test_2021_11_25(helpers, accounts, lido, ldo_holder, dao_voting, ldo_token, 
 
     assert multisig_balance_after - multisig_balance_before == isidoros_payout.amount + referral_payout.amount
     assert dao_balance_before - dao_balance_after == isidoros_payout.amount + referral_payout.amount
-
-    anyblock_an_operator_info = node_operators_registry.getNodeOperator (anyblock_an_limits.id, True)
-    anyblock_an_limit_after = anyblock_an_operator_info[3]
-    anyblock_an_name = anyblock_an_operator_info[1]
-
-    assert anyblock_an_limit_after > anyblock_an_limit_before
-    assert anyblock_an_limit_after == anyblock_an_limits.limit
-    assert anyblock_an_name == anyblock_an_limits.name
