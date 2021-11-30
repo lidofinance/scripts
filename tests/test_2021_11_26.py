12ee1b9c306a1221ea73f0fca6ef3f197470ddb7
Eugene Mamin <TheDZhon@gmail.com>
2021-12-01 02:08:09 +0300

    test: update test on 26.11.2021 with events parser
    Checking for events count, sequence and arguments.

--- tests/test_2021_11_26.py
+++ tests/test_2021_11_26.py
@@ -40,3 +40,30 @@ def test_2021_11_26(helpers, accounts, ldo_holder, dao_voting, ldo_token):
 
     assert multisig_balance_after - multisig_balance_before == isidoros_payout.amount + referral_payout.amount
     assert dao_balance_before - dao_balance_after == isidoros_payout.amount + referral_payout.amount
+
+    assert helpers.count_vote_items_by_events() == 2, "Incorrect voting items count"
+
+    helpers.display_voting_events()
+    # helpers.display_voting_call_trace() # uncomment for a paranoid mode ON
+
+    evs = helpers.group_voting_events()
+
+    # asserts on item 1
+    vote_item1_events = evs['vote_item1']
+    assert vote_item1_events.count('VaultTransfer') == 1, "Incorrect vault transfers occurred"
+    assert vote_item1_events.count('Transfer') == 1, "Incorrect LDO transfers occurred"
+    assert vote_item1_events['VaultTransfer']['to'] == isidoros_payout.address
+    assert vote_item1_events['VaultTransfer']['amount'] == isidoros_payout.amount
+
+    expected_events_sequence1 = ['LogScriptCall', 'NewTransaction', 'Transfer', 'VaultTransfer']
+    assert expected_events_sequence1 == [i.name for i in vote_item1_events]
+
+    # asserts on item 2
+    vote_item2_events = evs['vote_item2']
+    assert vote_item2_events.count('VaultTransfer') == 1, "Incorrect vault transfers occurred"
+    assert vote_item2_events.count('Transfer') == 1, "Incorrect LDO transfers occurred"
+    assert vote_item2_events['VaultTransfer']['to'] == referral_payout.address
+    assert vote_item2_events['VaultTransfer']['amount'] == referral_payout.amount
+
+    expected_events_sequence2 = ['LogScriptCall', 'NewTransaction', 'Transfer', 'VaultTransfer']
+    assert expected_events_sequence2 == [i.name for i in vote_item2_events]
