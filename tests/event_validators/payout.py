278e006e7b1d08e3a36ea64885e3732f3b614db7
Eugene Mamin <TheDZhon@gmail.com>
2021-12-10 18:11:31 +0300

    test: extract documented validate_events_chain fn

--- tests/event_validators/payout.py
+++ tests/event_validators/payout.py
@@ -10,16 +10,27 @@ class Payout(NamedTuple):
     to_addr: str
     amount: int
 
+# Check that all tx_events contained in reference_events (with proper ordering and occurrences count).
+# Allow some of the reference_events skipped in the tx_events. 
+#
+# Examples:
+#
+# tx_events: ['A', 'B', 'C'], reference_events: ['A', 'B', 'C'] => valid
+# tx_events: ['A', 'B', 'D'], reference_events: ['A', 'B', 'C', 'D'] => valid
+#
+# tx_events: ['A', 'B', 'D'], reference_events: ['A', 'B', 'C'] => invalid // extra 'D' event
+# tx_events: ['A', 'B', 'A', 'B'], reference_events: ['A', 'B'] => invalid // duplicated 'A', 'B' events chain
+# tx_events: ['A', 'C', 'B'], reference_events: ['A', 'B', 'C'] => invalid // wrong order
+def validate_events_chain (tx_events: [str], reference_events: [str]):
+    for ev in tx_events:
+        idx = next((reference_events.index(e) for e in reference_events if e == ev), len(reference_events))
+        assert idx != len(reference_events), f"{ev} not found in the remaining {reference_events} events chain"
+        reference_events=reference_events[idx+1:]
+
 def validate_payout_event (event: EventDict, p: Payout):
     _ldo_events_chain = ['LogScriptCall', 'NewPeriod', 'NewTransaction', 'Transfer', 'VaultTransfer']
 
-    # We check that transaction events contained in _ldo_events_chain (ordering and occurrences count are preserved)
-    # e.g. duplicated chain will trigger assert
-    events_chain = [e.name for e in event]
-    for ev in events_chain:
-        idx = next((_ldo_events_chain.index(e) for e in _ldo_events_chain if e == ev), len(_ldo_events_chain))
-        assert idx != len(_ldo_events_chain), f"{ev} not found in the remaining {_ldo_events_chain} events chain"
-        _ldo_events_chain=_ldo_events_chain[idx:]
+    validate_events_chain ([e.name for e in event], _ldo_events_chain)
 
     assert event.count('VaultTransfer') == 1
     assert event.count('Transfer') == 1
