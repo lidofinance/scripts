854a0abd00113121da349abc43c921aac17c34ba
Eugene Mamin <TheDZhon@gmail.com>
2021-12-23 15:30:13 +0300

    fix: remove orphaned ipfs code, update comments
    Remove ipfs vote helpers.
    Preserve comments consistency.

--- scripts/vote_2021_12_23.py
+++ scripts/vote_2021_12_23.py
@@ -1,8 +1,8 @@
 """
 Voting 23/12/2021.
 
-1. Send 4,200 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 10,000 DAI Isidoros Passadis Dec comp
-2. Send 6,900 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 16,666 DAI Jacob Blish Dec comp
+1. Send 4,200 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 10,000 DAI Master of Validators Dec comp
+2. Send 6,900 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 16,666 DAI BizDev Leader Dec comp
 3. Referral program payout of 235,290 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb
 
 """
@@ -29,9 +29,7 @@ from utils.config import (
     lido_dao_steth_address,
     lido_dao_voting_address,
     lido_dao_token_manager_address,
-    lido_dao_finance_address,
-    lido_dao_lido_repo,
-    lido_dao_node_operators_registry_repo,
+    lido_dao_finance_address
 )
 
 try:
@@ -69,16 +67,6 @@ def make_ldo_payout(
         finance=finance
     )
 
-def add_implementation_to_repo(repo, version, address, content_uri):
-    return (
-      repo.address,
-      repo.newVersion.encode_input(
-          version,
-          address,
-          content_uri
-      )
-    )
-
 def start_vote(
     tx_params: Dict[str, str],
     silent: bool = False
@@ -86,8 +74,6 @@ def start_vote(
     """Prepare and run voting."""
 
     voting = interface.Voting(lido_dao_voting_address)
-    lido_repo = interface.Repo(lido_dao_lido_repo)
-    nos_repo = interface.Repo(lido_dao_node_operators_registry_repo)
     finance = interface.Finance(lido_dao_finance_address)
     token_manager = interface.TokenManager(
         lido_dao_token_manager_address
@@ -95,36 +81,20 @@ def start_vote(
 
     _make_ldo_payout = partial(make_ldo_payout, finance=finance)
 
-    update_lido_app = {
-        'address': '0xC7B5aF82B05Eb3b64F12241B04B2cF14469E39F7',
-        'ipfsCid': 'QmQkJMtvu4tyJvWrPXJfjLfyTWn959iayyNjp7YqNzX7pS',
-        'content_uri': '0x697066733a516d516b4a4d7476753474794a76577250584a666a4c667954576e393539696179794e6a703759714e7a58377053',
-        'id': '0x3ca7c3e38968823ccb4c78ea688df41356f182ae1d159e4ee608d30d68cef320',
-        'version': (2, 0, 1),
-    }
-
-    update_node_operators_registry_app = {
-        'address': '0xec3567ae258639a0FF5A02F7eAF4E4aE4416C5fe',
-        'ipfsCid': 'Qma7PXHmEj4js2gjM9vtHPtqvuK82iS5EYPiJmzKLzU58G',
-        'content_uri': '0x697066733a516d61375058486d456a346a7332676a4d3976744850747176754b3832695335455950694a6d7a4b4c7a55353847',
-        'id': '0x7071f283424072341f856ac9e947e7ec0eb68719f757a7e785979b6b8717579d',
-        'version': (2, 0, 1),
-    }
-
     encoded_call_script = encode_call_script([
         # 1. Send 4,200 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 10,000 DAI Isidoros Passadis Dec comp
 
         _make_ldo_payout(
             target_address='0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb',
             ldo_in_wei=4_200 * (10 ** 18),
-            reference='Master of Validators monthly comp'
+            reference='Master of Validators Dec comp'
         ),
 
         # 2. Send 6,900 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 16,666 DAI Jacob Blish Dec comp
         _make_ldo_payout(
             target_address='0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb',
             ldo_in_wei=6_900 * (10 ** 18),
-            reference='Jacob Blish monthly comp'
+            reference='BizDev Leader Dec comp'
         ),
 
         # 3. Referral program payout of 235,290 LDO to financial multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb
@@ -164,7 +134,7 @@ def start_vote(
         vote_desc=(
             'Omnibus vote: '
             '1) Allocate 4,200 LDO tokens to Master of Validators Dec 2021 compensation;'
-            '2) Allocate 6,900 LDO tokens to Jacob Blish Dec 2021 compensation;'
+            '2) Allocate 6,900 LDO tokens to BizDev Leader Dec 2021 compensation;'
             '3) Allocate 235,290 LDO tokens for the 11th period referral rewards.'
         ),
         evm_script=encoded_call_script,
