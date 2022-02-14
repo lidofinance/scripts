a751abf0e9740792843f0d9343c3a852aa8978dd
Alexey Potapkin <apotapkin@demax.ru>
2022-02-14 12:00:52 +0300

    Make setup script look like a real one

--- scripts/setup_easytrack_limits.py
+++ scripts/setup_easytrack_limits.py
@@ -2,7 +2,7 @@
 """
 
 import time
-from typing import (Dict, Optional, Tuple)
+from typing import (Dict, Optional, Tuple, List)
 
 from brownie.network.transaction import TransactionReceipt
 
@@ -10,13 +10,11 @@ from utils.config import (
     get_deployer_account,
     lido_dao_finance_address,
     lido_dao_acl_address,
-    ldo_token_address,
-    lido_dao_steth_address,
 )
 from utils.evm_script import encode_call_script
 from utils.finance import ZERO_ADDRESS
-from utils.permission_parameters import *
-from utils.permissions import encode_permission_grant_p
+from utils.permission_parameters import SpecialArgumentID, encode_argument_value_if, Param, ArgumentValue, Op
+from utils.permissions import encode_permission_grant_p, encode_permission_revoke
 from utils.voting import confirm_vote_script, create_vote
 
 try:
@@ -28,12 +26,25 @@ except ImportError:
         'set_console_globals(interface=interface)'
     )
 
-eth_limit = 1_000 * (10 ** 18)
-steth_limit = 1_000 * (10 ** 18)
-ldo_limit = 5_000_000 * (10 ** 18)
+evmscriptexecutor_address = '0xFE5986E06210aC1eCC1aDCafc0cc7f8D63B3F977'
+
+eth = {
+    'limit': 1_000 * (10 ** 18),
+    'address': ZERO_ADDRESS,
+}
+
+steth = {
+    'limit': 1_000 * (10 ** 18),
+    'address': '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
+}
+
+ldo = {
+    'limit': 5_000_000 * (10 ** 18),
+    'address': '0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32',
+}
 
 
-def require_amount_under_limits() -> List[Param]:
+def require_amount_limits() -> List[Param]:
     """ Here we want to build such permissions that checks the _token and _amount pairs
         on each Finance#newImmediatePayment launch.
 
@@ -50,23 +61,23 @@ def require_amount_under_limits() -> List[Param]:
         Param(SpecialArgumentID.LOGIC_OP_PARAM_ID, Op.IF_ELSE,
               encode_argument_value_if(condition=1, success=2, failure=3)),
         # 1: (_token == ETH)
-        Param(token_arg_index, Op.EQ, ArgumentValue(ZERO_ADDRESS)),
+        Param(token_arg_index, Op.EQ, ArgumentValue(eth['address'])),
         # 2: { return _amount <= 1000 }
-        Param(amount_arg_index, Op.LTE, ArgumentValue(eth_limit)),
+        Param(amount_arg_index, Op.LTE, ArgumentValue(eth['limit'])),
         # 3: else if (4) then (5) else (6)
         Param(SpecialArgumentID.LOGIC_OP_PARAM_ID, Op.IF_ELSE,
               encode_argument_value_if(condition=4, success=5, failure=6)),
         # 4: (_token == LDO)
-        Param(token_arg_index, Op.EQ, ArgumentValue(ldo_token_address)),
+        Param(token_arg_index, Op.EQ, ArgumentValue(ldo['address'])),
         # 5: { return _amount <= 1_000_000 }
-        Param(amount_arg_index, Op.LTE, ArgumentValue(ldo_limit)),
+        Param(amount_arg_index, Op.LTE, ArgumentValue(ldo['limit'])),
         # 6: else if (7) then (8) else (9)
         Param(SpecialArgumentID.LOGIC_OP_PARAM_ID, Op.IF_ELSE,
               encode_argument_value_if(condition=7, success=8, failure=9)),
         # 7: (_token == stETH)
-        Param(token_arg_index, Op.EQ, ArgumentValue(lido_dao_steth_address)),
+        Param(token_arg_index, Op.EQ, ArgumentValue(steth['address'])),
         # 8: { return _amount <= 1000 }
-        Param(amount_arg_index, Op.LTE, ArgumentValue(steth_limit)),
+        Param(amount_arg_index, Op.LTE, ArgumentValue(steth['limit'])),
         # 9: else { return false }
         Param(SpecialArgumentID.PARAM_VALUE_PARAM_ID, Op.RET, ArgumentValue(0))
     ]
@@ -82,13 +93,15 @@ def start_vote(
     finance = interface.Finance(lido_dao_finance_address)
 
     encoded_call_script = encode_call_script([
-        encode_permission_grant_p(finance, 'CREATE_PAYMENTS_ROLE', tx_params['from'], acl,
-                                  params=require_amount_under_limits())
+        encode_permission_revoke(finance, 'CREATE_PAYMENTS_ROLE', evmscriptexecutor_address, acl),
+        encode_permission_grant_p(finance, 'CREATE_PAYMENTS_ROLE', evmscriptexecutor_address, acl, require_amount_limits())
     ])
 
     return confirm_vote_script(encoded_call_script, silent) and create_vote(
         vote_desc=(
-            'Test vote Aragon ACL:'
+            'Omnibus vote:',
+            '1) Revoke role CREATE_PAYMENTS_ROLE from EVMScriptExecutor',
+            '2) Grant role CREATE_PAYMENTS_ROLE to EVMScriptExecutor with amount limits'
         ),
         evm_script=encoded_call_script,
         tx_params=tx_params
--- tests/test_adding_permissions.py
+++ tests/test_adding_permissions.py
@@ -6,71 +6,79 @@ import brownie
 import pytest
 from brownie.test import given, strategy
 
-from scripts.add_granular_permission import start_vote, eth_limit, steth_limit, ldo_limit
-from utils.finance import ZERO_ADDRESS
+from scripts.setup_easytrack_limits import eth, steth, ldo, require_amount_limits
+from utils.evm_script import encode_call_script
+from utils.permissions import encode_permission_grant_p
+from utils.voting import confirm_vote_script, create_vote
 
 
 @pytest.fixture(scope="module", autouse=True)
-def tx(helpers, ldo_holder, vote_id_from_env, accounts, dao_voting):
-    vote_id = vote_id_from_env or start_vote({'from': ldo_holder}, silent=True)[0]
+def voting_tx(finance, acl, ldo_holder, helpers, accounts, dao_voting):
+    encoded_call_script = encode_call_script([
+        encode_permission_grant_p(finance, 'CREATE_PAYMENTS_ROLE', ldo_holder.address, acl, require_amount_limits())
+    ])
+
+    vote_id = confirm_vote_script(encoded_call_script, silent=True) and create_vote(
+        vote_desc='Test vote',
+        evm_script=encoded_call_script,
+        tx_params={'from': ldo_holder}
+    )[0]
 
     return helpers.execute_vote(
         vote_id=vote_id, accounts=accounts, dao_voting=dao_voting, topup='0.5 ether'
     )
 
 
-ldo_token = '0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32'
-steth_token = '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84'
 usdc_token = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
 
 
-@given(amount=strategy('uint', min_value=eth_limit + 1))
+@given(amount=strategy('uint', min_value=eth['limit'] + 1))
 def test_permission_fails_for_eth(acl, finance, ldo_holder, amount):
     assert not acl.hasPermission['address,address,bytes32,uint[]'](ldo_holder, finance, finance.CREATE_PAYMENTS_ROLE(),
-                                                                   [ZERO_ADDRESS, ldo_holder.address, amount])
+                                                                   [eth['address'], ldo_holder.address, amount])
     with brownie.reverts('APP_AUTH_FAILED'):
-        finance.newImmediatePayment(ZERO_ADDRESS, ldo_holder.address, amount, 'Should be reverted',
+        finance.newImmediatePayment(eth['address'], ldo_holder.address, amount, 'Should be reverted',
                                     {'from': ldo_holder})
 
 
-@given(amount=strategy('uint', min_value=1, max_value=eth_limit))
+@given(amount=strategy('uint', min_value=1, max_value=eth['limit']))
 def test_permission_pass_for_eth(acl, finance, ldo_holder, amount):
     assert acl.hasPermission['address,address,bytes32,uint[]'](ldo_holder, finance, finance.CREATE_PAYMENTS_ROLE(),
-                                                               [ZERO_ADDRESS, ldo_holder.address, amount])
-    finance.newImmediatePayment(ZERO_ADDRESS, ldo_holder.address, amount, 'test', {'from': ldo_holder})
+                                                               [eth['address'], ldo_holder.address, amount])
+    finance.newImmediatePayment(eth['address'], ldo_holder.address, amount, 'test', {'from': ldo_holder})
 
 
-@given(amount=strategy('uint', min_value=steth_limit + 1))
+@given(amount=strategy('uint', min_value=steth['limit'] + 1))
 def test_permission_fails_for_steth(acl, finance, ldo_holder, amount):
     assert not acl.hasPermission['address,address,bytes32,uint[]'](ldo_holder, finance, finance.CREATE_PAYMENTS_ROLE(),
-                                                                   [steth_token, ldo_holder.address, amount])
+                                                                   [steth['address'], ldo_holder.address, amount])
     with brownie.reverts('APP_AUTH_FAILED'):
-        finance.newImmediatePayment(steth_token, ldo_holder.address, amount, 'Should be reverted',
+        finance.newImmediatePayment(steth['address'], ldo_holder.address, amount, 'Should be reverted',
                                     {'from': ldo_holder})
 
 
-@given(amount=strategy('uint', min_value=1, max_value=steth_limit))
+@given(amount=strategy('uint', min_value=1, max_value=steth['limit']))
 def test_permission_pass_for_steth(acl, finance, ldo_holder, amount):
     assert acl.hasPermission['address,address,bytes32,uint[]'](ldo_holder, finance, finance.CREATE_PAYMENTS_ROLE(),
-                                                               [steth_token, ldo_holder.address, amount])
-    finance.newImmediatePayment(steth_token, ldo_holder.address, amount, 'test', {'from': ldo_holder})
+                                                               [steth['address'], ldo_holder.address, amount])
+    finance.newImmediatePayment(steth['address'], ldo_holder.address, amount, 'test', {'from': ldo_holder})
 
 
-@given(amount=strategy('uint', min_value=ldo_limit + 1))
+@given(amount=strategy('uint', min_value=ldo['limit'] + 1))
 def test_permission_fails_for_ldo(acl, finance, ldo_holder, amount):
-    assert not acl.hasPermission['address,address,bytes32,uint[]'](ldo_token, finance,
+    assert not acl.hasPermission['address,address,bytes32,uint[]'](ldo['address'], finance,
                                                                    finance.CREATE_PAYMENTS_ROLE(),
-                                                                   [steth_token, ldo_holder.address, amount])
+                                                                   [ldo['address'], ldo_holder.address, amount])
     with brownie.reverts('APP_AUTH_FAILED'):
-        finance.newImmediatePayment(ldo_token, ldo_holder.address, amount, 'Should be reverted',
+        finance.newImmediatePayment(ldo['address'], ldo_holder.address, amount, 'Should be reverted',
                                     {'from': ldo_holder})
 
 
-@given(amount=strategy('uint', min_value=1, max_value=ldo_limit))
+@given(amount=strategy('uint', min_value=1, max_value=ldo['limit']))
 def test_permission_pass_for_ldo(acl, finance, ldo_holder, amount):
     assert acl.hasPermission['address,address,bytes32,uint[]'](ldo_holder, finance, finance.CREATE_PAYMENTS_ROLE(),
-                                                               [ldo_token, ldo_holder.address, amount])
-    finance.newImmediatePayment(ldo_token, ldo_holder.address, amount, 'test', {'from': ldo_holder})
+                                                               [ldo['address'], ldo_holder.address, amount])
+    finance.newImmediatePayment(ldo['address'], ldo_holder.address, amount, 'test', {'from': ldo_holder})
 
 
 @given(amount=strategy('uint', min_value=1))
