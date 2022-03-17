53052a694061311d5f53d15f1fc0ebebd9dc17bc
Eugene Mamin <TheDZhon@gmail.com>
2022-03-17 11:18:42 +0000

    fix: improve voting script and test
    Remove redundant code chunks.
    Update amounts.

--- scripts/vote_2022_03_17.py
+++ scripts/vote_2022_03_17.py
@@ -1,109 +1,29 @@
 """
 Voting 17/03/2022.
 
-1. Send 722,962 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABbfor 17th period referral rewards
-2. Send 3,896 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 10,000 DAI Isidoros Passadis March comp
-3. Send 6,493 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 16,666 DAI Jacob Blish March comp
+1. Send 722,962 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 17th period referral rewards
+2. Send 3,900 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 10,000 DAI Isidoros Passadis March comp
+3. Send 6,500 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 16,666 DAI Jacob Blish March comp
 """
 
 import time
 
-from typing import (Dict, Tuple, Optional, List)
+from typing import (Dict, Tuple, Optional)
 
 from brownie.network.transaction import TransactionReceipt
 
-from utils.finance import make_ldo_payout, ZERO_ADDRESS
-from utils.permission_parameters import Param, SpecialArgumentID, encode_argument_value_if, ArgumentValue, Op
-from utils.permissions import encode_permission_revoke, encode_permission_grant_p
+from utils.finance import make_ldo_payout
 from utils.voting import confirm_vote_script, create_vote
 from utils.evm_script import encode_call_script
-from utils.config import (
-    get_deployer_account,
-    lido_dao_finance_address,
-    lido_dao_acl_address,
-    lido_dao_steth_address,
-    ldo_token_address,
-    dai_token_address,
-    lido_easytrack_evmscriptexecutor
-)
+from utils.config import get_deployer_account
+
 from utils.brownie_prelude import *
-
-evmscriptexecutor = lido_easytrack_evmscriptexecutor
-
-eth = {
-    'limit': 1_000 * (10 ** 18),
-    'address': ZERO_ADDRESS,
-}
-
-steth = {
-    'limit': 1_000 * (10 ** 18),
-    'address': lido_dao_steth_address,
-}
-
-ldo = {
-    'limit': 5_000_000 * (10 ** 18),
-    'address': ldo_token_address,
-}
-
-dai = {
-    'limit': 100_000 * (10 ** 18),
-    'address': dai_token_address,
-}
-
-
-def amount_limits() -> List[Param]:
-    """ Here we want to build such permissions that checks the _token and _amount pairs
-        on each Finance#newImmediatePayment launch.
-
-        Arguments provided from that method:
-        `authP(CREATE_PAYMENTS_ROLE, _arr(_token, _receiver, _amount, MAX_UINT256, uint256(1), getTimestamp()))`
-
-        See https://etherscan.io/address/0x836835289a2e81b66ae5d95b7c8dbc0480dcf9da#code#L1549
-    """
-    token_arg_index = 0
-    amount_arg_index = 2
-
-    return [
-        # 0: if (1) then (2) else (3)
-        Param(SpecialArgumentID.LOGIC_OP_PARAM_ID, Op.IF_ELSE,
-              encode_argument_value_if(condition=1, success=2, failure=3)),
-        # 1: (_token == LDO)
-        Param(token_arg_index, Op.EQ, ArgumentValue(ldo['address'])),
-        # 2: { return _amount <= 5_000_000 }
-        Param(amount_arg_index, Op.LTE, ArgumentValue(ldo['limit'])),
-        # 3: else if (4) then (5) else (6)
-        Param(SpecialArgumentID.LOGIC_OP_PARAM_ID, Op.IF_ELSE,
-              encode_argument_value_if(condition=4, success=5, failure=6)),
-        # 4: (_token == ETH)
-        Param(token_arg_index, Op.EQ, ArgumentValue(eth['address'])),
-        # 5: { return _amount <= 1000 }
-        Param(amount_arg_index, Op.LTE, ArgumentValue(eth['limit'])),
-        # 6: else if (7) then (8) else (9)
-        Param(SpecialArgumentID.LOGIC_OP_PARAM_ID, Op.IF_ELSE,
-              encode_argument_value_if(condition=7, success=8, failure=9)),
-        # 7: (_token == DAI)
-        Param(token_arg_index, Op.EQ, ArgumentValue(dai['address'])),
-        # 8: { return _amount <= 100_000 }
-        Param(amount_arg_index, Op.LTE, ArgumentValue(dai['limit'])),
-        # 9: else if (10) then (11) else (12)
-        Param(SpecialArgumentID.LOGIC_OP_PARAM_ID, Op.IF_ELSE,
-              encode_argument_value_if(condition=10, success=11, failure=12)),
-        # 10: (_token == stETH)
-        Param(token_arg_index, Op.EQ, ArgumentValue(steth['address'])),
-        # 11: { return _amount <= 1000 }
-        Param(amount_arg_index, Op.LTE, ArgumentValue(steth['limit'])),
-        # 12: else { return false }
-        Param(SpecialArgumentID.PARAM_VALUE_PARAM_ID, Op.RET, ArgumentValue(0))
-    ]
-
 
 def start_vote(
         tx_params: Dict[str, str],
         silent: bool = False
 ) -> Tuple[int, Optional[TransactionReceipt]]:
     """Prepare and run voting."""
-    acl = interface.ACL(lido_dao_acl_address)
-    finance = interface.Finance(lido_dao_finance_address)
 
     encoded_call_script = encode_call_script([
         # 1. Send 722,962 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 17th period referral rewards
@@ -113,29 +33,27 @@ def start_vote(
             reference="17th period referral rewards"
         ),
 
-        # 2. Send 3,896 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 10,000 DAI Isidoros Passadis March comp
+        # 2. Send 3,900 LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 10,000 DAI Isidoros Passadis March comp
         make_ldo_payout(
             target_address='0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb',
-            ldo_in_wei=3_896 * (10 ** 18),
+            ldo_in_wei=3_900 * (10 ** 18),
             reference='Master of Validators March comp'
         ),
 
-        # 3. Send 6,493  LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 16,666 DAI Jacob Blish March comp
+        # 3. Send 6,500  LDO to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb for 16,666 DAI Jacob Blish March comp
         make_ldo_payout(
             target_address='0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb',
-            ldo_in_wei=6_493 * (10 ** 18),
+            ldo_in_wei=6_500 * (10 ** 18),
             reference='BizDev Leader March comp'
-        ),
-
+        )
     ])
 
     return confirm_vote_script(encoded_call_script, silent) and create_vote(
         vote_desc=(
             'Omnibus vote: '
             '1) Allocate 722,962 LDO tokens for the 17th period referral rewards;'
-            '2) Allocate 3,896 LDO tokens to Master of Validators March 2022 compensation;'
-            '3) Allocate 6,493 LDO tokens to BizDev Leader March 2022 compensation;'
-
+            '2) Allocate 3,900 LDO tokens to Master of Validators March 2022 compensation;'
+            '3) Allocate 6,500 LDO tokens to BizDev Leader March 2022 compensation.'
         ),
         evm_script=encoded_call_script,
         tx_params=tx_params
