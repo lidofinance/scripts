c21124d234598e177bb56acfa4d11277e04e64fa
Eugene Mamin <TheDZhon@gmail.com>
2022-03-22 10:50:26 +0000

    fix: remove issue and vesting points
    Proceed with the option #2:
    https://research.lido.fi/t/proposal-to-freeze-vesting-for-compromised-address/1864/13.

--- scripts/vote_2022_03_22.py
+++ scripts/vote_2022_03_22.py
@@ -4,10 +4,8 @@ Voting 22/03/2022.
 1. Create role BURN_ROLE for TokenManager 0xDfe76d11b365f5e0023343A367f0b311701B3bc1
 2. Create role ISSUE_ROLE for TokenManager 0xDfe76d11b365f5e0023343A367f0b311701B3bc1
 3. Burn X LDO tokens on 0x48Acf41D10a063f9A6B718B9AAd2e2fF5B319Ca2
-4. Issue X LDO tokens in favor of TokenManager 0xDfe76d11b365f5e0023343A367f0b311701B3bc1
-5. Assign vested X LDO tokens to 0x... till Sun Dec 18 2022 00:00:00 +UTC
-6. Revoke BURN_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db
-7. Revoke ISSUE_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db
+4. Revoke BURN_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db
+5. Revoke ISSUE_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db
 
 """
 
@@ -86,12 +84,6 @@ def start_vote(
     ldo_amount: int = 3_700_000 * 10 ** 18 # FIXME: Should be recalculated according to the execution time with additional margin
 
     source_address: str = '0x48Acf41D10a063f9A6B718B9AAd2e2fF5B319Ca2'
-    target_address: str = '0xb8FFC3Cd6e7Cf5a098A1c92F48009765B24088Dc' #FIXME: CHANGE IT
-
-    # see also https://www.unixtimestamp.com/index.php for time conversions
-    start: int = 1639785600   # Sat Dec 18 2021 00:00:00 GMT+0000 (3 months ago)
-    cliff: int = 1639785600   # Sat Dec 18 2021 00:00:00 GMT+0000 (3 months ago)
-    vesting: int = 1671321600 # Sun Dec 18 2022 00:00:00 GMT+0000 (in 9 months)
 
     encoded_call_script = encode_call_script([
         # 1. Create role BURN_ROLE for TokenManager 0xDfe76d11b365f5e0023343A367f0b311701B3bc1
@@ -100,19 +92,9 @@ def start_vote(
         create_permission(entity=voting, target_app=token_manager, permission_name='ISSUE_ROLE', manager=voting, acl=acl),
         # 3. Burn X LDO tokens on 0x48Acf41D10a063f9A6B718B9AAd2e2fF5B319Ca2
         burn_ldo(source_address, ldo_amount),
-        # 4. Issue X LDO tokens in favor of TokenManager 0xDfe76d11b365f5e0023343A367f0b311701B3bc1
-        issue_ldo(ldo_amount),
-        # 5. Assign vested X LDO tokens to 0x... till Sun Dec 18 2022 00:00:00 +UTC
-        assign_vested(
-            target_address,
-            ldo_amount,
-            start=start,
-            cliff=cliff,
-            vesting=vesting
-        ),
-        # 6. Revoke BURN_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db
+        # 4. Revoke BURN_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db
         encode_permission_revoke(target_app=token_manager, permission_name='BURN_ROLE', revoke_from=voting, acl=acl),
-        # 7. Revoke ISSUE_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db
+        # 5. Revoke ISSUE_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db
         encode_permission_revoke(target_app=token_manager, permission_name='ISSUE_ROLE', revoke_from=voting, acl=acl)
     ])
 
@@ -122,10 +104,8 @@ def start_vote(
             '1) Create role BURN_ROLE for TokenManager 0xDfe76d11b365f5e0023343A367f0b311701B3bc1;'
             '2) Create role ISSUE_ROLE for TokenManager 0xDfe76d11b365f5e0023343A367f0b311701B3bc1;'
             '3) Burn X LDO tokens on 0x48Acf41D10a063f9A6B718B9AAd2e2fF5B319Ca2;'
-            '4) Issue X LDO tokens in favor of TokenManager 0xDfe76d11b365f5e0023343A367f0b311701B3bc1;'
-            '5) Assign vested X LDO tokens to 0x... till Sun Dec 18 2022 00:00:00 +UTC;'
-            '6) Revoke BURN_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db;'
-            '7) Revoke ISSUE_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db.'
+            '4) Revoke BURN_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db;'
+            '5) Revoke ISSUE_ROLE from Voting 0xbc0B67b4553f4CF52a913DE9A6eD0057E2E758Db.'
         ),
         evm_script=encoded_call_script,
         tx_params=tx_params
--- tests/test_2022_03_22.py
+++ tests/test_2022_03_22.py
@@ -6,11 +6,6 @@ from tx_tracing_helpers import *
 
 ldo_amount: int = 3_700_000 * 10 ** 18 # FIXME: CHANGE IT
 source_address: str = '0x48Acf41D10a063f9A6B718B9AAd2e2fF5B319Ca2' #FIXME: CHANGE IT
-target_address: str = '0xb8FFC3Cd6e7Cf5a098A1c92F48009765B24088Dc' #FIXME: CHANGE IT
-
-start: int = 1639785600   # Sat Dec 18 2021 00:00:00 GMT+0000 (3 months ago) #FIXME: CHANGE IT
-cliff: int = 1639785600   # Sat Dec 18 2021 00:00:00 GMT+0000 (3 months ago) #FIXME: CHANGE IT
-vesting: int = 1671321600 # Sun Dec 18 2022 00:00:00 GMT+0000 (in 9 months) #FIXME: CHANGE IT
 
 lido_dao_token = '0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32'
 
@@ -21,9 +16,7 @@ def test_ldo_recover(
 ):
     total_supply_before = ldo_token.totalSupply()
     source_balance_before = ldo_token.balanceOf(source_address)
-    target_balance_before = ldo_token.balanceOf(target_address)
     token_manager_balance_before = ldo_token.balanceOf(dao_token_manager)
-    vesting_length_before = dao_token_manager.vestingsLengths(target_address)
 
     assert not acl.hasPermission(dao_voting, dao_token_manager, dao_token_manager.BURN_ROLE())
     assert not acl.hasPermission(dao_voting, dao_token_manager, dao_token_manager.ISSUE_ROLE())
@@ -39,30 +32,17 @@ def test_ldo_recover(
 
     total_supply_after = ldo_token.totalSupply()
     source_balance_after= ldo_token.balanceOf(source_address)
-    target_balance_after = ldo_token.balanceOf(target_address)
     token_manager_balance_after = ldo_token.balanceOf(dao_token_manager)
-    vesting_length_after = dao_token_manager.vestingsLengths(target_address)
 
-    assert total_supply_before == total_supply_after, "Total supply changed"
+    assert total_supply_before == (total_supply_after + ldo_amount), "Total supply changed"
     assert source_balance_before == source_balance_after + ldo_amount, "Incorrect LDO amount"
-    assert target_balance_before == target_balance_after - ldo_amount, "Incorrect LDO amount"
     assert token_manager_balance_before == token_manager_balance_after, "Incorrect LDO amount"
 
     assert not acl.hasPermission(dao_voting, dao_token_manager, dao_token_manager.BURN_ROLE())
     assert not acl.hasPermission(dao_voting, dao_token_manager, dao_token_manager.ISSUE_ROLE())
 
-    assert vesting_length_after == vesting_length_before + 1, "Incorrect vestings length"
-
-    vesting_params = dao_token_manager.getVesting(target_address, vesting_length_before)
-
-    assert vesting_params['amount'] == ldo_amount
-    assert vesting_params['start'] == start
-    assert vesting_params['cliff'] == cliff
-    assert vesting_params['vesting'] == vesting
-    assert vesting_params['revokable'] == False
-
     ### validate vote events
-    assert count_vote_items_by_events(tx) == 7, "Incorrect voting items count"
+    assert count_vote_items_by_events(tx) == 5, "Incorrect voting items count"
 
     display_voting_events(tx)
 
