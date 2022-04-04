4ea124f255b6e1d1b85082e013658fc1230468be
Alexey Potapkin <apotapkin@demax.ru>
2022-04-04 22:14:48 +0300

    fix: fix encode_permission to work for new roles

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,17 +1,21 @@
 from typing import Tuple, List
+from web3 import Web3
 
+from brownie import convert
 from utils.config import contracts
 from utils.permission_parameters import Param, encode_permission_params
+
 
 def encode_permission_create(
     entity,
     target_app,
-    permission_name,
+    permission_name: str,
     manager
 ) -> Tuple[str, str]:
     acl = contracts.acl
-    permission_id = getattr(target_app, permission_name)()
+    permission_id = convert.to_int(Web3.keccak(text=permission_name))
     return acl.address, acl.createPermission.encode_input(entity, target_app, permission_id, manager)
+
 
 def encode_permission_grant(
         target_app,
@@ -19,13 +23,13 @@ def encode_permission_grant(
         grant_to: str
 ) -> Tuple[str, str]:
     acl = contracts.acl
-    permission_id = getattr(target_app, permission_name)()
+    permission_id = convert.to_int(Web3.keccak(text=permission_name))
     return acl.address, acl.grantPermission.encode_input(grant_to, target_app, permission_id)
 
 
 def encode_permission_revoke(target_app, permission_name, revoke_from) -> Tuple[str, str]:
     acl = contracts.acl
-    permission_id = getattr(target_app, permission_name)()
+    permission_id = convert.to_int(Web3.keccak(text=permission_name))
     return acl.address, acl.revokePermission.encode_input(revoke_from, target_app, permission_id)
 
 
@@ -36,7 +40,7 @@ def encode_permission_grant_p(
         params: List[Param],
 ) -> Tuple[str, str]:
     acl = contracts.acl
-    permission_id = getattr(target_app, permission_name)()
+    permission_id = convert.to_int(Web3.keccak(text=permission_name))
 
     uint256_params = encode_permission_params(params)
 
