0ed3b4bdf7f45e523cad260a4fa8ad95d1e712fc
Eugene Mamin <TheDZhon@gmail.com>
2022-04-12 10:16:45 +0000

    feat: add vote 2022_04_12
    Needs to be called with access lists.

---
+++ scripts/vote_2022_04_12.py
@@ -0,0 +1,68 @@
+"""
+Voting 12/04/2022.
+
+1. Refund previous depositor' spending to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb
+   with 254.684812629886507249 ETH.
+2. Fund dedicated depositor multisig 0x5181d5D56Af4f823b96FE05f062D7a09761a5a53 with 130 ETH.
+
+"""
+
+import time
+
+from typing import (Dict, Tuple, Optional)
+
+from brownie.network.transaction import TransactionReceipt
+
+from utils.voting import confirm_vote_script, create_vote
+from utils.finance import make_eth_payout
+from utils.evm_script import encode_call_script
+from utils.config import (
+    get_deployer_account,
+    get_is_live
+)
+
+def start_vote(
+    tx_params: Dict[str, str],
+    silent: bool = False
+) -> Tuple[int, Optional[TransactionReceipt]]:
+    """Prepare and run voting."""
+
+    encoded_call_script = encode_call_script([
+        # 1. Refund previous depositor' spending to finance multisig 0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb
+        # with 254.684812629886507249 ETH.
+        make_eth_payout(
+            target_address='0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb',
+            eth_in_wei=254_684_812_629_886_507_249,
+            reference='Refund depositor\'s spending'
+        ),
+        # 2. Fund dedicated depositor multisig 0x5181d5D56Af4f823b96FE05f062D7a09761a5a53 with 130 ETH.
+        make_eth_payout(
+            target_address='0x5181d5D56Af4f823b96FE05f062D7a09761a5a53',
+            eth_in_wei=130 * (10 ** 18),
+            reference='Fund depositor\'s multisig'
+        ),
+    ])
+
+    return confirm_vote_script(encoded_call_script, silent) and create_vote(
+        vote_desc=(
+            'Omnibus vote: '
+            '1) Refund previous depositor\' spending to finance multisig with 254.684812629886507249 ETH; '
+            '2) Fund dedicated depositor multisig 0x5181d5D56Af4f823b96FE05f062D7a09761a5a53 with 130 ETH.'
+        ),
+        evm_script=encoded_call_script,
+        tx_params=tx_params
+    )
+
+
+def main():
+    tx_params = {'from': get_deployer_account()}
+
+    if get_is_live():
+        tx_params['max_fee'] = '300 gwei'
+        tx_params['priority_fee'] = '2 gwei'
+
+    vote_id, _ = start_vote(tx_params=tx_params)
+
+    vote_id >= 0 and print(f'Vote created: {vote_id}.')
+
+    time.sleep(5)  # hack for waiting thread #2.
---
+++ tests/test_2022_04_12.py
@@ -0,0 +1,68 @@
+"""
+Tests for voting 12/04/2022.
+"""
+
+from event_validators.payout import Payout, validate_payout_event
+
+from utils.finance import ZERO_ADDRESS
+from scripts.vote_2022_04_12 import start_vote
+from tx_tracing_helpers import *
+
+finance_multisig_address = '0x48F300bD3C52c7dA6aAbDE4B683dEB27d38B9ABb'
+depositor_multisig_address = '0x5181d5D56Af4f823b96FE05f062D7a09761a5a53'
+dao_agent_address = '0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c'
+
+refund_payout = Payout(
+    token_addr=ZERO_ADDRESS,
+    from_addr=dao_agent_address,
+    to_addr=finance_multisig_address,
+    amount=254_684_812_629_886_507_249
+)
+
+fund_payout = Payout(
+    token_addr=ZERO_ADDRESS,
+    from_addr=dao_agent_address,
+    to_addr=depositor_multisig_address,
+    amount=130 * (10 ** 18)
+)
+
+def test_2022_04_12(
+    helpers, accounts, ldo_holder, dao_voting,
+    vote_id_from_env, bypass_events_decoding
+):
+    finance_multisig_balance_before = accounts.at(finance_multisig_address, force=True)
+    depositor_multisig_balance_before = accounts.at(depositor_multisig_address, force=True)
+    dao_balance_before = accounts.at(dao_agent_address, force=True)
+
+    ##
+    ## START VOTE
+    ##
+    vote_id = vote_id_from_env or start_vote({'from': ldo_holder}, silent=True)[0]
+
+    tx: TransactionReceipt = helpers.execute_vote(
+        vote_id=vote_id, accounts=accounts, dao_voting=dao_voting
+    )
+
+    finance_multisig_balance_after = accounts.at(finance_multisig_address, force=True)
+    depositor_multisig_balance_after = accounts.at(depositor_multisig_address, force=True)
+    dao_balance_after = accounts.at(dao_agent_address, force=True)
+
+    assert finance_multisig_balance_after - finance_multisig_balance_before == refund_payout.amount
+    assert depositor_multisig_balance_after - depositor_multisig_balance_before == fund_payout.amount
+    assert dao_balance_after - dao_balance_before == refund_payout.amount + fund_payout.amount
+
+    ### validate vote events
+    assert count_vote_items_by_events(tx) == 2, "Incorrect voting items count"
+
+    display_voting_events(tx)
+
+    if bypass_events_decoding:
+        return
+
+    evs = group_voting_events(tx)
+
+    # asserts on vote item 1
+    validate_payout_event(evs[0], refund_payout)
+
+    # asserts on vote item 2
+    validate_payout_event(evs[1], fund_payout)
