c0c5e78539f924974cd32887a5da8ab710c58e4b
Eugene Mamin <TheDZhon@gmail.com>
2022-04-16 08:46:57 +0000

    fix: update unpause event validator

--- tests/event_validators/unpause.py
+++ tests/event_validators/unpause.py
@@ -7,9 +7,9 @@ from .common import validate_events_chain
 
 
 def validate_unpause_event(event: EventDict):
-    _unpause_events_chain = ['LogScriptCall', 'DepositsUnpaused']
+    # extra 'LogScriptCall' and 'ScriptResult' due to agent forwarding
+    _unpause_events_chain = ['LogScriptCall', 'LogScriptCall', 'DepositsUnpaused', 'ScriptResult']
 
     validate_events_chain([e.name for e in event], _unpause_events_chain)
 
     assert event.count('DepositsUnpaused') == 1
-
