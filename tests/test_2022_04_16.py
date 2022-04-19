fb3c2cb4556d4647f7da25a07afa1f8275324d2f
Alexey Potapkin <apotapkin@demax.ru>
2022-04-19 12:28:11 +0300

    chore: pep8 and comments for 16.04 vote

--- tests/test_2022_04_16.py
+++ tests/test_2022_04_16.py
@@ -1,5 +1,5 @@
 """
-Tests for voting 16/04/2021.
+Tests for voting 16/04/2022.
 """
 
 from event_validators.unpause import (
@@ -10,12 +10,13 @@ from tx_tracing_helpers import *
 
 from scripts.vote_2022_04_16 import start_vote
 
-def test_2021_11_11(ldo_holder, helpers, accounts, dao_voting, deposit_security_module, vote_id_from_env, bypass_events_decoding):
+
+def test_2022_04_16(ldo_holder, helpers, accounts, dao_voting, deposit_security_module, vote_id_from_env, bypass_events_decoding):
     assert deposit_security_module.isPaused()
 
-    ##
-    ## START VOTE
-    ##
+    #
+    # START VOTE
+    #
     vote_id = vote_id_from_env or start_vote({'from': ldo_holder}, silent=True)[0]
 
     tx: TransactionReceipt = helpers.execute_vote(
@@ -24,7 +25,7 @@ def test_2021_11_11(ldo_holder, helpers, accounts, dao_voting, deposit_security_
 
     assert not deposit_security_module.isPaused()
 
-    ### validate vote events
+    # validate vote events
     assert count_vote_items_by_events(tx, dao_voting) == 1, "Incorrect voting items count"
 
     display_voting_events(tx)
