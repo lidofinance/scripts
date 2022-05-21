a77c51ec515fb9b3368c9f45e4d126580a2094e0
Bogdan Kovtun <psirex48@gmail.com>
2022-05-21 03:15:44 +0300

    Add additional tests for pause/resume roles

--- tests/test_2022_05_17_pause_role_tests.py
+++ tests/test_2022_05_17_pause_role_tests.py
@@ -1,9 +1,10 @@
 """
-Tests for lido resume method for voting 17/05/2022
+Tests for lido resume/pause roles 17/05/2022
 """
 import json
 import pytest
-from brownie import reverts, web3
+from functools import partial
+from brownie import reverts, web3, ZERO_ADDRESS
 from scripts.vote_2022_05_17 import (
     start_vote,
     update_lido_app,
@@ -46,11 +47,6 @@ def stranger(accounts):
     return accounts[0]
 
 
-@pytest.fixture(scope="module")
-def dao_voting_as_eoa(accounts, dao_voting):
-    return accounts.at(dao_voting.address, force=True)
-
-
 @pytest.fixture(scope="module", autouse=True)
 def autoexecute_vote(vote_id_from_env, ldo_holder, helpers, accounts, dao_voting):
     vote_id = vote_id_from_env or start_vote({"from": ldo_holder}, silent=True)[0]
@@ -60,35 +56,107 @@ def autoexecute_vote(vote_id_from_env, ldo_holder, helpers, accounts, dao_voting
         dao_voting=dao_voting,
         skip_time=3 * 60 * 60 * 24,
     )
-    print(f"vote {vote_id} was executed")
 
 
 @pytest.fixture(scope="module", autouse=True)
-def autopause_lido(lido, dao_voting_as_eoa):
-    lido.stop({"from": dao_voting_as_eoa})
-    assert lido.isStopped()
-
-
-def test_resume_by_stranger(lido, stranger, dao_voting_as_eoa, acl):
-    resume_role = web3.keccak(text="RESUME_ROLE")
-    # Test that stranger has no RESUME_ROLE permission
-    assert not acl.hasPermission["address,address,bytes32,uint[]"](
-        stranger, lido, resume_role, []
+def create_resume_role_permission(acl, lido, dao_voting):
+    resume_role_encoded = web3.keccak(text="RESUME_ROLE")
+    acl.createPermission(
+        dao_voting,
+        lido,
+        resume_role_encoded,
+        dao_voting,
+        {"from": dao_voting},
     )
-    # Test that stranger can't resume the Lido
+    assert has_role(acl=acl, entity=dao_voting, app=lido, role="RESUME_ROLE")
+
+
+@pytest.fixture(scope="function")
+def stop_lido(lido, dao_voting):
+    if not lido.isStopped():
+        lido.stop({"from": dao_voting})
+
+
+@pytest.fixture(scope="function")
+def resume_lido(lido, dao_voting):
+    if lido.isStopped():
+        lido.resume({"from": dao_voting})
+
+
+@pytest.mark.usefixtures("stop_lido")
+def test_pause_role_cant_resume(acl, lido, dao_voting, stranger):
+    stranger_has_role = partial(has_role, acl=acl, app=lido, entity=stranger)
+    assert not stranger_has_role(role="RESUME_ROLE")
+
+    acl.grantPermission(
+        stranger,
+        lido,
+        web3.keccak(text="PAUSE_ROLE"),
+        {"from": dao_voting},
+    )
+    assert stranger_has_role(role="PAUSE_ROLE")
+
+    assert lido.isStopped()
     with reverts("APP_AUTH_FAILED"):
         lido.resume({"from": stranger})
 
-    acl.createPermission(
+
+@pytest.mark.usefixtures("resume_lido")
+def test_resume_role_cant_pause(acl, lido, dao_voting, stranger):
+    stranger_has_role = partial(has_role, acl=acl, app=lido, entity=stranger)
+    assert not stranger_has_role(role="PAUSE_ROLE")
+
+    acl.grantPermission(
         stranger,
         lido,
-        resume_role,
-        dao_voting_as_eoa,
-        {"from": dao_voting_as_eoa},
+        web3.keccak(text="RESUME_ROLE"),
+        {"from": dao_voting},
     )
-    assert acl.hasPermission["address,address,bytes32,uint[]"](
-        stranger, lido, resume_role, []
+    assert stranger_has_role(role="RESUME_ROLE")
+
+    assert not lido.isStopped()
+    with reverts("APP_AUTH_FAILED"):
+        lido.stop({"from": stranger})
+
+
+@pytest.mark.usefixtures("stop_lido")
+def test_resume_role_can_resume(acl, lido, dao_voting, stranger):
+    stranger_has_role = partial(has_role, acl=acl, app=lido, entity=stranger)
+    assert not stranger_has_role(role="PAUSE_ROLE")
+
+    acl.grantPermission(
+        stranger,
+        lido,
+        web3.keccak(text="RESUME_ROLE"),
+        {"from": dao_voting},
     )
-    # Test that stranger now can resume the Lido
+    assert stranger_has_role(role="RESUME_ROLE")
+
+    assert lido.isStopped()
     lido.resume({"from": stranger})
     assert not lido.isStopped()
+
+
+@pytest.mark.usefixtures("resume_lido")
+def test_pause_role_can_pause(acl, lido, dao_voting, stranger):
+    stranger_has_role = partial(has_role, acl=acl, app=lido, entity=stranger)
+    assert not stranger_has_role(role="RESUME_ROLE")
+
+    acl.grantPermission(
+        stranger,
+        lido,
+        web3.keccak(text="PAUSE_ROLE"),
+        {"from": dao_voting},
+    )
+    assert stranger_has_role(role="PAUSE_ROLE")
+
+    assert not lido.isStopped()
+    lido.stop({"from": stranger})
+    assert lido.isStopped()
+
+
+def has_role(acl, entity, app, role):
+    encoded_role = web3.keccak(text=role)
+    return acl.hasPermission["address,address,bytes32,uint[]"](
+        entity, app, encoded_role, []
+    )
