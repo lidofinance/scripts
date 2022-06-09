b50df84bc05cd3ba2d807adc8960507d7f0d2907
Artyom Veremeenko <arwer13@gmail.com>
2022-06-09 10:35:33 +0300

    replace 2-phase voting vote script with DepositSecurityModule upgrade

--- scripts/vote_2022_06_14.py
+++ scripts/vote_2022_06_14.py
@@ -1,110 +1,98 @@
 """
 Voting 14/06/2022.
 
-1. Push new voting app version to Voting Repo 0x41D65FA420bBC714686E798a0eB0Df3799cEF092.
-2. Upgrade the DAO Voting ##address TBA## contract implementation.
-3. Grant `UNSAFELY_MODIFY_VOTE_TIME_ROLE` to DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e.
-4. Update objection phase duration with `unsafelyChangeObjectionTime()` to 24 hours.
-5. Revoke `UNSAFELY_MODIFY_VOTE_TIME_ROLE` from DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e.
-
+1. Revoke DEPOSIT_ROLE from old DepositSecurityModule 0xDb149235B6F40dC08810AA69869783Be101790e7
+2. Grant DEPOSIT_ROLE to new DepositSecurityModule 0x710B3303fB508a84F10793c1106e32bE873C24cd
+3. Set lastDepositBlock of DepositSecurityModule to {{??? block number TBD}}
 
 """
 
 import time
 
-from typing import (Dict, Tuple, Optional)
+from typing import (Dict, Tuple, Optional, List)
 
 from brownie.network.transaction import TransactionReceipt
 
 from utils.voting import confirm_vote_script, create_vote
-from utils.permissions import encode_permission_create, encode_permission_grant, encode_permission_revoke
-from utils.repo import add_implementation_to_voting_app_repo
-from utils.kernel import update_app_implementation
 from utils.evm_script import encode_call_script
+from utils.agent import agent_forward
+
 from utils.config import (
     get_deployer_account,
-    contracts,
     get_is_live,
-    network_name
+    contracts, network_name,
+    lido_dao_deposit_security_module_address
 )
-
-update_voting_app = {
-    'new_address': '',  # TBA
-    'content_uri': '',  # TBA
-    'id': '0x0abcd104777321a82b010357f20887d61247493d89d2e987ff57bcecbde00e1e',
-    'version': (3, 0, 0),
-    'vote_time': 172_800 + 86_400,  # 48 + 24 hours
-    'objection_time': 86_400  # 24 hours
-}
-
-if network_name() in ("goerli", "goerli-fork"):
-    update_voting_app['new_address'] = '0x12D103a07Ac0429519C77E96781dFD5186119582'  # TBA
-    update_voting_app['content_uri'] = '0x697066733a516d5962774366374d6e6932797a31553358334769485667396f35316a6b53586731533877433257547755684859'  # TBA
-    update_voting_app['id'] = '0xee7f2abf043afe722001aaa900627a6e29adcbcce63a561fbd97e0a0c6429b94'
-    update_voting_app['version'] = (4, 0, 0)
-    update_voting_app['objection_time'] = 5 * 60  # 5 min
-    update_voting_app['vote_time'] = 10 * 60  # 10 min
+from utils.permissions import encode_permission_grant, encode_permission_revoke
+# noinspection PyUnresolvedReferences
+from utils.brownie_prelude import *
 
 
-def unsafely_change_objection_time(voting, new_time):
-    return (
-        voting.address,
-        voting.unsafelyChangeObjectionPhaseTime.encode_input(new_time)
-    )
+def get_proposed_deposit_security_module_address():
+    if network_name() in ('goerli', 'goerli-fork'):
+        return '0x7DC1C1ff64078f73C98338e2f17D1996ffBb2eDe'
+    elif network_name() in ('mainnet', 'mainnet-fork'):
+        return '0x710B3303fB508a84F10793c1106e32bE873C24cd'
+    else:
+        assert False, f'Unsupported network "{network_name()}"'
 
 
-def unsafely_change_vote_time(voting, new_time):
-    return (
-        voting.address,
-        voting.unsafelyChangeVoteTime.encode_input(new_time)
-    )
+def get_last_deposit_block():
+    if network_name() in ('goerli', 'goerli-fork'):
+        return 123
+    elif network_name() in ('mainnet', 'mainnet-fork'):
+        return 456  # TODO
+    else:
+        assert False, f'Unsupported network "{network_name()}"'
+
+
+def encode_set_last_deposit_block(last_deposit_block: int) -> Tuple[str, str]:
+    deposit_security_module = interface.DepositSecurityModule(
+        get_proposed_deposit_security_module_address())
+    # deposit_security_module: interface.DepositSecurityModule = contracts.deposit_security_module
+
+    # return agent_forward([(
+    #     deposit_security_module.address,
+    #     deposit_security_module.unpauseDeposits.encode_input()
+    # )])
+
+    return agent_forward([(
+    # return (
+        deposit_security_module.address,
+        deposit_security_module.setLastDepositBlock.encode_input(last_deposit_block)
+    # )
+    )])
 
 
 def start_vote(
     tx_params: Dict[str, str],
-    silent: bool = False
+    silent: bool = False,
 ) -> Tuple[int, Optional[TransactionReceipt]]:
     """Prepare and run voting."""
+    lido: interface.Lido = contracts.lido
 
-    voting: interface.Voting = contracts.voting
+    proposed_deposit_security_module_address = get_proposed_deposit_security_module_address()
+    last_deposit_block = get_last_deposit_block()
 
     encoded_call_script = encode_call_script([
-        # 1. Push new voting app version to Voting Repo 0x41D65FA420bBC714686E798a0eB0Df3799cEF092
-        add_implementation_to_voting_app_repo(
-            update_voting_app['version'],
-            update_voting_app['new_address'],
-            update_voting_app['content_uri']
-        ),
-        # 2. Upgrade the DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e contract implementation
-        update_app_implementation(
-            update_voting_app['id'],
-            update_voting_app['new_address']
-        ),
-        # 3. Grant `UNSAFELY_MODIFY_VOTE_TIME_ROLE` to DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e
-        encode_permission_grant(
-            target_app=voting,
-            permission_name='UNSAFELY_MODIFY_VOTE_TIME_ROLE',
-            grant_to=voting
-        ),
-        # 4. Update objection duration with `unsafelyChangeObjectionTime()` to 24 hours
-        unsafely_change_objection_time(voting, update_voting_app['objection_time']),
-        unsafely_change_vote_time(voting, update_voting_app['vote_time']),  # for goerli only
-        # 5. Revoke `UNSAFELY_MODIFY_VOTE_TIME_ROLE` from DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e
-        encode_permission_revoke(
-            target_app=voting,
-            permission_name='UNSAFELY_MODIFY_VOTE_TIME_ROLE',
-            revoke_from=voting
-        )
+        # 1. Revoke DEPOSIT_ROLE from the old DepositSecurityModule
+        encode_permission_revoke(target_app=lido, permission_name='DEPOSIT_ROLE',
+                                 revoke_from=lido_dao_deposit_security_module_address),
+
+        # 2. Grant DEPOSIT_ROLE to the new DepositSecurityModule
+        encode_permission_grant(target_app=lido, permission_name='DEPOSIT_ROLE',
+                                grant_to=proposed_deposit_security_module_address),
+
+        # 3. Set lastDepositBlock of DepositSecurityModule to ???
+        encode_set_last_deposit_block(last_deposit_block),
     ])
 
     return confirm_vote_script(encoded_call_script, silent) and create_vote(
         vote_desc=(
             'Omnibus vote: '
-            '1) Push new voting app version to Voting Repo; '
-            '2) Upgrade the DAO Voting contract implementation; '
-            '3) Grant `UNSAFELY_MODIFY_VOTE_TIME_ROLE` to DAO Voting; '
-            '4) Update objection phase duration with `unsafelyChangeObjectionTime` to 24 hours; '
-            '5) Revoke `UNSAFELY_MODIFY_VOTE_TIME_ROLE` from DAO Voting.'
+            '1) Revoke DEPOSIT_ROLE from the old DepositSecurityModule; ',
+            '2) Grant DEPOSIT_ROLE to the new DepositSecurityModule; ',
+            '3) Set lastDepositBlock of DepositSecurityModule to ???. ',
         ),
         evm_script=encoded_call_script,
         tx_params=tx_params
