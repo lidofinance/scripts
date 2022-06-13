0bf2c4f65669035fb920670828493c4e6512b6c7
Eugene Mamin <TheDZhon@gmail.com>
2022-06-13 12:25:32 +0000

    fix: update IPFS hashes and refactor voting a bit

--- scripts/vote_2022_06_14.py
+++ scripts/vote_2022_06_14.py
@@ -4,7 +4,7 @@ Voting 14/06/2022.
 1. Revoke DEPOSIT_ROLE from old DepositSecurityModule 0xDb149235B6F40dC08810AA69869783Be101790e7
 2. Grant DEPOSIT_ROLE to new DepositSecurityModule 0x710B3303fB508a84F10793c1106e32bE873C24cd
 3. Set lastDepositBlock of DepositSecurityModule to {{TODO block number}}
-4. Set Lido app IPFS hash to QmURb5WALQG8b2iWuGmyGaQ7kY5q5vd4oNK5ZVDLjRjj2m
+4. Set Lido app IPFS hash to QmRjCTdRbjkGUa7t6H2PnswGZyecnNSg8osk4kY2i82xUn
 
 """
 # noinspection PyUnresolvedReferences
@@ -16,7 +16,7 @@ from typing import (Dict, Tuple, Optional)
 from brownie.network.transaction import TransactionReceipt
 from brownie import web3
 
-from utils.voting import confirm_vote_script, create_vote
+from utils.voting import bake_vote_items, confirm_vote_script, create_vote
 from utils.evm_script import encode_call_script
 from utils.agent import agent_forward
 from utils.repo import add_implementation_to_lido_app_repo
@@ -52,12 +52,12 @@ def start_vote(
 
     lido_app_update_params = {
         'address': '0x47EbaB13B806773ec2A2d16873e2dF770D130b50',
-        'ipfsCid': 'QmURb5WALQG8b2iWuGmyGaQ7kY5q5vd4oNK5ZVDLjRjj2m',
-        'content_uri': '0x697066733a516d5552623557414c5147386232695775476d79476151376b593571357664346f4e4b355a56444c6a526a6a326d',
+        'ipfsCid': 'QmRjCTdRbjkGUa7t6H2PnswGZyecnNSg8osk4kY2i82xUn',
+        'content_uri': '0x697066733a516d526a43546452626a6b4755613774364832506e7377475a7965636e4e5367386f736b346b593269383278556e',
         'version': (4, 0, 0)
     }
 
-    encoded_call_script = encode_call_script([
+    call_script_items = [
         # 1. Revoke DEPOSIT_ROLE from the old DepositSecurityModule
         encode_permission_revoke(target_app=lido, permission_name='DEPOSIT_ROLE',
                                  revoke_from=current_deposit_security_module_address),
@@ -69,26 +69,23 @@ def start_vote(
         # 3. Set lastDepositBlock of DepositSecurityModule to {{TODO}}
         encode_set_last_deposit_block(proposed_deposit_security_module_address, last_deposit_block),
 
-        # 4. Set Lido app IPFS hash to QmcweCCxtTGubHuJVwDcTwikUevuvmAJJ7S5uoRicBxvxM
+        # 4. Set Lido app IPFS hash to QmRjCTdRbjkGUa7t6H2PnswGZyecnNSg8osk4kY2i82xUn
         add_implementation_to_lido_app_repo(
             lido_app_update_params['version'],
             lido_app_update_params['address'],
             lido_app_update_params['content_uri'],
-        ),
-    ])
+        )
+    ]
 
-    return confirm_vote_script(encoded_call_script, silent) and create_vote(
-        vote_desc=(
-            'Omnibus vote: '
-            '1) Revoke DEPOSIT_ROLE from the old DepositSecurityModule; ',
-            '2) Grant DEPOSIT_ROLE to the new DepositSecurityModule; ',
-            '3) Set lastDepositBlock of DepositSecurityModule to {{TBD}}; ',
-            '4) Set Lido app IPFS hash to QmURb5WALQG8b2iWuGmyGaQ7kY5q5vd4oNK5ZVDLjRjj2m. '
-        ),
-        evm_script=encoded_call_script,
-        tx_params=tx_params
-    )
+    vote_desc_items = [
+        '1) Revoke DEPOSIT_ROLE from the old DepositSecurityModule',
+        '2) Grant DEPOSIT_ROLE to the new DepositSecurityModule',
+        '3) Set lastDepositBlock of DepositSecurityModule to {{TBD}}',
+        '4) Set Lido app IPFS hash to QmRjCTdRbjkGUa7t6H2PnswGZyecnNSg8osk4kY2i82xUn'
+    ]
 
+    vote_items = bake_vote_items(vote_desc_items, call_script_items)
+    return confirm_vote_script(vote_items, silent) and create_vote(vote_items, tx_params)
 
 def main():
     tx_params = {'from': get_deployer_account()}
--- utils/voting.py
+++ utils/voting.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List, Dict
 
 from brownie import exceptions
 from brownie.utils import color
@@ -12,16 +12,43 @@ from utils.evm_script import (encode_call_script,
 
 from utils.config import (prompt_bool, chain_network, contracts, get_config_params)
 
+def bake_vote_items(
+    vote_desc_items: List[str],
+    call_script_items: List[Tuple[str, str]]
+) -> Dict[str, Tuple[str, str]]:
+    if not isinstance(call_script_items, list):
+        raise TypeError("callscript should be passed as a list of items")
+    if not isinstance(vote_desc_items, list):
+        raise TypeError("vote description has invalid format")
+    if len(vote_desc_items) != len(call_script_items):
+        raise ValueError(
+            f"vote desc and evm_script inconsistent vote items cnt: {len(vote_desc_items)} vs {len(call_script_items)}"
+        )
+    if len(vote_desc_items) > len(set(vote_desc_items)):
+        raise ValueError("vote desc contains repetitive items")
+    return dict(zip(vote_desc_items, call_script_items))
 
-def create_vote(vote_desc, evm_script, tx_params, verbose: bool = False) -> Tuple[int, Optional[TransactionReceipt]]:
+def create_vote(
+    vote_items: Dict[str, Tuple[str, str]],
+    tx_params: Dict[str, str],
+    verbose: bool = False
+) -> Tuple[int, Optional[TransactionReceipt]]:
+    vote_desc_str = ''
+    for v in vote_items.keys():
+        vote_desc_str += f'{v}; '
+    if len(vote_desc_str) > 0:
+        vote_desc_str = f'Omnibus vote: {vote_desc_str[:-2]}.'
+
     voting = contracts.voting
     token_manager = contracts.token_manager
+
+    evm_script = encode_call_script(vote_items.values())
 
     new_vote_script = encode_call_script([
         (voting.address,
          voting.newVote.encode_input(
              evm_script if evm_script is not None else EMPTY_CALLSCRIPT,
-             vote_desc, False, False))
+             vote_desc_str, False, False))
     ])
     tx = token_manager.forward(new_vote_script, tx_params)
     if tx.revert_msg is not None:
@@ -50,13 +77,16 @@ def create_vote(vote_desc, evm_script, tx_params, verbose: bool = False) -> Tupl
     return vote_id, tx
 
 
-def confirm_vote_script(encoded_call_script: str, silent: bool) -> bool:
+def confirm_vote_script(vote_items: Dict[str, Tuple[str, str]], silent: bool) -> bool:
+    encoded_call_script = encode_call_script(vote_items.values())
+
     human_readable_script = decode_evm_script(
         encoded_call_script, verbose=False, specific_net=chain_network, repeat_is_error=True
     )
 
     # Show detailed description of prepared voting.
     if not silent:
+        vote_descs = list(vote_items.keys())
         cfg_params = get_config_params()
 
         config_repr = 'All known addresses extracted from a config file:\n'
@@ -69,6 +99,7 @@ def confirm_vote_script(encoded_call_script: str, silent: bool) -> bool:
         total = len(human_readable_script)
         for ind, call in enumerate(human_readable_script):
             print(f'Point #{ind + 1}/{total}.')
+            print(f'Description: {color("green")}{vote_descs[ind]}.{color}')
             print(calls_info_pretty_print(call))
             print('---------------------------')
 
