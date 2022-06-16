634eb8318f6815f016feabfd621af173e8157634
Alexey Potapkin <apotapkin@demax.ru>
2022-06-16 17:16:32 +0300

    test: add Lido app updating to vote script

--- scripts/vote_2022_06_21.py
+++ scripts/vote_2022_06_21.py
@@ -1,11 +1,12 @@
 """
 Voting 21/06/2022.
 
-1. Push new voting app version to Voting Repo 0x41D65FA420bBC714686E798a0eB0Df3799cEF092.
-2. Upgrade the DAO Voting ##address TBA## contract implementation.
-3. Grant `UNSAFELY_MODIFY_VOTE_TIME_ROLE` to DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e.
-4. Update objection phase duration with `unsafelyChangeObjectionTime()` to 24 hours.
-5. Revoke `UNSAFELY_MODIFY_VOTE_TIME_ROLE` from DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e.
+1. Push new Voting app version to Voting Repo 0x41D65FA420bBC714686E798a0eB0Df3799cEF092
+2. Upgrade the Aragon Voting contract implementation 0x72fb5253AD16307B9E773d2A78CaC58E309d5Ba4
+3. Push new Lido app version to Lido Repo 0xF5Dc67E54FC96F993CD06073f71ca732C1E654B1
+4. Grant `UNSAFELY_MODIFY_VOTE_TIME_ROLE` to Aragon Voting 0x2e59A20f205bB85a89C53f1936454680651E618e
+5. Update objection phase duration with `unsafelyChangeObjectionTime()` to 24 hours
+6. Revoke `UNSAFELY_MODIFY_VOTE_TIME_ROLE` from Aragon Voting 0x2e59A20f205bB85a89C53f1936454680651E618e
 
 
 """
@@ -18,7 +19,10 @@ from brownie.network.transaction import TransactionReceipt
 
 from utils.voting import confirm_vote_script, create_vote, bake_vote_items
 from utils.permissions import encode_permission_grant, encode_permission_revoke
-from utils.repo import add_implementation_to_voting_app_repo
+from utils.repo import (
+    add_implementation_to_voting_app_repo,
+    add_implementation_to_lido_app_repo,
+)
 from utils.kernel import update_app_implementation
 from utils.config import (
     get_deployer_account,
@@ -27,12 +31,19 @@ from utils.config import (
 )
 
 update_voting_app = {
-    "new_address": "0x72fb5253AD16307B9E773d2A78CaC58E309d5Ba4",  # TBA
+    "new_address": "0x72fb5253AD16307B9E773d2A78CaC58E309d5Ba4",
     "content_uri": "0x697066733a516d514d64696979653134765966724a7753594250646e68656a446f62417877584b72524e45663438735370444d",
     # TBA
     "id": "0x0abcd104777321a82b010357f20887d61247493d89d2e987ff57bcecbde00e1e",
     "version": (3, 0, 0),
     "objection_time": 86_400,  # 24 hours
+}
+
+update_lido_app = {
+    "address": "0x47EbaB13B806773ec2A2d16873e2dF770D130b50",
+    "content_uri": "0x697066733a516d514d64696979653134765966724a7753594250646e68656a446f62417877584b72524e45663438735370444d",
+    # TODO: check
+    "version": (3, 1, 0),
 }
 
 
@@ -56,32 +67,39 @@ def start_vote(
 
     vote_items = bake_vote_items(
         vote_desc_items=[
-            "1) Push new voting app version to Voting Repo",
+            "1) Push new Voting app version to Voting Repo",
             "2) Upgrade the DAO Voting contract implementation",
+            "3) Push new Lido app version to Lido Repo"
             "3) Grant `UNSAFELY_MODIFY_VOTE_TIME_ROLE` to DAO Voting",
             "4) Update objection phase duration with `unsafelyChangeObjectionTime` to 24 hours",
             "5) Revoke `UNSAFELY_MODIFY_VOTE_TIME_ROLE` from DAO Voting",
         ],
         call_script_items=[
-            # 1. Push new voting app version to Voting Repo 0x41D65FA420bBC714686E798a0eB0Df3799cEF092
+            # 1. Push new Voting app version to Voting Repo 0x41D65FA420bBC714686E798a0eB0Df3799cEF092
             add_implementation_to_voting_app_repo(
                 update_voting_app["version"],
                 update_voting_app["new_address"],
                 update_voting_app["content_uri"],
             ),
-            # 2. Upgrade the DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e contract implementation
+            # 2. Upgrade the Aragon Voting contract implementation 0x72fb5253AD16307B9E773d2A78CaC58E309d5Ba4.
             update_app_implementation(
                 update_voting_app["id"], update_voting_app["new_address"]
             ),
-            # 3. Grant `UNSAFELY_MODIFY_VOTE_TIME_ROLE` to DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e
+            # 3. Push new Lido app version to Lido Repo 0xF5Dc67E54FC96F993CD06073f71ca732C1E654B1
+            add_implementation_to_lido_app_repo(
+                update_lido_app["version"],
+                update_lido_app["address"],
+                update_lido_app["content_uri"],
+            ),
+            # 4. Grant `UNSAFELY_MODIFY_VOTE_TIME_ROLE` to Aragon Voting 0x2e59A20f205bB85a89C53f1936454680651E618e
             encode_permission_grant(
                 target_app=voting,
                 permission_name="UNSAFELY_MODIFY_VOTE_TIME_ROLE",
                 grant_to=voting,
             ),
-            # 4. Update objection duration with `unsafelyChangeObjectionTime()` to 24 hours
+            # 5. Update objection duration with `unsafelyChangeObjectionTime()` to 24 hours
             unsafely_change_objection_time(voting, update_voting_app["objection_time"]),
-            # 5. Revoke `UNSAFELY_MODIFY_VOTE_TIME_ROLE` from DAO Voting 0x2e59A20f205bB85a89C53f1936454680651E618e
+            # 6. Revoke `UNSAFELY_MODIFY_VOTE_TIME_ROLE` from Aragon Voting 0x2e59A20f205bB85a89C53f1936454680651E618e
             encode_permission_revoke(
                 target_app=voting,
                 permission_name="UNSAFELY_MODIFY_VOTE_TIME_ROLE",
