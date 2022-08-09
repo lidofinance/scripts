b2de35b965fd406edb95bff7fad3cd2b4a15e5ea
Eugene Mamin <TheDZhon@gmail.com>
2022-08-09 15:13:52 +0000

    fix: update payout amount to 700 ETH
    The exchange rate is changing.

--- scripts/vote_2022_08_09.py
+++ scripts/vote_2022_08_09.py
@@ -9,7 +9,7 @@ Voting 09/08/2022.
 
 import time
 
-from typing import (Dict, Tuple, Optional)
+from typing import Dict, Tuple, Optional
 
 from brownie.network.transaction import TransactionReceipt
 
@@ -23,9 +23,9 @@ from utils.config import (
 from utils.finance import make_ldo_payout, make_weth_payout
 from utils.brownie_prelude import *
 
-rcc_multisig_address = '0xDE06d17Db9295Fa8c4082D4f73Ff81592A3aC437'
-eth_amount: int = 690 * (10 ** 18)  # 1_117_380 * 1.05 / 1700
-ldo_amount: int = 67_017.32 * (10 ** 18)
+rcc_multisig_address = "0xDE06d17Db9295Fa8c4082D4f73Ff81592A3aC437"
+eth_amount: int = 700 * (10**18)  # 1_117_380 * 1.05 / 1700
+ldo_amount: int = 67_017.32 * (10**18)
 
 
 def encode_weth_wrap_agent_execute_call(agent, weth_token):
@@ -38,10 +38,7 @@ def encode_weth_wrap_agent_execute_call(agent, weth_token):
     )
 
 
-def start_vote(
-    tx_params: Dict[str, str],
-    silent: bool = False
-) -> Tuple[int, Optional[TransactionReceipt]]:
+def start_vote(tx_params: Dict[str, str], silent: bool = False) -> Tuple[int, Optional[TransactionReceipt]]:
     """Prepare and run voting."""
     agent = interface.Agent(lido_dao_agent_address)
     weth_token = interface.WethToken(weth_token_address)
@@ -49,20 +46,14 @@ def start_vote(
     call_script_items = [
         # 1. Send $1,117,380.00 +5% in ETH to the RCC multisig 0xDE06d17Db9295Fa8c4082D4f73Ff81592A3aC437
         encode_weth_wrap_agent_execute_call(agent, weth_token),
-
         # 2. Send $1,117,380.00 +5% in WETH to the RCC multisig 0xDE06d17Db9295Fa8c4082D4f73Ff81592A3aC437
         make_weth_payout(
-            target_address=rcc_multisig_address,
-            weth_in_wei=eth_amount,
-            reference="RCC Multisig Jul-Sep Payout"
+            target_address=rcc_multisig_address, weth_in_wei=eth_amount, reference="RCC Multisig Jul-Sep Payout"
         ),
-
         # 3. Send 67,017.32 LDO to the RCC multisig 0xDE06d17Db9295Fa8c4082D4f73Ff81592A3aC437
         make_ldo_payout(
-            target_address=rcc_multisig_address,
-            ldo_in_wei=ldo_amount,
-            reference="RCC Multisig Jul-Sep Payout"
-        )
+            target_address=rcc_multisig_address, ldo_in_wei=ldo_amount, reference="RCC Multisig Jul-Sep Payout"
+        ),
     ]
 
     # NB: In case of single vote item the ending period is added automatically
@@ -77,14 +68,14 @@ def start_vote(
 
 
 def main():
-    tx_params = {'from': get_deployer_account()}
+    tx_params = {"from": get_deployer_account()}
 
     if get_is_live():
-        tx_params['max_fee'] = '300 gwei'
-        tx_params['priority_fee'] = '2 gwei'
+        tx_params["max_fee"] = "300 gwei"
+        tx_params["priority_fee"] = "2 gwei"
 
     vote_id, _ = start_vote(tx_params=tx_params)
 
-    vote_id >= 0 and print(f'Vote created: {vote_id}.')
+    vote_id >= 0 and print(f"Vote created: {vote_id}.")
 
     time.sleep(5)  # hack for waiting thread #2.
--- tests/test_2022_08_09.py
+++ tests/test_2022_08_09.py
@@ -3,41 +3,50 @@ Tests for voting 09/08/2022.
 """
 from scripts.vote_2022_08_09 import start_vote
 from utils.test.tx_tracing_helpers import *
-from utils.test.event_validators.payout import (Payout, validate_token_payout_event,
-    validate_agent_execute_ether_payout_to_gnosis_event)
+from utils.test.event_validators.payout import (
+    Payout,
+    validate_token_payout_event,
+    validate_agent_execute_ether_payout_to_gnosis_event,
+)
 from utils.finance import ZERO_ADDRESS
 from brownie.network.transaction import TransactionReceipt
 
 
-dao_agent_address = '0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c'
-lido_dao_token = '0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32'
+dao_agent_address = "0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c"
+lido_dao_token = "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32"
 
-rcc_multisig_address = '0xDE06d17Db9295Fa8c4082D4f73Ff81592A3aC437'
+rcc_multisig_address = "0xDE06d17Db9295Fa8c4082D4f73Ff81592A3aC437"
 
 rcc_ldo_payout = Payout(
-    token_addr=lido_dao_token,
-    from_addr=dao_agent_address,
-    to_addr=rcc_multisig_address,
-    amount=67_017.32 * (10 ** 18)
+    token_addr=lido_dao_token, from_addr=dao_agent_address, to_addr=rcc_multisig_address, amount=67_017.32 * (10**18)
 )
 
 rcc_eth_payout = Payout(
-    token_addr=ZERO_ADDRESS,
-    from_addr=dao_agent_address,
-    to_addr=rcc_multisig_address,
-    amount=690 * (10 ** 18)
+    token_addr=ZERO_ADDRESS, from_addr=dao_agent_address, to_addr=rcc_multisig_address, amount=700 * (10**18)
 )
 
 
-def test_vote(helpers, accounts, ldo_holder, dao_voting, vote_id_from_env, bypass_events_decoding, ldo_token, dao_agent, weth_token):
+def test_vote(
+    helpers,
+    accounts,
+    ldo_holder,
+    dao_voting,
+    vote_id_from_env,
+    bypass_events_decoding,
+    ldo_token,
+    dao_agent,
+    weth_token,
+):
     rcc_multisig = accounts.at(rcc_multisig_address, force=True)
 
     rcc_eth_before = rcc_multisig.balance()
     agent_eth_before = dao_agent.balance()
-    rcc_ldo_before = ldo_token.balanceOf(rcc_multisig_address)
-    agent_ldo_before = ldo_token.balanceOf(dao_agent.address)
+
     rcc_weth_before = weth_token.balanceOf(rcc_multisig_address)
     agent_weth_before = weth_token.balanceOf(dao_agent.address)
+
+    rcc_ldo_before = ldo_token.balanceOf(rcc_multisig_address)
+    agent_ldo_before = ldo_token.balanceOf(dao_agent.address)
 
     # START VOTE
     vote_id: int = vote_id_from_env or start_vote({"from": ldo_holder}, silent=True)[0]
@@ -50,20 +59,22 @@ def test_vote(helpers, accounts, ldo_holder, dao_voting, vote_id_from_env, bypas
     if not bypass_events_decoding:
         assert count_vote_items_by_events(tx, dao_voting) == 3, "Incorrect voting items count"
 
-    assert rcc_multisig.balance() == rcc_eth_before, \
-        "RCC multisig ETH balance must remain the same"
-    assert dao_agent.balance() == agent_eth_before - rcc_eth_payout.amount, \
-      "Agent ETH balance must decrease by the correct amount"
+    assert rcc_multisig.balance() == rcc_eth_before, "RCC multisig ETH balance must remain the same"
+    assert (
+        dao_agent.balance() == agent_eth_before - rcc_eth_payout.amount
+    ), "Agent ETH balance must decrease by the correct amount"
 
-    assert weth_token.balanceOf(rcc_multisig_address) == rcc_weth_before + rcc_eth_payout.amount, \
-        "Incorrect WETH amount on RCC multisig"
-    assert weth_token.balanceOf(dao_agent.address) == agent_weth_before, \
-        "Incorrect WETH amount on DAO Agent"
+    assert (
+        weth_token.balanceOf(rcc_multisig_address) == rcc_weth_before + rcc_eth_payout.amount
+    ), "Incorrect WETH amount on RCC multisig"
+    assert weth_token.balanceOf(dao_agent.address) == agent_weth_before, "Incorrect WETH amount on DAO Agent"
 
-    assert ldo_token.balanceOf(rcc_multisig_address) == rcc_ldo_before + rcc_ldo_payout.amount, \
-        "Incorrect LDO amount on RCC multisig"
-    assert ldo_token.balanceOf(dao_agent.address) == agent_ldo_before - rcc_ldo_payout.amount, \
-        "Incorrect LDO amount on DAO Agent"
+    assert (
+        ldo_token.balanceOf(rcc_multisig_address) == rcc_ldo_before + rcc_ldo_payout.amount
+    ), "Incorrect LDO amount on RCC multisig"
+    assert (
+        ldo_token.balanceOf(dao_agent.address) == agent_ldo_before - rcc_ldo_payout.amount
+    ), "Incorrect LDO amount on DAO Agent"
 
     # Check events if their decoding is available
     if bypass_events_decoding:
