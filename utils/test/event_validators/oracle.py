e3c03b519a2163cb8340119ded5b5e15dfa190b3
Eugene Mamin <TheDZhon@gmail.com>
2023-01-09 17:00:22 +0300

    fix: add missing event validators for oracle

--- utils/test/event_validators/oracle.py
+++ utils/test/event_validators/oracle.py
@@ -10,3 +10,23 @@ def validate_beacon_report_receiver_set_event(event: EventDict, callback: str):
     assert event.count('BeaconReportReceiverSet') == 1
 
     assert event['BeaconReportReceiverSet']['callback'] == callback
+
+
+def validate_oracle_member_added(event: EventDict, new_member: str):
+    _events_chain = ["LogScriptCall", "MemberAdded"]
+
+    validate_events_chain([e.name for e in event], _events_chain)
+
+    assert event.count("MemberAdded") == 1
+
+    assert event["MemberAdded"]["member"] == new_member
+
+
+def validate_oracle_quorum_changed(event: EventDict, new_quorum: int):
+    _events_chain = ["LogScriptCall", "QuorumChanged"]
+
+    validate_events_chain([e.name for e in event], _events_chain)
+
+    assert event.count("QuorumChanged") == 1
+
+    assert event["QuorumChanged"]["quorum"] == new_quorum
