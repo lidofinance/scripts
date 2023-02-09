b7eca039d1f3e4c0c876f2be8df433af102999eb
Victor Suzdalev <victor.suzdalev@gmail.com>
2023-02-09 14:59:52 +0000

    feat: read from csv

--- scripts/prepare_calldata.py
+++ scripts/prepare_calldata.py
@@ -1,20 +1,17 @@
+import csv
 from brownie import Contract
 
-relay_list = [
-  {'uri':'https://0x9000009807ed12c1f08bf4e81c6da3ba8e3fc3d953898ce0102433094e5f22f21102ec057841fcb81978ed1ea0fa8246@builder-relay-mainnet.blocknative.com', 'operator': 'Blocknative', 'description': 'Blocknative Relay', 'is_mandatory': True},
-  {'uri':'https://0xb0b07cd0abef743db4260b0ed50619cf6ad4d82064cb4fbec9d3ec530f7c5e6793d9f286c4e082c0244ffb9f2658fe88@bloxroute.regulated.blxrbdn.com', 'operator': 'bloXroute', 'description': 'bloXroute Regulated Relay', 'is_mandatory':True},
-  {'uri':'https://0xad0a8bb54565c2211cee576363f3a347089d2f07cf72679d16911d740262694cadb62d7fd7483f27afd714ca0f1b9118@bloxroute.ethical.blxrbdn.com', 'operator': 'bloXroute', 'description': 'bloXroute Ethical Relay', 'is_mandatory':True},
-  {'uri':'https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com', 'operator': 'bloXroute', 'description': 'bloXroute Max-Profit Relay', 'is_mandatory':True},
-  {'uri':'https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net', 'operator': 'Flashbots', 'description': 'Flashbots Relay', 'is_mandatory':True},
-  {'uri':'https://0xb3ee7afcf27f1f1259ac1787876318c6584ee353097a50ed84f51a1f21a323b3736f271a895c7ce918c038e4265918be@relay.edennetwork.io', 'operator': 'Eden Network', 'description': 'Eden Network Relay', 'is_mandatory':True},
-  {'uri':'https://0x98650451ba02064f7b000f5768cf0cf4d4e492317d82871bdc87ef841a0743f69f0f1eea11168503240ac35d101c9135@mainnet-relay.securerpc.com/', 'operator': 'Manifold Finance', 'description': 'Manifold SecureRPC Relay', 'is_mandatory':False},
-  {'uri':'https://0xa1559ace749633b997cb3fdacffb890aeebdb0f5a3b6aaa7eeeaf1a38af0a8fe88b9e4b1f61f236d2e64d95733327a62@relay.ultrasound.money', 'operator': 'Ultra Sound', 'description': 'Ultra Sound Relay', 'is_mandatory':False},
-  {'uri':'https://0xa7ab7a996c8584251c8f925da3170bdfd6ebc75d50f5ddc4050a6fdc77f2a3b5fce2cc750d0865e05d7228af97d69561@agnostic-relay.net', 'operator': 'Agnostic', 'description': 'Agnostic Relay', 'is_mandatory':False},
-  {'uri':'https://0xa15b52576bcbf1072f4a011c0f99f9fb6c66f3e1ff321f11f461d15e31b1cb359caa092c71bbded0bae5b5ea401aab7e@aestus.live', 'operator': 'Aestus', 'description': 'Aestus Relay', 'is_mandatory':False},
-]
+def read_csv_data(filename):
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"', skipinitialspace=True)
+        return list(reader)
 
-relay_list_contract = Contract.from_explorer('0xF95f069F9AD107938F6ba802a3da87892298610E')
+def read_csv_purchasers(filename):
+    data = [ {'operator':item[1], 'uri':item[2], 'description': item[3],'is_mandatory': bool(item[4]) } for item in read_csv_data(filename)]
+    return data[2:]
 
 def main():
+    relay_list = read_csv_purchasers("RMC Onchain Tx #1 - Sheet1.csv")
+    relay_list_contract = Contract.from_explorer('0xF95f069F9AD107938F6ba802a3da87892298610E')
     for relay in relay_list:
         print(relay_list_contract.add_relay.encode_input(relay['uri'], relay['operator'], relay['is_mandatory'], relay['description']))
