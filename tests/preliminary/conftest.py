b7c3917af2b5f13ce47a701223d84a6312eb79fe
Artyom Veremeenko <arwer13@gmail.com>
2023-04-11 11:40:58 +0400

    fix(preliminary tests): correct conftest preparation fixture

--- tests/preliminary/conftest.py
+++ tests/preliminary/conftest.py
@@ -12,6 +12,5 @@ from utils.shapella_upgrade import prepare_for_shapella_upgrade_voting
 
 @pytest.fixture(scope="module")
 def shapella_upgrade_template():
-    if ContractsLazyLoader.upgrade_template == None:
-        ContractsLazyLoader.upgrade_template = prepare_for_shapella_upgrade_voting(deployer_eoa, silent=True)
-    return contracts.shapella_upgrade_template
+    upgrade_template = prepare_for_shapella_upgrade_voting(deployer_eoa, silent=True)
+    return upgrade_template
