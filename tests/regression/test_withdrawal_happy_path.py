c6f7857a9a258f7feb9a7287d1312f1fedcf6297
Evgeny Taktarov <je.mouth@gmail.com>
2023-04-20 23:09:01 +0700

    feat: move tests to regression

--- tests/regression/test_withdrawal_happy_path.py
+++ tests/regression/test_withdrawal_happy_path.py
@@ -1,108 +1,49 @@
-import math
-
-import pytest
-from brownie import accounts, web3  # type: ignore
+from brownie import accounts  # type: ignore
 from utils.test.oracle_report_helpers import (
-    ONE_DAY,
-    SHARE_RATE_PRECISION,
-    push_oracle_report,
-    get_finalization_batches,
-    simulate_report,
-    wait_to_next_available_report_time,
+    oracle_report,
 )
+
+from utils.test.helpers import almostEqEth, steth_balance, ETH
 
 from utils.config import (
     contracts,
 )
 
 
-def ETH(amount):
-    return math.floor(amount * 10**18)
-
-
-def SHARES(amount):
-    return ETH(amount)
-
-
-def steth_balance(account):
-    return contracts.lido.balanceOf(account)
-
-
-def eth_balance(account):
-    return web3.eth.getBalance(account)
-
-
-def almostEqEth(b1, b2):
-    return abs(b1 - b2) <= 10
-
-
-@pytest.fixture(scope="module")
-def holder(accounts):
-    whale = "0x41318419CFa25396b47A94896FfA2C77c6434040"
-    contracts.lido.transfer(accounts[0], ETH(101), {"from": whale})
-    return accounts[0]
-
-
-def oracle_report():
-    wait_to_next_available_report_time()
-
-    (refSlot, _) = contracts.hash_consensus_for_accounting_oracle.getCurrentFrame()
-    elRewardsVaultBalance = eth_balance(contracts.execution_layer_rewards_vault.address)
-    withdrawalVaultBalance = eth_balance(contracts.withdrawal_vault.address)
-    (coverShares, nonCoverShares) = contracts.burner.getSharesRequestedToBurn()
-    (_, beaconValidators, beaconBalance) = contracts.lido.getBeaconStat()
-
-    postCLBalance = beaconBalance + ETH(20)
-
-    (postTotalPooledEther, postTotalShares, withdrawals, elRewards) = simulate_report(
-        refSlot=refSlot,
-        beaconValidators=beaconValidators,
-        postCLBalance=postCLBalance,
-        withdrawalVaultBalance=withdrawalVaultBalance,
-        elRewardsVaultBalance=elRewardsVaultBalance,
-    )
-    simulatedShareRate = postTotalPooledEther * SHARE_RATE_PRECISION // postTotalShares
-    sharesRequestedToBurn = coverShares + nonCoverShares
-
-    finalization_batches = get_finalization_batches(simulatedShareRate, withdrawals, elRewards)
-
-    push_oracle_report(
-        refSlot=refSlot,
-        clBalance=postCLBalance,
-        numValidators=beaconValidators,
-        withdrawalVaultBalance=withdrawalVaultBalance,
-        sharesRequestedToBurn=sharesRequestedToBurn,
-        withdrawalFinalizationBatches=finalization_batches,
-        elRewardsVaultBalance=elRewardsVaultBalance,
-        simulatedShareRate=simulatedShareRate,
-    )
-
-
-def test_withdraw(holder):
-    account = accounts.at(holder, force=True)
+def test_withdraw(steth_holder):
+    account = accounts.at(steth_holder, force=True)
     REQUESTS_COUNT = 10
     REQUEST_AMOUNT = ETH(1)
     REQUESTS_SUM = REQUESTS_COUNT * REQUEST_AMOUNT
 
     # pre request
 
-    no_requests = contracts.withdrawal_queue.getWithdrawalRequests(holder, {"from": holder})
+    no_requests = contracts.withdrawal_queue.getWithdrawalRequests(steth_holder, {"from": steth_holder})
     assert len(no_requests) == 0
 
     # request
-    contracts.lido.approve(contracts.withdrawal_queue.address, REQUESTS_SUM, {"from": holder})
-    steth_balance_before = steth_balance(holder)
+    contracts.lido.approve(contracts.withdrawal_queue.address, REQUESTS_SUM, {"from": steth_holder})
+    steth_balance_before = steth_balance(steth_holder)
     request_tx = contracts.withdrawal_queue.requestWithdrawals(
-        [REQUEST_AMOUNT for _ in range(REQUESTS_COUNT)], holder, {"from": holder}
+        [REQUEST_AMOUNT for _ in range(REQUESTS_COUNT)], steth_holder, {"from": steth_holder}
     )
-    steth_balance_after = steth_balance(holder)
+    steth_balance_after = steth_balance(steth_holder)
+
     # post request checks
     assert request_tx.events.count("WithdrawalRequested") == REQUESTS_COUNT
+    for i, event in enumerate(request_tx.events["WithdrawalRequested"]):
+        (requestId, requestor, owner, amountOfStETH, amountOfShares) = event
+        assert requestId == i + 1
+        assert almostEqEth(amountOfStETH, REQUEST_AMOUNT)
+        assert almostEqEth(amountOfShares, contracts.lido.getSharesByPooledEth(amountOfStETH))
+        assert owner == steth_holder
+        assert requestor == steth_holder
+
     assert almostEqEth(steth_balance_before - steth_balance_after, REQUESTS_SUM)
 
     # check each request
-    requests_ids = contracts.withdrawal_queue.getWithdrawalRequests(holder, {"from": holder})
-    statuses = contracts.withdrawal_queue.getWithdrawalStatus(requests_ids, {"from": holder})
+    requests_ids = contracts.withdrawal_queue.getWithdrawalRequests(steth_holder, {"from": steth_holder})
+    statuses = contracts.withdrawal_queue.getWithdrawalStatus(requests_ids, {"from": steth_holder})
     claimableEther = contracts.withdrawal_queue.getClaimableEther(requests_ids, [0 for _ in requests_ids])
     assert len(requests_ids) == REQUESTS_COUNT
     assert len(statuses) == REQUESTS_COUNT
@@ -112,7 +53,7 @@ def test_withdraw(holder):
         (amountOfStETH, amountOfShares, owner, _, isFinalized, isClaimed) = statuses[i]
         assert almostEqEth(amountOfStETH, REQUEST_AMOUNT)
         assert almostEqEth(amountOfShares, contracts.lido.getSharesByPooledEth(amountOfStETH))
-        assert owner == holder
+        assert owner == steth_holder
         assert not isFinalized
         assert not isClaimed
         assert claimableEther[i] == 0
@@ -120,8 +61,12 @@ def test_withdraw(holder):
     pre_lastCheckpointIndex = contracts.withdrawal_queue.getLastCheckpointIndex()
     assert pre_lastCheckpointIndex == 0
 
+    # first oracle report, requests might not get finalized due to sanity check requestTimestampMargin
     oracle_report()
-    # post report WQ state
+    # second report requests will get finalized for sure
+    oracle_report()
+
+    # post reports WQ state
     assert contracts.withdrawal_queue.getLastFinalizedRequestId() == requests_ids[-1]
     lastCheckpointIndex = contracts.withdrawal_queue.getLastCheckpointIndex()
     assert lastCheckpointIndex == 1
@@ -131,7 +76,7 @@ def test_withdraw(holder):
     hints = contracts.withdrawal_queue.findCheckpointHints(requests_ids, 1, lastCheckpointIndex)
     assert len(hints) == REQUESTS_COUNT
 
-    post_report_statuses = contracts.withdrawal_queue.getWithdrawalStatus(requests_ids, {"from": holder})
+    post_report_statuses = contracts.withdrawal_queue.getWithdrawalStatus(requests_ids, {"from": steth_holder})
     post_report_claimableEther = contracts.withdrawal_queue.getClaimableEther(requests_ids, hints)
 
     for i, request_id in enumerate(requests_ids):
@@ -139,7 +84,7 @@ def test_withdraw(holder):
         (amountOfStETH, amountOfShares, owner, _, isFinalized, isClaimed) = post_report_statuses[i]
         assert amountOfStETH == statuses[i][0]  # amountOfShares remains unchanged
         assert amountOfShares == statuses[i][1]  # amountOfShares remains unchanged
-        assert owner == holder
+        assert owner == steth_holder
         assert isFinalized
         assert not isClaimed
         assert almostEqEth(post_report_claimableEther[i], REQUEST_AMOUNT)
@@ -148,14 +93,14 @@ def test_withdraw(holder):
 
     # claim
     claim_balance_before = account.balance()
-    claim_tx = contracts.withdrawal_queue.claimWithdrawals(requests_ids, hints, {"from": holder})
+    claim_tx = contracts.withdrawal_queue.claimWithdrawals(requests_ids, hints, {"from": steth_holder})
     assert claim_tx.events.count("WithdrawalClaimed") == REQUESTS_COUNT
     claim_balance_after = account.balance()
     assert almostEqEth(
         claim_balance_after - claim_balance_before + claim_tx.gas_used * claim_tx.gas_price, REQUESTS_SUM
     )
 
-    post_claim_statuses = contracts.withdrawal_queue.getWithdrawalStatus(requests_ids, {"from": holder})
+    post_claim_statuses = contracts.withdrawal_queue.getWithdrawalStatus(requests_ids, {"from": steth_holder})
     post_claim_claimableEther = contracts.withdrawal_queue.getClaimableEther(requests_ids, hints)
 
     for i, request_id in enumerate(requests_ids):
@@ -163,7 +108,7 @@ def test_withdraw(holder):
         (amountOfStETH, amountOfShares, owner, _, isFinalized, isClaimed) = post_claim_statuses[i]
         assert amountOfStETH == statuses[i][0]  # amountOfShares remains unchanged
         assert amountOfShares == statuses[i][1]  # amountOfShares remains unchanged
-        assert owner == holder
+        assert owner == steth_holder
         assert isFinalized
         assert isClaimed
         assert almostEqEth(post_report_claimableEther[i], REQUEST_AMOUNT)
