893c313da47000ce56e51a261c2fb30df9a64c78
Evgeny Taktarov <je.mouth@gmail.com>
2023-04-24 12:47:05 +0700

    fix: naming and pause asserts

--- tests/regression/test_gate_seal.py
+++ tests/regression/test_gate_seal.py
@@ -13,7 +13,7 @@ from utils.config import (
 )
 
 
-DURATION_SECONDS = 6 * 24 * 60 * 60  # 6 days
+PAUSE_DURATION_SECONDS = 6 * 24 * 60 * 60  # 6 days
 EXPIRY_TIMESTAMP = 1713139200  # 2024-04-15 00:00GMT
 
 
@@ -24,7 +24,7 @@ def shared_setup(fn_isolation):
 
 def test_gate_seal_configuration():
     assert contracts.gate_seal.get_sealing_committee() == (deployer_eoa)
-    assert contracts.gate_seal.get_seal_duration_seconds() == DURATION_SECONDS
+    assert contracts.gate_seal.get_seal_duration_seconds() == PAUSE_DURATION_SECONDS
     sealables = contracts.gate_seal.get_sealables()
     assert len(sealables) == 2
     assert lido_dao_withdrawal_queue in sealables
@@ -33,7 +33,7 @@ def test_gate_seal_configuration():
     assert not contracts.gate_seal.is_expired()
 
 
-def test_gate_expiration():
+def test_gate_seal_expiration():
     assert not contracts.gate_seal.is_expired()
     time = chain.time()
     chain.sleep(EXPIRY_TIMESTAMP - time)
@@ -85,7 +85,7 @@ def test_gate_seal_scenario(steth_holder):
     assert seal_tx.events.count("Sealed") == 2
     for i, seal_event in enumerate(seal_tx.events["Sealed"]):
         assert seal_event["gate_seal"] == gate_seal_address
-        assert seal_event["sealed_for"] == DURATION_SECONDS
+        assert seal_event["sealed_for"] == PAUSE_DURATION_SECONDS
         assert seal_event["sealed_by"] == deployer_eoa
         assert seal_event["sealable"] == sealables[i]
         assert seal_event["sealed_at"] == seal_tx.timestamp
@@ -93,17 +93,17 @@ def test_gate_seal_scenario(steth_holder):
     # brownie for some reason fails to decode second event
     # assert seal_tx.events.count("Paused") == 2
     for pause_event in seal_tx.events["Paused"]:
-        assert pause_event["duration"] == DURATION_SECONDS
+        assert pause_event["duration"] == PAUSE_DURATION_SECONDS
 
     assert contracts.gate_seal.is_expired()
     with reverts("gate seal: expired"):
         seal_tx = contracts.gate_seal.seal(sealables, {"from": deployer_eoa})
 
     assert contracts.withdrawal_queue.isPaused()
-    assert contracts.withdrawal_queue.getResumeSinceTimestamp() == seal_tx.timestamp + DURATION_SECONDS
+    assert contracts.withdrawal_queue.getResumeSinceTimestamp() == seal_tx.timestamp + PAUSE_DURATION_SECONDS
 
     assert contracts.validators_exit_bus_oracle.isPaused()
-    assert contracts.validators_exit_bus_oracle.getResumeSinceTimestamp() == seal_tx.timestamp + DURATION_SECONDS
+    assert contracts.validators_exit_bus_oracle.getResumeSinceTimestamp() == seal_tx.timestamp + PAUSE_DURATION_SECONDS
 
     # reverts on requestWithdrawals
     contracts.lido.approve(contracts.withdrawal_queue.address, REQUESTS_SUM, {"from": steth_holder})
@@ -121,11 +121,17 @@ def test_gate_seal_scenario(steth_holder):
         contracts.withdrawal_queue.finalize(1, 1, {"from": steth_holder})
 
     """ claim """
+    assert contracts.withdrawal_queue.isPaused()
+    assert contracts.validators_exit_bus_oracle.isPaused()
+
     lastCheckpointIndex = contracts.withdrawal_queue.getLastCheckpointIndex()
     hints = contracts.withdrawal_queue.findCheckpointHints(claimable_request_ids, 1, lastCheckpointIndex)
     claim_balance_before = account.balance()
     claim_tx = contracts.withdrawal_queue.claimWithdrawals(claimable_request_ids, hints, {"from": steth_holder})
     claim_balance_after = account.balance()
+
+    assert contracts.withdrawal_queue.isPaused()
+    assert contracts.validators_exit_bus_oracle.isPaused()
 
     assert almostEqEth(
         claim_balance_after - claim_balance_before + claim_tx.gas_used * claim_tx.gas_price, REQUESTS_SUM
@@ -141,17 +147,20 @@ def test_gate_seal_scenario(steth_holder):
         EPOCHS_PER_FRAME,
         _,
     ) = get_time_config()
-    MAX_REPORTS_UNTIL_RESUME = DURATION_SECONDS // (SECONDS_PER_SLOT * SLOTS_PER_EPOCH * EPOCHS_PER_FRAME) + 2
+    MAX_REPORTS_UNTIL_RESUME = PAUSE_DURATION_SECONDS // (SECONDS_PER_SLOT * SLOTS_PER_EPOCH * EPOCHS_PER_FRAME) + 2
     reports_passed = 0
     while True:
         (report_tx, _) = oracle_report(silent=True)
         reports_passed += 1
         print(
-            f"Oracle report {reports_passed} at {report_tx.timestamp}/{seal_tx.timestamp + DURATION_SECONDS} seconds to resume"
+            f"Oracle report {reports_passed} at {report_tx.timestamp}/{seal_tx.timestamp + PAUSE_DURATION_SECONDS} seconds to resume"
         )
         if report_tx.events.count("WithdrawalsFinalized") == 1:
             break
         assert reports_passed <= MAX_REPORTS_UNTIL_RESUME
+
+    assert not contracts.withdrawal_queue.isPaused()
+    assert not contracts.validators_exit_bus_oracle.isPaused()
 
     """ post seal claim """
     lastCheckpointIndex = contracts.withdrawal_queue.getLastCheckpointIndex()
