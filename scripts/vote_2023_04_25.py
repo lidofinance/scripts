e46343f601c1153000fad73295576da74fc64906
zuzueeka <ekzueva1907@gmail.com>
2023-04-25 15:19:05 +0300

    change items order

--- scripts/vote_2023_04_25.py
+++ scripts/vote_2023_04_25.py
@@ -1,11 +1,11 @@
 """
 Voting 25/04/2023.
 
-1. Change the on-chain name of node operator with id 1 from 'Certus One' to 'jumpcrypto'
-2. Change the on-chain name of node operator with id 21 from 'ConsenSys Codefi' to 'Consensys'
-3. Change the on-chain name of node operator with id 8 from 'SkillZ' to 'Kiln'
-4. Change the reward address of node operator with id 8 from 0xe080E860741b7f9e8369b61645E68AD197B1e74C to 0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690
-5. Increase Easy Track motions amount limit: set motionsCountLimit to 20
+1. Increase Easy Track motions amount limit: set motionsCountLimit to 20
+2. Change the on-chain name of node operator with id 1 from 'Certus One' to 'jumpcrypto'
+3. Change the on-chain name of node operator with id 21 from 'ConsenSys Codefi' to 'Consensys'
+4. Change the on-chain name of node operator with id 8 from 'SkillZ' to 'Kiln'
+5. Change the reward address of node operator with id 8 from 0xe080E860741b7f9e8369b61645E68AD197B1e74C to 0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690
 """
 
 import time
@@ -33,6 +33,8 @@ from utils.easy_track import (
 def start_vote(tx_params: Dict[str, str], silent: bool = False) -> Tuple[int, Optional[TransactionReceipt]]:
     """Prepare and run voting."""
 
+    motionsCountLimit = 20
+
     NO_registry = interface.NodeOperatorsRegistry(lido_dao_node_operators_registry)
 
     CertusOne_Jumpcrypto_id = 1
@@ -45,27 +47,25 @@ def start_vote(tx_params: Dict[str, str], silent: bool = False) -> Tuple[int, Op
     SkillZ_Kiln_new_name = "Kiln"
     SkillZ_Kiln_new_address = "0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690"
 
-    motionsCountLimit = 20
-
     call_script_items = [
-        # 1. Change the on-chain name of node operator with id 1 from 'Certus One' to 'jumpcrypto'
-        encode_set_node_operator_name(CertusOne_Jumpcrypto_id, CertusOne_Jumpcrypto_new_name, NO_registry),
-        # 2. Change the on-chain name of node operator with id 21 from 'ConsenSys Codefi' to 'Consensys'
-        encode_set_node_operator_name(ConsenSysCodefi_Consensys_id, ConsenSysCodefi_Consensys_new_name, NO_registry),
-        # 3. Change the on-chain name of node operator with id 8 from 'SkillZ' to 'Kiln'
-        encode_set_node_operator_name(SkillZ_Kiln_id, SkillZ_Kiln_new_name, NO_registry),
-        # 4. Change the reward address of node operator with id 8 from 0xe080E860741b7f9e8369b61645E68AD197B1e74C to 0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690
-        encode_set_node_operator_reward_address(SkillZ_Kiln_id, SkillZ_Kiln_new_address, NO_registry),
-        #5. Increase Easy Track motions amount limit: set motionsCountLimit to 20
+        # 1. Increase Easy Track motions amount limit: set motionsCountLimit to 20
         set_motions_count_limit(motionsCountLimit)
+        # 2. Change the on-chain name of node operator with id 1 from 'Certus One' to 'jumpcrypto'
+        encode_set_node_operator_name(CertusOne_Jumpcrypto_id, CertusOne_Jumpcrypto_new_name, NO_registry),
+        # 3. Change the on-chain name of node operator with id 21 from 'ConsenSys Codefi' to 'Consensys'
+        encode_set_node_operator_name(ConsenSysCodefi_Consensys_id, ConsenSysCodefi_Consensys_new_name, NO_registry),
+        # 4. Change the on-chain name of node operator with id 8 from 'SkillZ' to 'Kiln'
+        encode_set_node_operator_name(SkillZ_Kiln_id, SkillZ_Kiln_new_name, NO_registry),
+        # 5. Change the reward address of node operator with id 8 from 0xe080E860741b7f9e8369b61645E68AD197B1e74C to 0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690
+        encode_set_node_operator_reward_address(SkillZ_Kiln_id, SkillZ_Kiln_new_address, NO_registry),
     ]
 
     vote_desc_items = [
-        "1) Change the on-chain name of node operator with id 1 from 'Certus One' to 'jumpcrypto'",
-        "2) Change the on-chain name of node operator with id 21 from 'ConsenSys Codefi' to 'Consensys'",
-        "3) Change the on-chain name of node operator with id 8 from 'SkillZ' to 'Kiln'",
-        "4) Change the reward address of node operator with id 8 from 0xe080E860741b7f9e8369b61645E68AD197B1e74C to 0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690",
-        "5) Increase Easy Track motions amount limit: set motionsCountLimit to 20",
+        "1) Increase Easy Track motions amount limit: set motionsCountLimit to 20",
+        "2) Change the on-chain name of node operator with id 1 from 'Certus One' to 'jumpcrypto'",
+        "3) Change the on-chain name of node operator with id 21 from 'ConsenSys Codefi' to 'Consensys'",
+        "4) Change the on-chain name of node operator with id 8 from 'SkillZ' to 'Kiln'",
+        "5) Change the reward address of node operator with id 8 from 0xe080E860741b7f9e8369b61645E68AD197B1e74C to 0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690",
     ]
 
     vote_items = bake_vote_items(vote_desc_items, call_script_items)
