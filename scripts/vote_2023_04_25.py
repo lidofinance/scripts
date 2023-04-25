711e3544d101743ad0f6ef8b342c4a38f78eb519
zuzueeka <ekzueva1907@gmail.com>
2023-04-25 15:16:35 +0300

    add names for indexes

--- scripts/vote_2023_04_25.py
+++ scripts/vote_2023_04_25.py
@@ -36,26 +36,26 @@ def start_vote(tx_params: Dict[str, str], silent: bool = False) -> Tuple[int, Op
     NO_registry = interface.NodeOperatorsRegistry(lido_dao_node_operators_registry)
 
     CertusOne_Jumpcrypto_id = 1
-    CertusOne_Jumpcrypto_name = "jumpcrypto"
+    CertusOne_Jumpcrypto_new_name = "jumpcrypto"
 
     ConsenSysCodefi_Consensys_id = 21
-    ConsenSysCodefi_Consensys_name = "Consensys"
+    ConsenSysCodefi_Consensys_new_name = "Consensys"
 
     SkillZ_Kiln_id = 8
-    SkillZ_Kiln_name = "Kiln"
-    SkillZ_Kiln_address = "0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690"
+    SkillZ_Kiln_new_name = "Kiln"
+    SkillZ_Kiln_new_address = "0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690"
 
     motionsCountLimit = 20
 
     call_script_items = [
         # 1. Change the on-chain name of node operator with id 1 from 'Certus One' to 'jumpcrypto'
-        encode_set_node_operator_name(CertusOne_Jumpcrypto_id, CertusOne_Jumpcrypto_name, NO_registry),
+        encode_set_node_operator_name(CertusOne_Jumpcrypto_id, CertusOne_Jumpcrypto_new_name, NO_registry),
         # 2. Change the on-chain name of node operator with id 21 from 'ConsenSys Codefi' to 'Consensys'
-        encode_set_node_operator_name(ConsenSysCodefi_Consensys_id, ConsenSysCodefi_Consensys_name, NO_registry),
+        encode_set_node_operator_name(ConsenSysCodefi_Consensys_id, ConsenSysCodefi_Consensys_new_name, NO_registry),
         # 3. Change the on-chain name of node operator with id 8 from 'SkillZ' to 'Kiln'
-        encode_set_node_operator_name(SkillZ_Kiln_id, SkillZ_Kiln_name, NO_registry),
+        encode_set_node_operator_name(SkillZ_Kiln_id, SkillZ_Kiln_new_name, NO_registry),
         # 4. Change the reward address of node operator with id 8 from 0xe080E860741b7f9e8369b61645E68AD197B1e74C to 0xD6B7d52E15678B9195F12F3a6D6cb79dcDcCb690
-        encode_set_node_operator_reward_address(SkillZ_Kiln_id, SkillZ_Kiln_address, NO_registry),
+        encode_set_node_operator_reward_address(SkillZ_Kiln_id, SkillZ_Kiln_new_address, NO_registry),
         #5. Increase Easy Track motions amount limit: set motionsCountLimit to 20
         set_motions_count_limit(motionsCountLimit)
     ]
