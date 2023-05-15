94b09f3c6c346327e0f8510405e32b7a9b9067d0
Eugene Mamin <TheDZhon@gmail.com>
2023-05-16 00:37:41 +0300

    fix: revive acceptance tests

---
+++ tests/acceptance/test_locator.py
@@ -0,0 +1,51 @@
+import pytest
+from brownie import interface  # type: ignore
+
+from utils.config import contracts, LIDO_LOCATOR, LIDO_LOCATOR_IMPL
+
+
+@pytest.fixture(scope="module")
+def contract() -> interface.LidoLocator:
+    return interface.LidoLocator(LIDO_LOCATOR)
+
+
+def test_proxy(contract):
+    proxy = interface.OssifiableProxy(contract)
+    assert proxy.proxy__getImplementation() == LIDO_LOCATOR_IMPL
+    assert proxy.proxy__getAdmin() == contracts.agent.address
+
+
+def test_addresses(contract):
+    assert contract.accountingOracle() == contracts.accounting_oracle
+    assert contract.depositSecurityModule() == contracts.deposit_security_module
+    assert contract.elRewardsVault() == contracts.execution_layer_rewards_vault
+    assert contract.legacyOracle() == contracts.legacy_oracle
+    assert contract.lido() == contracts.lido
+    assert contract.oracleReportSanityChecker() == contracts.oracle_report_sanity_checker
+    assert contract.postTokenRebaseReceiver() == contracts.legacy_oracle
+    assert contract.burner() == contracts.burner
+    assert contract.stakingRouter() == contracts.staking_router
+    assert contract.treasury() == contracts.agent
+    assert contract.validatorsExitBusOracle() == contracts.validators_exit_bus_oracle
+    assert contract.withdrawalQueue() == contracts.withdrawal_queue
+    assert contract.withdrawalVault() == contracts.withdrawal_vault
+    assert contract.oracleDaemonConfig() == contracts.oracle_daemon_config
+
+    assert contract.coreComponents() == (
+        contracts.execution_layer_rewards_vault,
+        contracts.oracle_report_sanity_checker,
+        contracts.staking_router,
+        contracts.agent,
+        contracts.withdrawal_queue,
+        contracts.withdrawal_vault,
+    )
+
+    assert contract.oracleReportComponentsForLido() == (
+        contracts.accounting_oracle,
+        contracts.execution_layer_rewards_vault,
+        contracts.oracle_report_sanity_checker,
+        contracts.burner,
+        contracts.withdrawal_queue,
+        contracts.withdrawal_vault,
+        contracts.legacy_oracle,
+    )
---
+++ tests/acceptance/test_node_operators_registry.py
@@ -0,0 +1,124 @@
+import pytest
+from brownie import ZERO_ADDRESS, interface, web3, reverts  # type: ignore
+
+from utils.config import (
+    contracts,
+    NODE_OPERATORS_REGISTRY,
+    NODE_OPERATORS_REGISTRY_IMPL,
+    NODE_OPERATORS_REGISTRY_ARAGON_APP_ID,
+    CURATED_STAKING_MODULE_STUCK_PENALTY_DELAY,
+    CURATED_STAKING_MODULE_OPERATORS_COUNT,
+    CURATED_STAKING_MODULE_OPERATORS_ACTIVE_COUNT,
+    CURATED_STAKING_MODULE_TYPE,
+)
+
+
+@pytest.fixture(scope="module")
+def contract() -> interface.NodeOperatorsRegistry:
+    return interface.NodeOperatorsRegistry(NODE_OPERATORS_REGISTRY)
+
+
+def test_links(contract):
+    assert contract.getLocator() == contracts.lido_locator
+
+
+def test_aragon(contract):
+    proxy = interface.AppProxyUpgradeable(contract)
+    assert proxy.implementation() == NODE_OPERATORS_REGISTRY_IMPL
+    assert contract.kernel() == contracts.kernel
+    assert contract.appId() == NODE_OPERATORS_REGISTRY_ARAGON_APP_ID
+    assert contract.hasInitialized() == True
+    assert contract.isPetrified() == False
+
+
+def test_role_keccaks(contract):
+    assert contract.MANAGE_SIGNING_KEYS() == web3.keccak(text="MANAGE_SIGNING_KEYS").hex()
+    assert contract.SET_NODE_OPERATOR_LIMIT_ROLE() == web3.keccak(text="SET_NODE_OPERATOR_LIMIT_ROLE").hex()
+    assert contract.MANAGE_NODE_OPERATOR_ROLE() == web3.keccak(text="MANAGE_NODE_OPERATOR_ROLE").hex()
+    assert contract.STAKING_ROUTER_ROLE() == web3.keccak(text="STAKING_ROUTER_ROLE").hex()
+
+
+def test_versioned(contract):
+    assert contract.getContractVersion() == 2
+
+
+def test_initialize(contract):
+    with reverts("INIT_ALREADY_INITIALIZED"):
+        contract.initialize(
+            contracts.lido_locator,
+            CURATED_STAKING_MODULE_TYPE,
+            CURATED_STAKING_MODULE_STUCK_PENALTY_DELAY,
+            {"from": contracts.voting},
+        )
+
+
+def test_finalize_upgrade(contract):
+    with reverts("UNEXPECTED_CONTRACT_VERSION"):
+        contract.finalizeUpgrade_v2(
+            contracts.lido_locator,
+            CURATED_STAKING_MODULE_TYPE,
+            CURATED_STAKING_MODULE_STUCK_PENALTY_DELAY,
+            {"from": contracts.voting},
+        )
+
+
+def test_petrified():
+    contract = interface.NodeOperatorsRegistry(NODE_OPERATORS_REGISTRY_IMPL)
+    with reverts("INIT_ALREADY_INITIALIZED"):
+        contract.initialize(
+            contracts.lido_locator,
+            CURATED_STAKING_MODULE_TYPE,
+            CURATED_STAKING_MODULE_STUCK_PENALTY_DELAY,
+            {"from": contracts.voting},
+        )
+
+    with reverts("CONTRACT_NOT_INITIALIZED"):
+        contract.finalizeUpgrade_v2(
+            contracts.lido_locator,
+            CURATED_STAKING_MODULE_TYPE,
+            CURATED_STAKING_MODULE_STUCK_PENALTY_DELAY,
+            {"from": contracts.voting},
+        )
+
+
+def test_nor_state(contract):
+    node_operators_count = contract.getNodeOperatorsCount()
+    assert node_operators_count == CURATED_STAKING_MODULE_OPERATORS_COUNT
+    assert contract.getActiveNodeOperatorsCount() == CURATED_STAKING_MODULE_OPERATORS_ACTIVE_COUNT
+    assert contract.getNonce() >= 7315
+    assert contract.getStuckPenaltyDelay() == CURATED_STAKING_MODULE_STUCK_PENALTY_DELAY
+    assert contract.getType() == _str_to_bytes32("curated-onchain-v1")
+
+    summary = contract.getStakingModuleSummary()
+    assert summary["totalExitedValidators"] == 0
+    assert summary["totalDepositedValidators"] >= 177397
+    assert summary["depositableValidatorsCount"] > 0
+
+    for id in range(node_operators_count):
+        assert contract.getTotalSigningKeyCount(id) > 0
+        node_operator = contract.getNodeOperator(id, True)
+
+        assert node_operator["active"] == True
+        assert node_operator["name"] is not None
+        assert node_operator["name"] != ""
+        assert node_operator["rewardAddress"] != ZERO_ADDRESS
+        assert node_operator["totalVettedValidators"] > 0
+        assert node_operator["totalVettedValidators"] <= node_operator["totalAddedValidators"]
+        assert node_operator["totalExitedValidators"] == 0
+        assert node_operator["totalAddedValidators"] > 0
+        assert node_operator["totalDepositedValidators"] > 0
+        assert node_operator["totalDepositedValidators"] <= node_operator["totalAddedValidators"]
+
+        node_operator_summary = contract.getNodeOperatorSummary(id)
+        assert node_operator_summary["isTargetLimitActive"] is False
+        assert node_operator_summary["targetValidatorsCount"] == 0
+        assert node_operator_summary["stuckValidatorsCount"] == 0
+        assert node_operator_summary["refundedValidatorsCount"] == 0
+        assert node_operator_summary["stuckPenaltyEndTimestamp"] == 0
+        assert node_operator_summary["totalExitedValidators"] == 0
+        assert node_operator_summary["totalDepositedValidators"] > 0
+        assert node_operator_summary["depositableValidatorsCount"] is not None
+
+
+def _str_to_bytes32(s: str) -> str:
+    return "0x{:0<64}".format(s.encode("utf-8").hex())
