1d82098d6be3146ae377cdac2fb2b4da421f9c97
Eugene Mamin <TheDZhon@gmail.com>
2023-05-16 12:52:34 +0300

    feat: post-report tests scaffolds

--- tests/acceptance/conftest.py
+++ tests/acceptance/conftest.py
@@ -1,8 +1,22 @@
 import pytest
 
+from brownie import chain, ZERO_ADDRESS
+
+from typing import NewType, Tuple
 from utils.config import contracts
+from utils.test.oracle_report_helpers import ONE_DAY, SHARE_RATE_PRECISION, oracle_report
 from utils.import_current_votes import is_there_any_vote_scripts, start_and_execute_votes
 
+from utils.test.extra_data import (
+    ExtraDataService,
+)
+
+StakingModuleId = NewType("StakingModuleId", int)
+NodeOperatorId = NewType("NodeOperatorId", int)
+NodeOperatorGlobalIndex = Tuple[StakingModuleId, NodeOperatorId]
+
+def node_operator_gindex(module_id, node_operator_id) -> NodeOperatorGlobalIndex:
+    return module_id, node_operator_id
 
 @pytest.fixture(scope="module", autouse=True)
 def shared_setup(module_isolation):
@@ -15,3 +29,26 @@ def autoexecute_vote(helpers, vote_ids_from_env, accounts):
         helpers.execute_votes(accounts, vote_ids_from_env, contracts.voting, topup="0.5 ether")
     else:
         start_and_execute_votes(contracts.voting, helpers)
+
+        print('Sending first report')
+
+        # Prepare extra data
+        vals_stuck_non_zero = {}
+        vals_exited_non_zero = {
+            node_operator_gindex(1, 22): 11,
+        }
+        extra_data = ExtraDataService().collect(vals_stuck_non_zero, vals_exited_non_zero, 11, 11)
+
+        # Second report - first NO and second NO has stuck/exited
+        (report_tx, extra_report_tx) = oracle_report(
+            exclude_vaults_balances=False,
+            extraDataFormat=1,
+            extraDataHash=extra_data.data_hash,
+            extraDataItemsCount=1,
+            extraDataList=extra_data.extra_data,
+            numExitedValidatorsByStakingModule=[11],
+            stakingModuleIdsWithNewlyExitedValidators=[1],
+        )
+
+        chain.sleep(ONE_DAY // 24)
+        chain.mine()
