7c2521f6a220164b4a7cb916806fac38aa80cc47
zuzueeka <ekzueva1907@gmail.com>
2023-05-22 22:05:31 +0200

    add script and tests for 1-13 items

--- scripts/vote_2023_05_23.py
+++ scripts/vote_2023_05_23.py
@@ -2,10 +2,22 @@
 Voting 23/05/2023.
 
 1. Increase Easy Track motions amount limit: set motionsCountLimit to 20
+
 2. Add reWARDS program top up EVM script factory for stETH 0x1F2b79FE297B7098875930bBA6dd17068103897E
 3. Add reWARDS program add recipient EVM script factory for stETH 0x935cb3366Faf2cFC415B2099d1F974Fd27202b77
 4. Add reWARDS program remove recipient EVM script factory for stETH 0x22010d1747CaFc370b1f1FBBa61022A313c5693b
 
+5. Remove reWARDS program top up EVM script factory for LDO 0x85d703B2A4BaD713b596c647badac9A1e95bB03d
+6. Remove reWARDS program add recipient EVM script factory for LDO 0x1dCFc37719A99d73a0ce25CeEcbeFbF39938cF2C
+7. Remove reWARDS program remove recipient EVM script factory for LDO 0x00BB68a12180a8f7E20D8422ba9F81c07A19A79E
+
+8. Remove referral program top up EVM script factory for LDO 0x54058ee0E0c87Ad813C002262cD75B98A7F59218 from Easy Track
+9. Remove referral program add recipient EVM script factory for LDO Track 0x929547490Ceb6AeEdD7d72F1Ab8957c0210b6E51 from Easy Track
+10. Remove referral program remove recipient EVM script factory for LDO 0xE9eb838fb3A288bF59E9275Ccd7e124fDff88a9C  from Easy Track
+
+11. Remove referral program top up EVM script factory for DAI 0x009ffa22ce4388d2F5De128Ca8E6fD229A312450 from Easy Track
+12. Remove referral program add recipient EVM script factory for DAI  0x8F06a7f244F6Bb4B68Cd6dB05213042bFc0d7151 from Easy Track
+13. Remove referral program remove recipient EVM script factory for DAI  • 0xd8f9B72Cd97388f23814ECF429cd18815F6352c1 from Easy Track
 """
 
 import time
@@ -50,6 +62,21 @@ def start_vote(tx_params: Dict[str, str], silent: bool) -> Tuple[int, Optional[T
     reWARDS_stETH_add_recipient_factory = interface.AddAllowedRecipient("0x935cb3366Faf2cFC415B2099d1F974Fd27202b77")
     reWARDS_stETH_remove_recipient_factory = interface.RemoveAllowedRecipient("0x22010d1747CaFc370b1f1FBBa61022A313c5693b")
 
+    # 5-7 Remove reWARDS LDO setup from Easy Track
+    reWARDS_LDO_topup_factory = interface.TopUpAllowedRecipients("0x85d703B2A4BaD713b596c647badac9A1e95bB03d")
+    reWARDS_LDO_add_recipient_factory = interface.AddAllowedRecipient("0x1dCFc37719A99d73a0ce25CeEcbeFbF39938cF2C")
+    reWARDS_LDO_remove_recipient_factory = interface.RemoveAllowedRecipient("0x00BB68a12180a8f7E20D8422ba9F81c07A19A79E")
+
+    # 8-10 Remove LDO referral program from Easy Track
+    referral_program_LDO_topup_factory = interface.TopUpAllowedRecipients("0x54058ee0E0c87Ad813C002262cD75B98A7F59218")
+    referral_program_LDO_add_recipient_factory = interface.AddAllowedRecipient("0x929547490Ceb6AeEdD7d72F1Ab8957c0210b6E51")
+    referral_program_LDO_remove_recipient_factory = interface.RemoveAllowedRecipient("0xE9eb838fb3A288bF59E9275Ccd7e124fDff88a9C")
+
+    # 11-13 Remove LDO referral program from Easy Track
+    referral_program_DAI_topup_factory = interface.TopUpAllowedRecipients("0x009ffa22ce4388d2F5De128Ca8E6fD229A312450")
+    referral_program_DAI_add_recipient_factory = interface.AddAllowedRecipient("0x8F06a7f244F6Bb4B68Cd6dB05213042bFc0d7151")
+    referral_program_DAI_remove_recipient_factory = interface.RemoveAllowedRecipient("0xd8f9B72Cd97388f23814ECF429cd18815F6352c1")
+
 
     call_script_items = [
         # 1.
@@ -70,6 +97,24 @@ def start_vote(tx_params: Dict[str, str], silent: bool) -> Tuple[int, Optional[T
             factory=reWARDS_stETH_remove_recipient_factory,
             permissions=create_permissions(reWARDS_stETH_registry, "removeRecipient"),
         ),
+        # 5.
+        remove_evmscript_factory(factory=reWARDS_LDO_topup_factory),
+        # 6.
+        remove_evmscript_factory(factory=reWARDS_LDO_add_recipient_factory),
+        # 7.
+        remove_evmscript_factory(factory=reWARDS_LDO_remove_recipient_factory),
+        # 8.
+        remove_evmscript_factory(factory=referral_program_LDO_topup_factory),
+        # 9.
+        remove_evmscript_factory(factory=referral_program_LDO_add_recipient_factory),
+        # 10.
+        remove_evmscript_factory(factory=referral_program_LDO_remove_recipient_factory),
+        # 11.
+        remove_evmscript_factory(factory=referral_program_DAI_topup_factory),
+        # 12.
+        remove_evmscript_factory(factory=referral_program_DAI_add_recipient_factory),
+        # 13.
+        remove_evmscript_factory(factory=referral_program_DAI_remove_recipient_factory),
 
     ]
 
@@ -78,6 +123,16 @@ def start_vote(tx_params: Dict[str, str], silent: bool) -> Tuple[int, Optional[T
         "2) Add reWARDS program top up EVM script factory for stETH 0x1F2b79FE297B7098875930bBA6dd17068103897E",
         "3) Add reWARDS program add recipient EVM script factory for stETH 0x935cb3366Faf2cFC415B2099d1F974Fd27202b77",
         "4) Add reWARDS program remove recipient EVM script factory for stETH 0x22010d1747CaFc370b1f1FBBa61022A313c5693b",
+        "5) Remove reWARDS program top up EVM script factory for LDO 0x85d703B2A4BaD713b596c647badac9A1e95bB03d",
+        "6) Remove reWARDS program add recipient EVM script factory for LDO 0x1dCFc37719A99d73a0ce25CeEcbeFbF39938cF2C",
+        "7) Remove reWARDS program remove recipient EVM script factory for LDO 0x00BB68a12180a8f7E20D8422ba9F81c07A19A79E",
+        "8) Remove referral program top up EVM script factory for LDO 0x54058ee0E0c87Ad813C002262cD75B98A7F59218 from Easy Track",
+        "9) Remove referral program add recipient EVM script factory for LDO Track 0x929547490Ceb6AeEdD7d72F1Ab8957c0210b6E51 from Easy Track",
+        "10) Remove referral program remove recipient EVM script factory for LDO 0xE9eb838fb3A288bF59E9275Ccd7e124fDff88a9C  from Easy Track",
+        "11) Remove referral program top up EVM script factory for DAI 0x009ffa22ce4388d2F5De128Ca8E6fD229A312450 from Easy Track",
+        "12) Remove referral program add recipient EVM script factory for DAI  0x8F06a7f244F6Bb4B68Cd6dB05213042bFc0d7151 from Easy Track",
+        "13) Remove referral program remove recipient EVM script factory for DAI  • 0xd8f9B72Cd97388f23814ECF429cd18815F6352c1 from Easy Track",
+
     ]
 
     vote_items = bake_vote_items(vote_desc_items, call_script_items)
