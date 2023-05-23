371efb66ac1912426b932c415e89a04e36ee96cd
zuzueeka <ekzueva1907@gmail.com>
2023-05-23 18:07:45 +0200

    tweaks for NO happy path test

--- tests/regression/test_node-operators-registry-happy-path.py
+++ tests/regression/test_node-operators-registry-happy-path.py
@@ -117,12 +117,10 @@ def test_node_operators(nor, extra_data_service, impersonated_voting, eth_whale)
     tested_no_id_second = 28
     base_no_id = 23
 
-    nor.setNodeOperatorStakingLimit(tested_no_id_first, 10000, {"from": impersonated_voting})
-    nor.setNodeOperatorStakingLimit(tested_no_id_second, 10000, {"from": impersonated_voting})
-    nor.setNodeOperatorStakingLimit(base_no_id, 10000, {"from": impersonated_voting})
-
-    for op_index in range(29):
-        nor.setNodeOperatorStakingLimit(op_index, 0, {"from": impersonated_voting})
+    NO_amount = nor.getNodeOperatorsCount()
+    for op_index in range(NO_amount):
+        no = nor.getNodeOperator(op_index, True)
+        nor.setNodeOperatorStakingLimit(op_index, no["totalDepositedValidators"], {"from": impersonated_voting})
 
     increase_limit(nor, tested_no_id_first, tested_no_id_second, base_no_id, 3, impersonated_voting)
 
