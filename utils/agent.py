d29e351fcd231473cfcf4aa7153880e3f8270b32
Alexandr Tarelkin <alexandrtarelkin92@gmail.com>
2023-06-19 23:01:40 +0300

    stake agents eth

--- utils/agent.py
+++ utils/agent.py
@@ -12,3 +12,9 @@ from typing import (
 def agent_forward(call_script: Sequence[Tuple[str, str]]) -> Tuple[str, str]:
     agent = contracts.agent
     return (AGENT, agent.forward.encode_input(encode_call_script(call_script)))
+
+
+
+def agent_execute(target: str, value: str, data: str) -> Tuple[str, str]:
+    agent = contracts.agent
+    return (AGENT, agent.execute.encode_input(target, value, data))
