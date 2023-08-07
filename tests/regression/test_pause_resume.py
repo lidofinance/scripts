f3fb66c473102bf4a6bab2d2ab26119686b3fb37
Alexandr Tarelkin <alexandrtarelkin92@gmail.com>
2023-08-07 13:19:02 +0300

    fix regression test

--- tests/regression/test_pause_resume.py
+++ tests/regression/test_pause_resume.py
@@ -295,12 +295,12 @@ def test_paused_staking_module_can_reward():
     shares_before = contracts.lido.sharesOf(module_address)
     (report_tx, _) = oracle_report()
     print(report_tx.events["Transfer"])
-    assert report_tx.events["Transfer"][0]["to"] == module_address
-    assert report_tx.events["Transfer"][0]["from"] == ZERO_ADDRESS
-    assert report_tx.events["Transfer"][1]["to"] == contracts.agent
+    assert report_tx.events["Transfer"][1]["to"] == module_address
     assert report_tx.events["Transfer"][1]["from"] == ZERO_ADDRESS
-    assert almostEqEth(report_tx.events["Transfer"][0]["value"], report_tx.events["Transfer"][1]["value"])
-    assert report_tx.events["Transfer"][0]["value"] > 0
+    assert report_tx.events["Transfer"][2]["to"] == contracts.agent
+    assert report_tx.events["Transfer"][2]["from"] == ZERO_ADDRESS
+    assert almostEqEth(report_tx.events["Transfer"][1]["value"], report_tx.events["Transfer"][1]["value"])
+    assert report_tx.events["Transfer"][1]["value"] > 0
 
 
 def test_stopped_staking_module_cant_stake(stranger):
