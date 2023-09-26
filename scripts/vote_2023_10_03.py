6afb74e35d4eda6f19f2fdeeaf08a1e19c75cc04
Alexander Belokon <ikatamarinaki@gmail.com>
2023-09-26 17:44:52 +0200

    wip: vote_2023_10_03

--- scripts/vote_2023_10_03.py
+++ scripts/vote_2023_10_03.py
@@ -1,7 +1,16 @@
 """
 Voting 03/10/2023.
 
-1. Update Anchor Vault implementation from 0x07BE9BB2B1789b8F5B2f9345F18378A8B036A171 to #TBA
+1. Add node operator 'A41' with reward address `0x2A64944eBFaFF8b6A0d07B222D3d83ac29c241a7`
+2. Add node operator TBA with reward address TBA
+3. Add node operator TBA with reward address TBA
+4. Add node operator TBA with reward address TBA
+5. Add node operator TBA with reward address TBA
+6. Add node operator TBA with reward address TBA
+7. Add node operator TBA with reward address TBA
+8. Grant STAKING_MODULE_MANAGE_ROLE to Lido Agent
+9. Set Jump Crypto targetValidatorsLimits to 0
+10. Update Anchor Vault implementation from `0x07BE9BB2B1789b8F5B2f9345F18378A8B036A171` to TBA
 
 Vote passed & executed on #, block #
 """
@@ -13,6 +22,7 @@ from typing import Dict, Tuple, Optional
 from brownie.network.transaction import TransactionReceipt
 from utils.voting import bake_vote_items, confirm_vote_script, create_vote
 from utils.agent import agent_forward
+from utils.node_operators import encode_add_operator_lido
 
 from utils.config import (
     get_deployer_account,
@@ -21,15 +31,57 @@ from utils.config import (
     get_priority_fee,
 )
 
-ANCHOR_NEW_IMPL_ADDRESS = "#TBA"
 
 def start_vote(tx_params: Dict[str, str], silent: bool = False) -> Tuple[int, Optional[TransactionReceipt]]:
+    """Prepare and run voting."""
+
+    # Vote specific addresses and constants:
+    # Add node operator named A41
+    a41_node_operator = {
+        "name": "A41",
+        "address": "0x2A64944eBFaFF8b6A0d07B222D3d83ac29c241a7",
+    }
+    # 6 more
+
+    # web3.keccak(text="STAKING_MODULE_MANAGE_ROLE")
+    STAKING_MODULE_MANAGE_ROLE = "0x3105bcbf19d4417b73ae0e58d508a65ecf75665e46c2622d8521732de6080c48"
+
+    # contracts.node_operators_registry.getNodeOperator(1, True)
+    JUMP_CRYPTO_ID = 1
+
+    ANCHOR_NEW_IMPL_ADDRESS = "#TBA"
 
     # anchor vault finalize
     setup_calldata = contracts.anchor_vault.finalize_upgrade_v4.encode_input()
 
     call_script_items = [
-        # 1. Update Anchor Vault implementation
+        # I. Lido on ETH NOs onboarding (wave 5 st2)
+        ## 1. Add node operator named A41
+        encode_add_operator_lido(**a41_node_operator),
+        ## 6 more
+        # II. Support Jump Crypto voluntarily exits from the validator set
+        ## 8. Grant STAKING_MODULE_MANAGE_ROLE to Lido Agent
+        agent_forward(
+            [
+                (
+                    contracts.staking_router.address,
+                    contracts.staking_router.grantRole.encode_input(
+                        STAKING_MODULE_MANAGE_ROLE, contracts.agent.address
+                    ),
+                )
+            ]
+        ),
+        ## 9. Set Jump Crypto targetValidatorsCount to 0
+        agent_forward(
+            [
+                (
+                    contracts.staking_router.address,
+                    contracts.staking_router.updateTargetValidatorsLimits.encode_input(1, JUMP_CRYPTO_ID, True, 0),
+                )
+            ]
+        ),
+        # III. Anchor sunset
+        ## 10. Update Anchor Vault implementation
         agent_forward(
             [
                 (
@@ -37,16 +89,20 @@ def start_vote(tx_params: Dict[str, str], silent: bool = False) -> Tuple[int, Op
                     contracts.anchor_vault_proxy.proxy_upgradeTo.encode_input(ANCHOR_NEW_IMPL_ADDRESS, setup_calldata),
                 )
             ]
-        )
-        ,
+        ),
     ]
 
     vote_desc_items = [
-        "1) Update Anchor Vault implementation from 0x07BE9BB2B1789b8F5B2f9345F18378A8B036A171 to #TBA",
+        "1) Add A41 node operator",
+        # 6 more
+        "8) Grant STAKING_MODULE_MANAGE_ROLE to Lido Agent",
+        "9) Set Jump Crypto targetValidatorsLimits to 0",
+        "10) Update Anchor Vault implementation from 0x07BE9BB2B1789b8F5B2f9345F18378A8B036A171 to #TBA",
     ]
 
     vote_items = bake_vote_items(vote_desc_items, call_script_items)
     return confirm_vote_script(vote_items, silent) and list(create_vote(vote_items, tx_params))
+
 
 def main():
     tx_params = {"from": get_deployer_account()}
--- tests/test_2023_10_03.py
+++ tests/test_2023_10_03.py
@@ -10,14 +10,30 @@ from utils.test.tx_tracing_helpers import (
     group_voting_events,
 )
 
-from utils.config import (
-    contracts,
-    LDO_HOLDER_ADDRESS_FOR_TESTS
+from utils.config import contracts, LDO_HOLDER_ADDRESS_FOR_TESTS, network_name
+from utils.voting import find_metadata_by_vote_id
+from utils.ipfs import get_lido_vote_cid_from_str
+from utils.test.event_validators.node_operators_registry import (
+    NodeOperatorItem,
+    TargetValidatorsCountChanged,
+    validate_node_operator_added_event,
+    validate_target_validators_count_changed_event,
 )
+from utils.test.event_validators.permission import validate_grant_role_event
 
-from utils.test.event_validators.anchor import (
-    validate_anchor_vault_implementation_upgrade_events
-)
+from utils.test.event_validators.anchor import validate_anchor_vault_implementation_upgrade_events
+
+NEW_NODE_OPERATORS = [
+    # name, id, address
+    # to get current id use Node Operators registry's getNodeOperatorsCount function
+    # https://etherscan.io/address/0x55032650b14df07b85bF18A3a3eC8E0Af2e028d5#readProxyContract
+    NodeOperatorItem("X", 99, "X", 0),
+    # and 6 more
+]
+
+
+# web3.keccak(text="STAKING_MODULE_MANAGE_ROLE")
+STAKING_MODULE_MANAGE_ROLE = "0x3105bcbf19d4417b73ae0e58d508a65ecf75665e46c2622d8521732de6080c48"
 
 ANCHOR_OLD_IMPL_ADDRESS = "0x07BE9BB2B1789b8F5B2f9345F18378A8B036A171"
 ANCHOR_NEW_IMPL_ADDRESS = "#TBA"
@@ -41,8 +57,36 @@ bETH burned: 439111118580000000000
 REFUND_BETH_AMOUNT = 4449999990000000000 + 439111118580000000000
 
 
-def test_vote(helpers, accounts, vote_ids_from_env, interface, ldo_holder, stranger, bypass_events_decoding):
+def test_vote(helpers, accounts, vote_ids_from_env, interface, stranger, bypass_events_decoding):
+    # params
+    agent = contracts.agent
+    nor = contracts.node_operators_registry
+    staking_router = contracts.staking_router
+    target_NO_id = 1
+    target_validators_count_change_request = TargetValidatorsCountChanged(
+        nodeOperatorId=target_NO_id, targetValidatorsCount=0
+    )
 
+    # Check that all NOs are unknown yet (1-7)
+    for node_operator in NEW_NODE_OPERATORS:
+        with reverts("NODE_OPERATOR_NOT_FOUND"):
+            no = nor.getNodeOperator(node_operator.id, True)
+
+    # 8)
+    assert staking_router.hasRole(STAKING_MODULE_MANAGE_ROLE, agent.address) == False
+
+    # 9)
+    NO_summary_before = nor.getNodeOperatorSummary(target_NO_id)
+    assert NO_summary_before[0] == False
+    assert NO_summary_before[1] == 0
+    assert NO_summary_before[2] == 0
+    assert NO_summary_before[3] == 0
+    assert NO_summary_before[4] == 0
+    assert NO_summary_before[5] == 0
+    assert NO_summary_before[6] == 1000
+    assert NO_summary_before[7] == 0
+
+    # 10)
     max_uint256 = 2**256 - 1
 
     proxy = contracts.anchor_vault_proxy
@@ -71,19 +115,50 @@ def test_vote(helpers, accounts, vote_ids_from_env, interface, ldo_holder, stran
     beth_total_supply_before = bETH_token.totalSupply()
 
     # START VOTE
-    vote_ids = []
     if len(vote_ids_from_env) > 0:
-        vote_ids = vote_ids_from_env
+        (vote_id,) = vote_ids_from_env
     else:
         tx_params = {"from": LDO_HOLDER_ADDRESS_FOR_TESTS}
         vote_id, _ = start_vote(tx_params, silent=True)
-        vote_ids = [vote_id]
 
-    [vote_tx] = helpers.execute_votes(accounts, vote_ids, contracts.voting)
+    vote_tx = helpers.execute_vote(accounts, vote_id, contracts.voting)
 
-    print(f"voteId = {vote_ids}, gasUsed = {vote_tx.gas_used}")
-    assert count_vote_items_by_events(vote_tx, contracts.voting) == 1, "Incorrect voting items count"
+    print(f"voteId = {vote_id}, gasUsed = {vote_tx.gas_used}")
 
+    # validate vote events
+    assert count_vote_items_by_events(vote_tx, contracts.voting) == 10, "Incorrect voting items count"
+
+    metadata = find_metadata_by_vote_id(vote_id)
+
+    assert get_lido_vote_cid_from_str(metadata) == "bafkreiapvuobyrudww3oqhfopbs2fdmtebi6jnvpeb3plxkajnhafw25im"
+
+    # I
+    # Check that all NO were added
+    for node_operator in NEW_NODE_OPERATORS:
+        no = nor.getNodeOperator(node_operator.id, True)
+
+        message = f"Failed on {node_operator.name}"
+        assert no[0] is True, message  # is active
+        assert no[1] == node_operator.name, message  # name
+        assert no[2] == node_operator.reward_address, message  # rewards address
+        assert no[3] == 0  # staking limit
+
+    # II
+    # 8)
+    assert staking_router.hasRole(STAKING_MODULE_MANAGE_ROLE, agent.address) == True
+
+    # 9)
+    NO_summary_after = nor.getNodeOperatorSummary(target_NO_id)
+    assert NO_summary_after[0] == True
+    assert NO_summary_after[1] == 0
+    assert NO_summary_after[2] == 0
+    assert NO_summary_after[3] == 0
+    assert NO_summary_after[4] == 0
+    assert NO_summary_after[5] == 0
+    assert NO_summary_after[6] == 1000
+    assert NO_summary_after[7] == 0
+
+    # III
     address_implementation_after = proxy.implementation()
     assert address_implementation_before != address_implementation_after, "Implementation is not changed"
     assert address_implementation_after == ANCHOR_NEW_IMPL_ADDRESS, "New address is incorrect"
@@ -116,19 +191,21 @@ def test_vote(helpers, accounts, vote_ids_from_env, interface, ldo_holder, stran
         vault.collect_rewards({"from": stranger})
 
     with reverts("Minting is discontinued"):
-        vault.submit(
-            10**18,
-            TERRA_ADDRESS,
-            "0x8bada2e",
-            vault.version(),
-            {"from": stranger}
-        )
+        vault.submit(10**18, TERRA_ADDRESS, "0x8bada2e", vault.version(), {"from": stranger})
 
     display_voting_events(vote_tx)
 
-    if bypass_events_decoding:
+    if bypass_events_decoding or network_name() in ("goerli", "goerli-fork"):
         return
 
+    # Check events
     evs = group_voting_events(vote_tx)
+
+    for i in range(len(NEW_NODE_OPERATORS)):
+        validate_node_operator_added_event(evs[i], NEW_NODE_OPERATORS[i])
+
+    validate_grant_role_event(evs[0], STAKING_MODULE_MANAGE_ROLE, agent.address, agent.address)
+
+    validate_target_validators_count_changed_event(evs[1], target_validators_count_change_request)
 
     validate_anchor_vault_implementation_upgrade_events(evs[0], ANCHOR_NEW_IMPL_ADDRESS, ANCHOR_NEW_IMPL_VERSION)
