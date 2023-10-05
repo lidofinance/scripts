abbfedb6d4bc3c6253515ab8a04c6f03563db1f0
Aleksandr Tarelkin <tarelkinaa@bk.ru>
2023-10-05 13:53:24 +0300

    update contracts set

--- scripts/vote_2023_09_12.py
+++ scripts/vote_2023_09_12.py
@@ -59,10 +59,9 @@ def start_vote(tx_params: Dict[str, str], silent: bool) -> bool | list[int | Tra
             "0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0",
             contracts.node_operators_registry.initialize.encode_input("0x1eDf09b5023DC86737b59dE68a8130De878984f5", "0x01", 0)
         ),
-        encode_permission_create(entity="0xA28F6127269e85348735e5C6d5D5d0f37C893D3E", target_app="0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0", permission_name='MANAGE_NODE_OPERATOR_ROLE', manager=contracts.voting),
         encode_permission_create(entity="0x3c9AcA237b838c59612d79198685e7f20C7fE783", target_app="0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0", permission_name='MANAGE_NODE_OPERATOR_ROLE', manager=contracts.voting),
         encode_permission_create(entity="0x3c9AcA237b838c59612d79198685e7f20C7fE783", target_app="0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0", permission_name='SET_NODE_OPERATOR_LIMIT_ROLE', manager=contracts.voting),
-        encode_permission_create(entity="0x3c9AcA237b838c59612d79198685e7f20C7fE783", target_app="0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0", permission_name='MANAGE_SIGNING_KEYS', manager=contracts.voting),
+        encode_permission_create(entity="0x3c9AcA237b838c59612d79198685e7f20C7fE783", target_app="0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0", permission_name='MANAGE_SIGNING_KEYS', manager="0x3c9AcA237b838c59612d79198685e7f20C7fE783"),
         encode_permission_create(entity="0x3c9AcA237b838c59612d79198685e7f20C7fE783", target_app="0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0", permission_name='STAKING_ROUTER_ROLE', manager=contracts.voting),
         add_evmscript_factory(
             factory="0x083a26e5285610b91Fd74040B81C9b5a13523bbf",
@@ -75,10 +74,6 @@ def start_vote(tx_params: Dict[str, str], silent: bool) -> bool | list[int | Tra
         add_evmscript_factory(
             factory="0x47a8C2f54513d1d2445Ced353237F4ed406d16f2",
             permissions="0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0"+contracts.node_operators_registry.deactivateNodeOperator.signature[2:] + contracts.acl.address[2:]+ contracts.acl.revokePermission.signature[2:]
-        ),
-        add_evmscript_factory(
-            factory="0xA7AFa4E0Ce9d2A50C96Fe770D3cDd3259DAc0D76",
-            permissions="0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0"+contracts.node_operators_registry.setNodeOperatorStakingLimit.signature[2:]
         ),
         add_evmscript_factory(
             factory="0xB634357735a3b63645b54A2D928CE1b09caffC9d",
@@ -97,14 +92,13 @@ def start_vote(tx_params: Dict[str, str], silent: bool) -> bool | list[int | Tra
             permissions=contracts.acl.address+ contracts.acl.revokePermission.signature[2:] + contracts.acl.address[2:]+ contracts.acl.grantPermissionP.signature[2:]
         ),
         add_evmscript_factory(
-            factory="0xd3218b08cbB921908C41A74E79D7Ca2672B51f3E",
-            permissions=contracts.acl.address+ contracts.acl.removePermissionManager.signature[2:]
+            factory="0x56eC3C4CeF495C3CBaBdedfb73fd7cf4B8F5B437",
+            permissions="0x6370FA71b9Fd83aFC4196ee189a0d348C90E93b0"+ contracts.node_operators_registry.updateTargetValidatorsLimits.signature[2:]
         ),
     ]
 
     vote_desc_items = [
         f"1) Init staking module",
-            "2) grant MANAGE_NODE_OPERATOR_ROLE for committee",
             "2) grant MANAGE_NODE_OPERATOR_ROLE for et executor",
             "2) grant SET_NODE_OPERATOR_LIMIT_ROLE for et executor",
             "2) grant MANAGE_SIGNING_KEYS for et executor",
@@ -112,12 +106,11 @@ def start_vote(tx_params: Dict[str, str], silent: bool) -> bool | list[int | Tra
             "AddNodeOperators deployed at: 0x083a26e5285610b91Fd74040B81C9b5a13523bbf",
             "ActivateNodeOperators deployed at: 0x7983F6879C0C06a9718bAf90e6E0ebD3e7243A3F",
             "DeactivateNodeOperators deployed at: 0x47a8C2f54513d1d2445Ced353237F4ed406d16f2",
-            "IncreaseVettedValidatorsLimit deployed at: 0xA7AFa4E0Ce9d2A50C96Fe770D3cDd3259DAc0D76",
             "SetVettedValidatorsLimits deployed at: 0xB634357735a3b63645b54A2D928CE1b09caffC9d",
             "SetNodeOperatorNames deployed at: 0x827e3C09A6044afF2f5cF78BB064bB8a40B4C13F",
             "SetNodeOperatorRewardAddresses deployed at: 0x2e565f073FeBD66cb24dD9CA66Bffe6CeFd0B7Af",
             "TransferNodeOperatorManager deployed at: 0x8A33BA98C7165BDb61af1468fd5BD8aF22B3d87d",
-            "RenounceManageSigningKeysRoleManager deployed at: 0xd3218b08cbB921908C41A74E79D7Ca2672B51f3E",
+            "UpdateTargetValidatorLimits deployed at: 0x56eC3C4CeF495C3CBaBdedfb73fd7cf4B8F5B437",
         
     ]
 
