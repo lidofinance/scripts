2135dd2861d1953e54e85fa01c12b2ffd42fa305
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-03-20 17:22:43 +0100

    feat: add second test vote for a.DI

--- utils/agent.py
+++ utils/agent.py
@@ -14,7 +14,6 @@ def agent_forward(call_script: Sequence[Tuple[str, str]]) -> Tuple[str, str]:
     return (AGENT, agent.forward.encode_input(encode_call_script(call_script)))
 
 
-
-def agent_execute(target: str, value: str, data: str) -> Tuple[str, str]:
+def agent_execute(target: str, value: int, data: str) -> Tuple[str, str]:
     agent = contracts.agent
     return (AGENT, agent.execute.encode_input(target, value, data))
