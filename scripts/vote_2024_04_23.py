21bd4a78463150d604f8168e2dcca14893b82904
Alexander Belokon <ikatamarinaki@gmail.com>
2024-04-09 00:31:14 +0200

    feat: add voting script and tests for simple delegation release

--- scripts/vote_2024_04_23.py
+++ scripts/vote_2024_04_23.py
@@ -1,12 +1,9 @@
 """
-!! HOLESKY only
-
 Voting 23/04/2024.
 
-1. Push new Voting app version to Voting Repo 0x4Ee3118E3858E8D7164A634825BfE0F73d99C792
-2. Upgrade the Aragon Voting contract implementation 0x72fb5253AD16307B9E773d2A78CaC58E309d5Ba4
-3. Push new Lido app version to Lido Repo 0xF5Dc67E54FC96F993CD06073f71ca732C1E654B1
-4. Upgrade TRP voting adapter
+1. Push new Voting app version to the Voting Repo 0x4ee3118e3858e8d7164a634825bfe0f73d99c792
+2. Upgrade the Aragon Voting contract implementation 0x63C7F17210f6a7061e887D05BBF5412085e9DF43
+3. Upgrade TRP voting adapter 0x5Ea73d6AE9B2E57eF865A3059bdC5C06b8e46072
 
 """
 
@@ -20,30 +17,25 @@ from utils.config import (
     get_deployer_account,
     get_is_live,
     get_priority_fee,
+    contracts,
 )
-from utils.repo import (
-    add_implementation_to_voting_app_repo,
-    add_implementation_to_lido_app_repo,
-)
+from utils.repo import add_implementation_to_voting_app_repo
 from utils.kernel import update_app_implementation
+from utils.agent import agent_forward
 
-update_voting_app = {
-    "new_address": "0xcB738a79baeA44C93Ee46c02EF0FA975Bc4d058f",
+updated_trp_voting_adapter = "0x5Ea73d6AE9B2E57eF865A3059bdC5C06b8e46072"
+
+updated_voting_app = {
+    "address": "0x63C7F17210f6a7061e887D05BBF5412085e9DF43",
     "content_uri": "0x697066733a516d506f7478377a484743674265394445684d6f4238336572564a75764d74335971436e6454657a575652706441",
     "id": "0x0abcd104777321a82b010357f20887d61247493d89d2e987ff57bcecbde00e1e",
-    "version": (3, 0, 0),
+    "version": (4, 0, 0),
 }
-
-update_lido_app = {
-    "address": "0x47EbaB13B806773ec2A2d16873e2dF770D130b50",
-    "content_uri": "0x697066733a516d536359787a6d6d724156316344426a4c3369376a7a615a75694a373655716461465a694d6773786f46477a43",
-    "version": (3, 1, 0),
-}
-
 
 description = """
-
+aboba
 """
+
 
 def start_vote(tx_params: Dict[str, str], silent: bool) -> bool | list[int | TransactionReceipt | None]:
     """Prepare and run voting."""
@@ -53,23 +45,26 @@ def start_vote(tx_params: Dict[str, str], silent: bool) -> bool | list[int | Tra
         # I. Simple Delegation
         #
         (
-            "1) Push new Voting app version to Voting Repo",
+            "1) Push new Voting app version to the Voting Repo",
             add_implementation_to_voting_app_repo(
-                update_voting_app["version"],
-                update_voting_app["new_address"],
-                update_voting_app["content_uri"],
+                updated_voting_app["version"],
+                updated_voting_app["address"],
+                updated_voting_app["content_uri"],
             ),
         ),
         (
-            "2) Upgrade the DAO Voting contract implementation",
-            update_app_implementation(update_voting_app["id"], update_voting_app["new_address"]),
+            "2) Upgrade the Aragon Voting contract implementation",
+            update_app_implementation(updated_voting_app["id"], updated_voting_app["address"]),
         ),
         (
-            "3) Push new Lido app version to Lido Repo",
-            add_implementation_to_lido_app_repo(
-                update_lido_app["version"],
-                update_lido_app["address"],
-                update_lido_app["content_uri"],
+            "3) Upgrade TRP voting adapter",
+            agent_forward(
+                [
+                    (
+                        contracts.trp_escrow_factory.address,
+                        contracts.trp_escrow_factory.update_voting_adapter.encode_input(updated_trp_voting_adapter),
+                    )
+                ]
             ),
         ),
     )
---
+++ tests/test_2024_04_23.py
@@ -0,0 +1,98 @@
+"""
+Tests for voting 23/04/2024
+
+"""
+
+from brownie import accounts, interface, ZERO_ADDRESS
+from scripts.vote_2024_04_23 import start_vote
+from utils.test.event_validators.vesting_escrow import validate_voting_adapter_upgraded_event
+from utils.voting import find_metadata_by_vote_id
+from utils.ipfs import get_lido_vote_cid_from_str
+from utils.config import (
+    contracts,
+    network_name,
+    LDO_HOLDER_ADDRESS_FOR_TESTS,
+    VOTING,
+)
+from utils.test.event_validators.aragon import validate_push_to_repo_event, validate_app_update_event
+from utils.test.tx_tracing_helpers import *
+
+old_trp_voting_adapter_address = "0xCFda8aB0AE5F4Fa33506F9C51650B890E4871Cc1"
+
+updated_trp_voting_adapter_address = "0x5Ea73d6AE9B2E57eF865A3059bdC5C06b8e46072"
+
+old_voting_app = {
+    "address": "0x72fb5253AD16307B9E773d2A78CaC58E309d5Ba4",
+    "content_uri": "0x697066733a516d506f7478377a484743674265394445684d6f4238336572564a75764d74335971436e6454657a575652706441",
+    "id": "0x0abcd104777321a82b010357f20887d61247493d89d2e987ff57bcecbde00e1e",
+    "version": (3, 0, 0),
+}
+
+updated_voting_app = {
+    "address": "0x63C7F17210f6a7061e887D05BBF5412085e9DF43",
+    "content_uri": "0x697066733a516d506f7478377a484743674265394445684d6f4238336572564a75764d74335971436e6454657a575652706441",
+    "id": "0x0abcd104777321a82b010357f20887d61247493d89d2e987ff57bcecbde00e1e",
+    "version": (4, 0, 0),
+}
+
+deployer_address = "0x64C0fF5C25925aCB33D68F79AD728Fd63361ffce"
+
+
+def test_vote(helpers, vote_ids_from_env, bypass_events_decoding):
+
+    # Voting App before
+    voting_proxy = interface.AppProxyUpgradeable(contracts.voting.address)
+    voting_app_from_repo = contracts.voting_app_repo.getLatest()
+    voting_appId = voting_proxy.appId()
+
+    assert voting_app_from_repo[0] == old_voting_app["version"]
+    assert voting_app_from_repo[1] == old_voting_app["address"]
+    assert voting_proxy.implementation() == old_voting_app["address"]
+
+    # TRP Voting Adapter before
+    trp_voting_adapter_address = contracts.trp_escrow_factory.voting_adapter()
+    assert trp_voting_adapter_address == old_trp_voting_adapter_address
+
+    trp_voting_adapter = interface.VotingAdapter(trp_voting_adapter_address)
+    assert trp_voting_adapter.delegation_contract_addr() == ZERO_ADDRESS
+    assert trp_voting_adapter.voting_contract_addr() == VOTING
+
+    # START VOTE
+    if len(vote_ids_from_env) > 0:
+        (vote_id,) = vote_ids_from_env
+    else:
+        tx_params = {"from": LDO_HOLDER_ADDRESS_FOR_TESTS}
+        vote_id, _ = start_vote(tx_params, silent=True)
+
+    vote_tx = helpers.execute_vote(accounts, vote_id, contracts.voting)
+
+    print(f"voteId = {vote_id}, gasUsed = {vote_tx.gas_used}")
+
+    # Voting App after
+    voting_app_from_repo = contracts.voting_app_repo.getLatest()
+
+    assert voting_app_from_repo[0] == updated_voting_app["version"]
+    assert voting_app_from_repo[1] == updated_voting_app["address"]
+    assert voting_proxy.implementation() == updated_voting_app["address"]
+
+    # TRP Voting Adapter after
+    assert contracts.trp_escrow_factory.voting_adapter() == updated_trp_voting_adapter_address
+
+    assert trp_voting_adapter.delegation_contract_addr() == VOTING
+    assert trp_voting_adapter.voting_contract_addr() == VOTING
+
+    # Validating events
+    assert count_vote_items_by_events(vote_tx, contracts.voting) == 3, "Incorrect voting items count"
+
+    # metadata = find_metadata_by_vote_id(vote_id)
+    # assert get_lido_vote_cid_from_str(metadata) == "" TODO: add ipfs cid
+    display_voting_events(vote_tx)
+
+    if bypass_events_decoding or network_name() in ("goerli", "goerli-fork"):
+        return
+
+    evs = group_voting_events(vote_tx)
+
+    validate_push_to_repo_event(evs[0], updated_voting_app["version"])
+    validate_app_update_event(evs[1], voting_appId, updated_voting_app["address"])
+    validate_voting_adapter_upgraded_event(evs[2], updated_trp_voting_adapter_address)
