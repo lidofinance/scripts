92c5f3cf4611ee7e38a52c129b3f0b124751fd0e
Alexander Belokon <ikatamarinaki@gmail.com>
2024-04-12 00:56:56 +0200

    test: simple delegation tests tweaks and copy fixes

--- tests/regression/test_voting_acc.py
+++ tests/regression/test_voting_acc.py
@@ -5,7 +5,7 @@ from typing import Tuple, Optional
 
 import pytest
 
-from brownie import MockCallTarget, accounts, chain, reverts, interface
+from brownie import MockCallTarget, accounts, chain, reverts, interface, ZERO_ADDRESS
 from brownie.network.transaction import TransactionReceipt
 from utils.voting import create_vote, bake_vote_items
 from utils.config import LDO_VOTE_EXECUTORS_FOR_TESTS
@@ -16,6 +16,7 @@ from utils.config import contracts
 @pytest.fixture(scope="module")
 def call_target():
     return MockCallTarget.deploy({"from": accounts[0]})
+
 
 @pytest.fixture(scope="module")
 def test_trp_escrow(trp_recipient, ldo_holder):
@@ -147,25 +148,25 @@ def test_can_object(stranger, test_vote):
 
 """
 # [main] delegate vote for YES holders 1,2 (reject no delegation)
-# [main] holders 0,1 delegate to delegate
+# [main] holders 0,1 delegate to 'delegate'
 # [main] delegate vote for YES holder 3 (reject not a delegation for holder 3)
 # [main] delegate vote for YES holders 1,2,3 (vote for 1 and 2 not 3)
-# [main] delegate revote for NO holders 1,2
-# [main] holder 3 delegate to delegate
+# [main] delegate re-vote for NO holders 1,2
+# [main] holder 3 delegate to 'delegate'
 # [main] delegate vote for YES holders 3
 # [main] holder 3 vote for NO
 # [main] holder 2 delegate to holder 3
 # [main] holder 1 reset delegate
-# [main] delegate revote for YES holders 1 (reject not a delegate)
-# [main] delegate revote for YES holders 2 (reject not a delegate)
-# [main] delegate revote for YES holders 3 (reject cant overpower holder)
-# [main] holders 1,2,3 can revote main phase YES
-# [objc] holders 1,2,3 can revote NO
-# [objc] delegate revote NO (reject)
+# [main] delegate re-vote for YES holders 1 (reject not a delegate)
+# [main] delegate re-vote for YES holders 2 (reject not a delegate)
+# [main] delegate re-vote for YES holders 3 (reject cant overpower holder)
+# [main] holders 1,2,3 can re-vote main phase YES
+# [objc] holders 1,2,3 can re-vote NO
+# [objc] delegate re-vote NO (reject)
 """
 
 
-def test_simple_delegation(call_target, delegate, test_vote):
+def test_simple_delegation(delegate, test_vote):
     vote_id = test_vote[0]
 
     holder1 = LDO_VOTE_EXECUTORS_FOR_TESTS[0]
@@ -175,44 +176,50 @@ def test_simple_delegation(call_target, delegate, test_vote):
     assert contracts.voting.getVotePhase(vote_id) == 0  # Main phase
     # [main] delegate vote for YES holders 1 (reject no delegation)
     with reverts("VOTING_CAN_NOT_VOTE_FOR"):
-        # if you don't have this func in object update abi
+        # if you don't have this func in object, update abi
         contracts.voting.attemptVoteFor(vote_id, True, holder1, {"from": delegate})
 
-    # [main] holders 0,1 delegate to delegate
+    # [main] holders 0,1 delegate to 'delegate'
     for holder in [holder1, holder2]:
         contracts.voting.setDelegate(delegate, {"from": accounts.at(holder, force=True)})
 
-    # [main] delegate vote for YES holder 3 (reject not a delegation for holder 3)
+    # [main] delegate vote for YEA holder 3 (reject not a delegation for holder 3)
     with reverts("VOTING_CAN_NOT_VOTE_FOR"):
         contracts.voting.attemptVoteFor(vote_id, True, holder3, {"from": delegate})
 
-    # [main] delegate vote for YES holders 1,2 (vote for 1 and 2)
+    # [main] delegate vote for YEA holders 1,2 (vote for 1 and 2)
     contracts.voting.attemptVoteFor(vote_id, True, holder1, {"from": delegate})
     contracts.voting.attemptVoteFor(vote_id, True, holder2, {"from": delegate})
 
-    # [main] delegate revote for NO holders 1,2
-    contracts.voting.attemptVoteFor(vote_id, True, holder1, {"from": delegate})
-    contracts.voting.attemptVoteFor(vote_id, True, holder2, {"from": delegate})
+    # [main] delegate re-vote for NAY holders 1,2
+    contracts.voting.attemptVoteFor(vote_id, False, holder1, {"from": delegate})
+    contracts.voting.attemptVoteFor(vote_id, False, holder2, {"from": delegate})
 
-    # [main] holder 3 delegate to delegate
+    # [main] holder 3 delegate to 'delegate'
     contracts.voting.setDelegate(delegate, {"from": accounts.at(holder3, force=True)})
 
     holders = contracts.voting.getDelegatedVoters(delegate, 0, 5)
     assert holders[0] == [holder1, holder2, holder3]
+    for holder in holders[0]:
+        assert contracts.voting.getDelegate(holder) == delegate
 
-    # [main] delegate vote for YES holders 3
+    # [main] delegate vote for YEA holders 3
     contracts.voting.attemptVoteFor(vote_id, True, holder3, {"from": delegate})
 
-    # [main] holder 3 vote for NO
+    # [main] holder 3 vote for NAY
     contracts.voting.vote(vote_id, False, False, {"from": accounts.at(holder3, force=True)})
 
     # [main] holder 2 delegate to holder 3
     contracts.voting.setDelegate(holder3, {"from": accounts.at(holder2, force=True)})
 
+    assert contracts.voting.getDelegate(holder2) == holder3
+
     # [main] holder 1 reset delegate
     contracts.voting.resetDelegate({"from": accounts.at(holder1, force=True)})
 
-    # [main] delegate revote for YES holders 1-3 (reject)
+    assert contracts.voting.getDelegate(holder1) == ZERO_ADDRESS
+
+    # [main] delegate re-vote for YES holders 1-3 (reject)
     with reverts("VOTING_CAN_NOT_VOTE_FOR"):
         contracts.voting.attemptVoteFor(vote_id, True, holder1, {"from": delegate})
 
@@ -224,8 +231,9 @@ def test_simple_delegation(call_target, delegate, test_vote):
 
     holders = contracts.voting.getDelegatedVoters(delegate, 0, 5)
     assert holders[0] == [holder3]
+    assert contracts.voting.getDelegate(holder3) == delegate
 
-    # [main] holders 1,2,3 can revote main phase YES
+    # [main] holders 1,2,3 can re-vote main phase YEA
     for holder in [holder1, holder2, holder3]:
         contracts.voting.vote(vote_id, True, False, {"from": accounts.at(holder, force=True)})
 
@@ -234,11 +242,11 @@ def test_simple_delegation(call_target, delegate, test_vote):
 
     assert contracts.voting.getVotePhase(vote_id) == 1  # Objection phase
 
-    # [objc] holders 1,2,3 can revote NO
+    # [objc] holders 1,2,3 can re-vote NAY
     for holder in [holder1, holder2, holder3]:
         contracts.voting.vote(vote_id, False, False, {"from": accounts.at(holder, force=True)})
 
-    # [objc] delegate revote NO (reject)
+    # [objc] delegate re-vote NAY (reject)
     with reverts("VOTING_CAN_NOT_VOTE"):
         contracts.voting.attemptVoteFor(vote_id, True, holder1, {"from": delegate})
 
@@ -250,27 +258,27 @@ def test_simple_delegation(call_target, delegate, test_vote):
 
 
 """
-# [main] delegate vote for YES holders 1,2 (reject no delegation)
-# [main] holders 0,1 delegate to delegate
-# [main] delegate vote for YES holder 3 (reject not a delegation for holder 3)
-# [main] delegate vote for YES holders 1,2,3 (vote for 1 and 2 not 3)
-# [main] delegate revote for NO holders 1,2
-# [main] holder 3 delegate to delegate
-# [main] delegate vote for YES holders 3
-# [main] holder 3 vote for NO
+# [main] delegate vote for YEA holders 1,2 (reject no delegation)
+# [main] holders 0,1 delegate to 'delegate'
+# [main] delegate vote for YEA holder 3 (reject not a delegation for holder 3)
+# [main] delegate vote for YEA holders 1,2,3 (vote for 1 and 2 not 3)
+# [main] delegate re-vote for NAY holders 1,2
+# [main] holder 3 delegate to 'delegate'
+# [main] delegate vote for YEA holders 3
+# [main] holder 3 vote for NAY
 # [main] holder 2 delegate to holder 3
 # [main] holder 1 reset delegate
-# [main] delegate revote for YES holders 1-3 (reject)
-# [main] delegate revote for YES holders 1 (reject not a delegate)
-# [main] delegate revote for YES holders 2 (reject not a delegate)
-# [main] delegate revote for YES holders 3 (reject cant overpower holder)
-# [main] holders 1,2,3 can revote main phase YES
-# [objc] holders 1,2,3 can revote NO
-# [objc] delegate revote NO (reject)
+# [main] delegate re-vote for YEA holders 1-3 (reject)
+# [main] delegate re-vote for YEA holders 1 (reject not a delegate)
+# [main] delegate re-vote for YEA holders 2 (reject not a delegate)
+# [main] delegate re-vote for YEA holders 3 (reject cant overpower holder)
+# [main] holders 1,2,3 can re-vote main phase YEA
+# [objc] holders 1,2,3 can re-vote NAY
+# [objc] delegate re-vote NAY (reject)
 """
 
 
-def test_simple_delegation_multi(call_target, delegate, test_vote):
+def test_simple_delegation_multiple(delegate, test_vote):
     vote_id = test_vote[0]
 
     holder1 = LDO_VOTE_EXECUTORS_FOR_TESTS[0]
@@ -278,35 +286,37 @@ def test_simple_delegation_multi(call_target, delegate, test_vote):
     holder3 = LDO_VOTE_EXECUTORS_FOR_TESTS[2]
 
     assert contracts.voting.getVotePhase(vote_id) == 0  # Main phase
-    # [main] delegate vote for YES holders 1,2 (reject no delegation)
+    # [main] delegate vote for YEA holders 1,2 (reject no delegation)
     with reverts("VOTING_CAN_NOT_VOTE_FOR"):
-        # if you don't have this func in object update abi
+        # if you don't have this func in object, update abi
         contracts.voting.attemptVoteForMultiple(vote_id, True, [holder1, holder2], {"from": delegate})
 
-    # [main] holders 0,1 delegate to delegate
+    # [main] holders 0,1 delegate to 'delegate'
     for holder in [holder1, holder2]:
         contracts.voting.setDelegate(delegate, {"from": accounts.at(holder, force=True)})
 
-    # [main] delegate vote for YES holder 3 (reject not a delegation for holder 3)
+    # [main] delegate vote for YEA holder 3 (reject not a delegation for holder 3)
     with reverts("VOTING_CAN_NOT_VOTE_FOR"):
         contracts.voting.attemptVoteForMultiple(vote_id, True, [holder3], {"from": delegate})
 
-    # [main] delegate vote for YES holders 1,2,3 (vote for 1 and 2 not 3)
+    # [main] delegate vote for YEA holders 1,2,3 (vote for 1 and 2 not 3)
     contracts.voting.attemptVoteForMultiple(vote_id, True, [holder1, holder2, holder3], {"from": delegate})
 
-    # [main] delegate revote for NO holders 1,2
-    contracts.voting.attemptVoteForMultiple(vote_id, True, [holder1, holder2], {"from": delegate})
+    # [main] delegate re-vote for NAY holders 1,2
+    contracts.voting.attemptVoteForMultiple(vote_id, False, [holder1, holder2], {"from": delegate})
 
-    # [main] holder 3 delegate to delegate
+    # [main] holder 3 delegate to 'delegate'
     contracts.voting.setDelegate(delegate, {"from": accounts.at(holder3, force=True)})
 
     holders = contracts.voting.getDelegatedVoters(delegate, 0, 5)
     assert holders[0] == [holder1, holder2, holder3]
+    for holder in holders[0]:
+        assert contracts.voting.getDelegate(holder) == delegate
 
-    # [main] delegate vote for YES holders 3
+    # [main] delegate vote for YEA holders 3
     contracts.voting.attemptVoteForMultiple(vote_id, True, [holder3], {"from": delegate})
 
-    # [main] holder 3 vote for NO
+    # [main] holder 3 vote for NAY
     contracts.voting.vote(vote_id, False, False, {"from": accounts.at(holder3, force=True)})
 
     # [main] holder 2 delegate to holder 3
@@ -315,26 +325,27 @@ def test_simple_delegation_multi(call_target, delegate, test_vote):
     # [main] holder 1 reset delegate
     contracts.voting.resetDelegate({"from": accounts.at(holder1, force=True)})
 
-    # [main] delegate revote for YES holders 1-3 (reject)
+    # [main] delegate re-vote for YEA holders 1-3 (reject)
     with reverts("VOTING_CAN_NOT_VOTE_FOR"):
         contracts.voting.attemptVoteForMultiple(vote_id, True, [holder1, holder2, holder3], {"from": delegate})
 
     holders = contracts.voting.getDelegatedVoters(delegate, 0, 5)
     assert holders[0] == [holder3]
+    assert contracts.voting.getDelegate(holder3) == delegate
 
-    # [main] delegate revote for YES holders 1 (reject not a delegate)
+    # [main] delegate re-vote for YEA holders 1 (reject not a delegate)
     with reverts("VOTING_CAN_NOT_VOTE_FOR"):
         contracts.voting.attemptVoteForMultiple(vote_id, True, [holder1], {"from": delegate})
 
-    # [main] delegate revote for YES holders 2 (reject not a delegate)
+    # [main] delegate re-vote for YEA holders 2 (reject not a delegate)
     with reverts("VOTING_CAN_NOT_VOTE_FOR"):
         contracts.voting.attemptVoteForMultiple(vote_id, True, [holder2], {"from": delegate})
 
-    # [main] delegate revote for YES holders 3 (reject cant overpower holder)
+    # [main] delegate re-vote for YEA holders 3 (reject cant overpower holder)
     with reverts("VOTING_CAN_NOT_VOTE_FOR"):
         contracts.voting.attemptVoteForMultiple(vote_id, True, [holder3], {"from": delegate})
 
-    # [main] holders 1,2,3 can revote main phase YES
+    # [main] holders 1,2,3 can re-vote main phase YEA
     for holder in [holder1, holder2, holder3]:
         contracts.voting.vote(vote_id, True, False, {"from": accounts.at(holder, force=True)})
 
@@ -343,16 +354,16 @@ def test_simple_delegation_multi(call_target, delegate, test_vote):
 
     assert contracts.voting.getVotePhase(vote_id) == 1  # Objection phase
 
-    # [objc] holders 1,2,3 can revote NO
+    # [objc] holders 1,2,3 can re-vote NAY
     for holder in [holder1, holder2, holder3]:
         contracts.voting.vote(vote_id, False, False, {"from": accounts.at(holder, force=True)})
 
-    # [objc] delegate revote NO (reject)
+    # [objc] delegate re-vote NAY (reject)
     with reverts("VOTING_CAN_NOT_VOTE"):
         contracts.voting.attemptVoteForMultiple(vote_id, True, [holder1, holder2, holder3], {"from": delegate})
 
 
-def test_trp_delegation(ldo_holder, test_trp_escrow, test_vote, delegate, trp_recipient, call_target):
+def test_trp_delegation(test_trp_escrow, test_vote, delegate, trp_recipient):
     vote_id = test_vote[0]
     assert contracts.voting.getVotePhase(vote_id) == 0  # Main phase
 
@@ -360,9 +371,13 @@ def test_trp_delegation(ldo_holder, test_trp_escrow, test_vote, delegate, trp_re
     trp_voting_adapter = interface.VotingAdapter(trp_voting_adapter_address)
     encoded_delegate_address = trp_voting_adapter.encode_delegate_calldata(delegate.address)
 
-    interface.Escrow(test_trp_escrow).delegate(encoded_delegate_address, {"from": trp_recipient})
+    trp_escrow_contract = interface.Escrow(test_trp_escrow)
 
-    contracts.voting.getDelegatedVoters(delegate.address, 0, 5, {"from": delegate})
+    trp_escrow_contract.delegate(encoded_delegate_address, {"from": trp_recipient})
+
+    delegated_voters = contracts.voting.getDelegatedVoters(delegate.address, 0, 5, {"from": delegate})
+    assert delegated_voters[0] == [test_trp_escrow]
+    assert contracts.voting.getDelegate(test_trp_escrow) == delegate
 
     vote_before = contracts.voting.getVote(vote_id, {"from": delegate})
     assert vote_before["yea"] == 0
@@ -371,8 +386,13 @@ def test_trp_delegation(ldo_holder, test_trp_escrow, test_vote, delegate, trp_re
     vote_after = contracts.voting.getVote(vote_id, {"from": delegate})
     assert vote_after["yea"] == 1_000_000_000_000_000_000
 
+    encoded_zero_address = trp_voting_adapter.encode_delegate_calldata(ZERO_ADDRESS)
 
-def test_trp_delegation_multi(ldo_holder, test_trp_escrow, test_vote, delegate, trp_recipient, call_target):
+    trp_escrow_contract.delegate(encoded_zero_address, {"from": trp_recipient})
+    assert contracts.voting.getDelegate(trp_recipient) == ZERO_ADDRESS
+
+
+def test_trp_delegation_multiple(test_trp_escrow, test_vote, delegate, trp_recipient):
     vote_id = test_vote[0]
     assert contracts.voting.getVotePhase(vote_id) == 0  # Main phase
 
