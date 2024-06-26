0404406e5fe93f12af53bc4ad684d8b0f9d44ca0
maxim <maksim@lido.fi>
2024-06-26 18:16:44 +0200

    feat: make sure that LDO_HOLDER_ADDRESS_FOR_TESTS has some eth before start vote

--- utils/import_current_votes.py
+++ utils/import_current_votes.py
@@ -4,7 +4,7 @@ import glob
 
 from brownie import accounts
 from brownie.network.transaction import TransactionReceipt
-
+from utils.test.helpers import ETH
 from utils.config import LDO_HOLDER_ADDRESS_FOR_TESTS, LIDO_V2_UPGRADE_TEMPLATE, get_is_live, contracts
 
 
@@ -56,6 +56,13 @@ def start_and_execute_votes(dao_voting, helpers) -> tuple[List[str], List[Transa
         start_vote_name = f"start_vote_{script_name}"
         exec(f"from {name_for_import} import start_vote as {start_vote_name}")
         start_vote = locals()[start_vote_name]
+
+        # before start vote make sure that test account have some ETH
+        # otherwise start_vote will fail with ValueError: Insufficient funds for gas * price + value
+        # check voting.py: tx = token_manager.forward(new_vote_script, tx_params)
+        ldo_holder_for_test = accounts.at(LDO_HOLDER_ADDRESS_FOR_TESTS, True)
+        if(ldo_holder_for_test.balance() < ETH(1)):
+            accounts[0].transfer(LDO_HOLDER_ADDRESS_FOR_TESTS, "1 ether")
 
         vote_id, _ = start_vote({"from": LDO_HOLDER_ADDRESS_FOR_TESTS}, silent=True)
         (tx,) = helpers.execute_votes(accounts, [vote_id], dao_voting, topup="0.5 ether")
