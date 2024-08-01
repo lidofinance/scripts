e4dec2f9206f44994d901e907a2ff4b0ea33d44f
maxim <maksim@lido.fi>
2024-08-01 23:16:10 +0200

    test: decrease vetted signing keys count

--- tests/acceptance/test_staking_module.py
+++ tests/acceptance/test_staking_module.py
@@ -3,6 +3,7 @@ from brownie import interface, reverts  # type: ignore
 from brownie.network.account import Account
 from brownie import convert
 from web3 import Web3
+from utils.test.keys_helpers import random_pubkeys_batch, random_signatures_batch
 
 from utils.config import (
     contracts,
@@ -14,7 +15,18 @@ from utils.config import (
 def voting(accounts):
     return accounts.at(contracts.voting.address, force=True)
 
-def update_target_validators_limits(staking_module, voting, stranger):
+def prep_ids_counts_payload(ids, counts):
+    return {
+        'operator_ids': "0x" + "".join(number_to_hex(id, 8) for id in ids),
+        'keys_counts': "0x" + "".join(number_to_hex(count, 16) for count in counts),
+    }
+
+def number_to_hex(n, byte_len=None):
+    s = hex(n)[2:]  # Convert to hex and remove the '0x' prefix
+    return s if byte_len is None else s.zfill(byte_len * 2)
+
+
+def add_node_operator(staking_module, voting, stranger):
     operator_id = staking_module.getNodeOperatorsCount()
 
     with reverts("APP_AUTH_FAILED"):
@@ -28,6 +40,11 @@ def update_target_validators_limits(staking_module, voting, stranger):
     )
 
     staking_module.addNodeOperator("test", f"0xbb{str(1).zfill(38)}", {"from": stranger} )
+
+    return operator_id
+
+def update_target_validators_limits(staking_module, voting, stranger):
+    operator_id = add_node_operator(staking_module, voting, stranger)
 
     node_operator = staking_module.getNodeOperatorSummary(operator_id)
     assert node_operator["targetLimitMode"] == 0
@@ -67,6 +84,107 @@ def update_target_validators_limits(staking_module, voting, stranger):
     assert node_operator["targetLimitMode"] == 0
     assert node_operator["targetValidatorsCount"] == 0 # should be always 0 in disabled mode
 
+
+def decrease_vetted_signing_keys_count(staking_module, voting, stranger):
+    operator_id = add_node_operator(staking_module, voting, stranger)
+    operator = staking_module.getNodeOperator(operator_id, True)
+
+    keys_count = 10
+    staking_module.addSigningKeys(
+        operator_id,
+        keys_count,
+        random_pubkeys_batch(keys_count),
+        random_signatures_batch(keys_count),
+        {"from": operator["rewardAddress"]},
+    )
+
+    contracts.acl.grantPermission(
+        stranger,
+        staking_module,
+        convert.to_uint(Web3.keccak(text="SET_NODE_OPERATOR_LIMIT_ROLE")),
+        {"from": voting},
+    )
+
+    staking_module.setNodeOperatorStakingLimit(operator_id, 8, {"from": stranger})
+
+    node_operator_before_unvetting = staking_module.getNodeOperator(operator_id, True)
+    assert node_operator_before_unvetting["totalAddedValidators"] == keys_count
+    assert node_operator_before_unvetting["totalVettedValidators"] == 8
+
+    ids_counts_payload = prep_ids_counts_payload([operator_id], [6])
+
+    with reverts("APP_AUTH_FAILED"):
+        staking_module.decreaseVettedSigningKeysCount(
+            ids_counts_payload["operator_ids"],
+            ids_counts_payload["keys_counts"],
+            {"from": stranger}
+        )
+
+    contracts.acl.grantPermission(
+        stranger,
+        staking_module,
+        convert.to_uint(Web3.keccak(text="STAKING_ROUTER_ROLE")),
+        {"from": voting},
+    )
+
+    with reverts("OUT_OF_RANGE"):
+        ids_counts_payload_invalid_id = prep_ids_counts_payload([operator_id + 1], [6])
+        staking_module.decreaseVettedSigningKeysCount(
+            ids_counts_payload_invalid_id["operator_ids"],
+            ids_counts_payload_invalid_id["keys_counts"],
+            {"from": stranger}
+        )
+
+    with reverts("VETTED_KEYS_COUNT_INCREASED"):
+        ids_counts_payload_increase = prep_ids_counts_payload([operator_id], [9])
+        staking_module.decreaseVettedSigningKeysCount(
+            ids_counts_payload_increase["operator_ids"],
+            ids_counts_payload_increase["keys_counts"],
+            {"from": stranger}
+        )
+
+    with reverts("VETTED_KEYS_COUNT_INCREASED"):
+        ids_counts_payload_increase = prep_ids_counts_payload([operator_id], [11])
+        staking_module.decreaseVettedSigningKeysCount(
+            ids_counts_payload_increase["operator_ids"],
+            ids_counts_payload_increase["keys_counts"],
+            {"from": stranger}
+        )
+
+    staking_module.decreaseVettedSigningKeysCount(
+        ids_counts_payload["operator_ids"],
+        ids_counts_payload["keys_counts"],
+        {"from": stranger}
+    )
+
+    node_operator_after_unvetting = staking_module.getNodeOperator(operator_id, True)
+    assert node_operator_after_unvetting["totalAddedValidators"] == keys_count
+    assert node_operator_after_unvetting["totalVettedValidators"] == 6
+
+    # second attempt to decrease keys count to the same value do nothing
+    staking_module.decreaseVettedSigningKeysCount(
+        ids_counts_payload["operator_ids"],
+        ids_counts_payload["keys_counts"],
+        {"from": stranger}
+    )
+
+    node_operator_after_unvetting = staking_module.getNodeOperator(operator_id, True)
+    assert node_operator_after_unvetting["totalAddedValidators"] == keys_count
+    assert node_operator_after_unvetting["totalVettedValidators"] == 6
+
+    # decrease to zero
+    ids_counts_zero_payload = prep_ids_counts_payload([operator_id], [0])
+    staking_module.decreaseVettedSigningKeysCount(
+        ids_counts_zero_payload["operator_ids"],
+        ids_counts_zero_payload["keys_counts"],
+        {"from": stranger}
+    )
+
+    node_operator_after_unvetting = staking_module.getNodeOperator(operator_id, True)
+    assert node_operator_after_unvetting["totalAddedValidators"] == keys_count
+    assert node_operator_after_unvetting["totalVettedValidators"] == 0
+
+
 def test_curated_module_update_target_validators_limits(voting, stranger):
     staking_module = interface.NodeOperatorsRegistry(NODE_OPERATORS_REGISTRY)
     update_target_validators_limits(staking_module, voting, stranger)
@@ -74,3 +192,11 @@ def test_curated_module_update_target_validators_limits(voting, stranger):
 def test_simple_dvt_module_update_target_validators_limits(voting, stranger):
     staking_module = interface.SimpleDVT(SIMPLE_DVT)
     update_target_validators_limits(staking_module, voting, stranger)
+
+def test_curated_module_decrease_vetted_signing_keys_count(voting, stranger):
+    staking_module = interface.NodeOperatorsRegistry(NODE_OPERATORS_REGISTRY)
+    decrease_vetted_signing_keys_count(staking_module, voting, stranger)
+
+def test_simple_dvt_module_decrease_vetted_signing_keys_count(voting, stranger):
+    staking_module = interface.SimpleDVT(SIMPLE_DVT)
+    decrease_vetted_signing_keys_count(staking_module, voting, stranger)
