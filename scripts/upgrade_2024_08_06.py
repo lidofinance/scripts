a2cace0ac11ea37c1a116160a41122be0c346038
phonktown <ruslankhorolets@gmail.com>
2024-08-08 17:14:59 +0300

    feat(Next vote): Add oracle rotation

--- scripts/upgrade_2024_08_06.py
+++ scripts/upgrade_2024_08_06.py
@@ -1,6 +1,13 @@
 """
 Voting 06/08/2024.
 
+I. Replacing Rated Labs with MatrixedLink in Lido on Ethereum Oracle set
+1. Remove the oracle member named 'Rated Labs' with address 0xec4bfbaf681eb505b94e4a7849877dc6c600ca3a from HashConsensus for AccountingOracle on Lido on Ethereum
+2. Remove the oracle member named 'Rated Labs' with address 0xec4bfbaf681eb505b94e4a7849877dc6c600ca3a from HashConsensus for ValidatorsExitBusOracle on Lido on Ethereum
+3. Add oracle member named 'MatrixedLink' with address 0xe57B3792aDCc5da47EF4fF588883F0ee0c9835C9 to HashConsensus for AccountingOracle on Lido on Ethereum Oracle set
+4. Add oracle member named 'MatrixedLink' with address 0xe57B3792aDCc5da47EF4fF588883F0ee0c9835C9 to HashConsensus for ValidatorsExitBusOracle on Lido on Ethereum Oracle set
+
+II. Simple Delegation
 1. Push new Voting app version to the Voting Repo 0x4ee3118e3858e8d7164a634825bfe0f73d99c792
 2. Upgrade the Aragon Voting contract implementation 0xf165148978Fa3cE74d76043f833463c340CFB704
 3. Upgrade TRP voting adapter 0x4b2AB543FA389Ca8528656282bF0011257071BED
@@ -9,7 +16,8 @@ Voting 06/08/2024.
 
 import time
 
-from typing import Dict
+from typing import Dict, Tuple
+from brownie import interface
 from brownie.network.transaction import TransactionReceipt
 from utils.voting import bake_vote_items, confirm_vote_script, create_vote
 from utils.ipfs import upload_vote_ipfs_description, calculate_vote_ipfs_description
@@ -23,6 +31,30 @@ from utils.repo import add_implementation_to_voting_app_repo
 from utils.kernel import update_app_implementation
 from utils.agent import agent_forward
 
+def encode_remove_accounting_oracle_member(member: str, quorum: int) -> Tuple[str, str]:
+    hash_consensus: interface.LidoOracle = contracts.hash_consensus_for_accounting_oracle
+
+    return (hash_consensus.address, hash_consensus.removeMember.encode_input(member, quorum))
+
+def encode_remove_validators_exit_bus_oracle_member(member: str, quorum: int) -> Tuple[str, str]:
+    hash_consensus: interface.LidoOracle = contracts.hash_consensus_for_validators_exit_bus_oracle
+
+    return (hash_consensus.address, hash_consensus.removeMember.encode_input(member, quorum))
+
+
+def encode_add_accounting_oracle_member(member: str, quorum: int) -> Tuple[str, str]:
+    hash_consensus: interface.LidoOracle = contracts.hash_consensus_for_accounting_oracle
+
+    return (hash_consensus.address, hash_consensus.addMember.encode_input(member, quorum))
+
+
+def encode_add_validators_exit_bus_oracle_member(member: str, quorum: int) -> Tuple[str, str]:
+    hash_consensus: interface.LidoOracle = contracts.hash_consensus_for_validators_exit_bus_oracle
+
+    return (hash_consensus.address, hash_consensus.addMember.encode_input(member, quorum))
+
+
+
 updated_trp_voting_adapter = "0x4b2AB543FA389Ca8528656282bF0011257071BED"
 
 updated_voting_app = {
@@ -33,16 +65,66 @@ updated_voting_app = {
 }
 
 description = """
-Simple delegation Voting Upgrade
+1. Replacement of Rated Labs with MatrixedLink in Lido on Ethereum Oracle set. [Snapshot vote](https://snapshot.org/#/lido-snapshot.eth/proposal/0x5667528b50af1668ea246bde5bbf136f202629dee50747bbcc0839f48bf396b1).
+2. Simple delegation Voting Upgrade
 """
+
+HASH_CONSENSUS_FOR_ACCOUNTING_ORACLE_QUORUM = 5
+HASH_CONSENSUS_FOR_VALIDATORS_EXIT_BUS_ORACLE_QUORUM = 5
 
 
 def start_vote(tx_params: Dict[str, str], silent: bool) -> bool | list[int | TransactionReceipt | None]:
     """Prepare and run voting."""
 
+    rated_labs_oracle_member = "0xec4bfbaf681eb505b94e4a7849877dc6c600ca3a"
+    matrixed_link_oracle_member = "0xe57B3792aDCc5da47EF4fF588883F0ee0c9835C9"
+
     vote_desc_items, call_script_items = zip(
         #
-        # I. Simple Delegation
+        # I. Replacement in the Lido Oracle set
+        #
+        (
+            "1) Remove the oracle member named 'Rated Labs' with address 0xec4bfbaf681eb505b94e4a7849877dc6c600ca3a from HashConsensus for AccountingOracle on Lido on Ethereum",
+             agent_forward(
+                [
+                    encode_remove_accounting_oracle_member(
+                        rated_labs_oracle_member, HASH_CONSENSUS_FOR_ACCOUNTING_ORACLE_QUORUM
+                    )
+                ],
+            ),
+        ),
+        (
+            "2) Remove the oracle member named 'Rated Labs' with address 0xec4bfbaf681eb505b94e4a7849877dc6c600ca3a from HashConsensus for ValidatorsExitBusOracle on Lido on Ethereum",
+             agent_forward(
+                [
+                    encode_remove_validators_exit_bus_oracle_member(
+                        rated_labs_oracle_member, HASH_CONSENSUS_FOR_ACCOUNTING_ORACLE_QUORUM
+                    )
+                ],
+            ),
+        ),
+        (
+            "3) Add oracle member named 'MatrixedLink' with address 0xe57B3792aDCc5da47EF4fF588883F0ee0c9835C9 to HashConsensus for AccountingOracle on Lido on Ethereum Oracle set",
+            agent_forward(
+                [
+                    encode_add_accounting_oracle_member(
+                        matrixed_link_oracle_member, HASH_CONSENSUS_FOR_ACCOUNTING_ORACLE_QUORUM
+                    ),
+                ]
+            ),
+        ),
+        (
+            "4) Add oracle member named 'MatrixedLink' with address 0xe57B3792aDCc5da47EF4fF588883F0ee0c9835C9 to HashConsensus for ValidatorsExitBusOracle on Lido on Ethereum Oracle set",
+            agent_forward(
+                [
+                    encode_add_validators_exit_bus_oracle_member(
+                        matrixed_link_oracle_member, HASH_CONSENSUS_FOR_VALIDATORS_EXIT_BUS_ORACLE_QUORUM
+                    ),
+                ]
+            ),
+        ),
+        #
+        # II. Simple Delegation
         #
         (
             "1) Push new Voting app version to the Voting Repo",
--- tests/test_upgrade_2024_08_06.py
+++ tests/test_upgrade_2024_08_06.py
@@ -14,8 +14,15 @@ from utils.config import (
     LDO_HOLDER_ADDRESS_FOR_TESTS,
     VOTING,
 )
+from utils.test.event_validators.hash_consensus import (
+    validate_hash_consensus_member_removed,
+    validate_hash_consensus_member_added,
+)
 from utils.test.event_validators.aragon import validate_push_to_repo_event, validate_app_update_event
 from utils.test.tx_tracing_helpers import *
+
+HASH_CONSENSUS_FOR_ACCOUNTING_ORACLE_QUORUM=5
+HASH_CONSENSUS_FOR_VALIDATORS_EXIT_BUS_ORACLE_QUORUM=5
 
 old_trp_voting_adapter_address = "0xCFda8aB0AE5F4Fa33506F9C51650B890E4871Cc1"
 
@@ -37,6 +44,25 @@ updated_voting_app = {
 
 
 def test_vote(helpers, vote_ids_from_env, bypass_events_decoding):
+
+    rated_labs_oracle_member = "0xec4bfbaf681eb505b94e4a7849877dc6c600ca3a"
+    matrixed_link_oracle_member = "0xe57B3792aDCc5da47EF4fF588883F0ee0c9835C9"
+
+
+    # Validate current oracle set state
+
+    accounting_hash_consensus = contracts.hash_consensus_for_accounting_oracle
+    validators_exit_bus_hash_consensus = contracts.hash_consensus_for_validators_exit_bus_oracle
+
+    assert accounting_hash_consensus.getIsMember(rated_labs_oracle_member)
+    assert validators_exit_bus_hash_consensus.getIsMember(rated_labs_oracle_member)
+
+    assert not accounting_hash_consensus.getIsMember(matrixed_link_oracle_member)
+    assert not validators_exit_bus_hash_consensus.getIsMember(matrixed_link_oracle_member)
+
+    assert accounting_hash_consensus.getQuorum() == HASH_CONSENSUS_FOR_ACCOUNTING_ORACLE_QUORUM
+    assert validators_exit_bus_hash_consensus.getQuorum() == HASH_CONSENSUS_FOR_VALIDATORS_EXIT_BUS_ORACLE_QUORUM
+
 
     # Voting App before
     voting_proxy = interface.AppProxyUpgradeable(contracts.voting.address)
@@ -82,8 +108,29 @@ def test_vote(helpers, vote_ids_from_env, bypass_events_decoding):
         trp_voting_adapter.delegation_contract_addr()
     assert trp_voting_adapter.voting_contract_addr() == VOTING
 
+    # Replacing Rated Labs with MatrixedLink in Lido on Ethereum Oracle set
+
+    # 1) Remove the oracle member named 'Rated Labs' with address
+    #    0xec4bfbaf681eb505b94e4a7849877dc6c600ca3a from HashConsensus for AccountingOracle on Lido on Ethereum
+    assert not accounting_hash_consensus.getIsMember(rated_labs_oracle_member)
+
+    # 2) Remove the oracle member named 'Rated Labs' with address
+    #    0xec4bfbaf681eb505b94e4a7849877dc6c600ca3a from HashConsensus for ValidatorsExitBusOracle on Lido on Ethereum
+    assert not validators_exit_bus_hash_consensus.getIsMember(rated_labs_oracle_member)
+
+    # 5. Add oracle member named 'MatrixedLink' with address
+    #    0x1d0813bf088be3047d827d98524fbf779bc25f00 to HashConsensus for AccountingOracle on Lido on Ethereum Oracle set
+    assert accounting_hash_consensus.getIsMember(matrixed_link_oracle_member)
+
+    # 6. Add oracle member named 'MatrixedLink' with address
+    #    0x1d0813bf088be3047d827d98524fbf779bc25f00 to HashConsensus for ValidatorsExitBusOracle on Lido on Ethereum Oracle set
+    assert validators_exit_bus_hash_consensus.getIsMember(matrixed_link_oracle_member)
+
+    assert accounting_hash_consensus.getQuorum() == HASH_CONSENSUS_FOR_ACCOUNTING_ORACLE_QUORUM
+    assert validators_exit_bus_hash_consensus.getQuorum() == HASH_CONSENSUS_FOR_VALIDATORS_EXIT_BUS_ORACLE_QUORUM
+
     # Validating events
-    assert count_vote_items_by_events(vote_tx, contracts.voting) == 3, "Incorrect voting items count"
+    assert count_vote_items_by_events(vote_tx, contracts.voting) == 7, "Incorrect voting items count"
 
     # metadata = find_metadata_by_vote_id(vote_id)
     # assert get_lido_vote_cid_from_str(metadata) == "" # TODO: add ipfs cid
@@ -94,6 +141,20 @@ def test_vote(helpers, vote_ids_from_env, bypass_events_decoding):
 
     evs = group_voting_events(vote_tx)
 
-    validate_push_to_repo_event(evs[0], updated_voting_app["version"])
-    validate_app_update_event(evs[1], voting_appId, updated_voting_app["address"])
-    validate_voting_adapter_upgraded_event(evs[2], updated_trp_voting_adapter_address)
+    validate_hash_consensus_member_removed(
+        evs[0], rated_labs_oracle_member, HASH_CONSENSUS_FOR_ACCOUNTING_ORACLE_QUORUM, new_total_members=8
+    )
+    validate_hash_consensus_member_removed(
+        evs[1], rated_labs_oracle_member, HASH_CONSENSUS_FOR_VALIDATORS_EXIT_BUS_ORACLE_QUORUM, new_total_members=8
+    )
+    validate_hash_consensus_member_added(
+        evs[2], matrixed_link_oracle_member, HASH_CONSENSUS_FOR_ACCOUNTING_ORACLE_QUORUM, new_total_members=9
+    )
+    validate_hash_consensus_member_added(
+        evs[3], matrixed_link_oracle_member, HASH_CONSENSUS_FOR_VALIDATORS_EXIT_BUS_ORACLE_QUORUM, new_total_members=9
+    )
+
+    validate_push_to_repo_event(evs[4], updated_voting_app["version"])
+    validate_app_update_event(evs[5], voting_appId, updated_voting_app["address"])
+    validate_voting_adapter_upgraded_event(evs[6], updated_trp_voting_adapter_address)
+
