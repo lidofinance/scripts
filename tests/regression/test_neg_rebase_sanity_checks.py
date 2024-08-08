f218f96f8b12871eb01de2a609b8882b1614325e
VP <vp@lido.fi>
2024-08-08 21:19:59 +0200

    test: add more than 54 days of reports

--- tests/regression/test_neg_rebase_sanity_checks.py
+++ tests/regression/test_neg_rebase_sanity_checks.py
@@ -20,20 +20,41 @@ def test_negative_rebase_correct_exited_validators_count(oracle_report_sanity_ch
     locator = contracts.lido_locator
     assert oracle_report_sanity_checker.address == locator.oracleReportSanityChecker()
 
-    ids = contracts.staking_router.getStakingModuleIds()
-    exited = 0
-    for id in ids:
-        exited += contracts.staking_router.getStakingModule(id)["exitedValidatorsCount"]
-
-    reported_validators_count = exited + 2
+    reported_validators_count = exited_validators_count() + 2
     print(f'reported_validators_count: {reported_validators_count}')
 
     oracle_report(cl_diff=-ETH(40000), stakingModuleIdsWithNewlyExitedValidators=[1],
                    numExitedValidatorsByStakingModule=[reported_validators_count])
 
     count = oracle_report_sanity_checker.getReportDataCount()
-    print(f'count: {count}')
     assert count > 0
     (_, storedExitedValidators, _) = oracle_report_sanity_checker.reportData(count - 1)
 
     assert storedExitedValidators == reported_validators_count
+
+def test_negative_rebase_more_than_54_reports(oracle_report_sanity_checker):
+    locator = contracts.lido_locator
+    assert oracle_report_sanity_checker.address == locator.oracleReportSanityChecker()
+
+    reported_validators_count = exited_validators_count() + 2
+    print(f'reported_validators_count: {reported_validators_count}')
+
+    for _ in range(58):
+        reported_validators_count += 3
+        oracle_report(cl_diff=-ETH(400), stakingModuleIdsWithNewlyExitedValidators=[1],
+                       numExitedValidatorsByStakingModule=[reported_validators_count])
+
+    count = oracle_report_sanity_checker.getReportDataCount()
+    assert count > 0
+    (_, storedExitedValidators, _) = oracle_report_sanity_checker.reportData(count - 1)
+
+    assert storedExitedValidators == reported_validators_count
+
+
+def exited_validators_count():
+    ids = contracts.staking_router.getStakingModuleIds()
+    exited = 0
+    for id in ids:
+        exited += contracts.staking_router.getStakingModule(id)["exitedValidatorsCount"]
+
+    return exited
