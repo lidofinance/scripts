94ba87f4d0d3b3d995f70df89bc02b9b368f05ba
kate-aleksseeva <kate_aleksseeva@lido.fi>
2024-09-13 17:00:29 +0300

    new files: vote and tests

---
+++ scripts/vote_2024_10_01_holesky.py
@@ -0,0 +1,79 @@
+"""
+Voting 01/10/2024!
+Add ET setup for Alliance in stETH
+!! Holesky only
+"""
+
+import time
+
+from typing import Dict, Tuple, Optional, List
+
+from brownie import interface
+from brownie.network.transaction import TransactionReceipt
+from utils.voting import bake_vote_items, confirm_vote_script, create_vote
+from utils.easy_track import add_evmscript_factory, create_permissions
+from utils.permission_parameters import Param, SpecialArgumentID, ArgumentValue, Op
+from utils.ipfs import upload_vote_ipfs_description, calculate_vote_ipfs_description
+
+from utils.config import (
+    get_deployer_account,
+    contracts,
+    get_is_live,
+    get_priority_fee,
+)
+
+from utils.easy_track import (
+    add_evmscript_factory,
+    create_permissions,
+    remove_evmscript_factory
+)
+
+description = """
+1. **Add Alliance Ops stablecoins top up EVM script factory, as decided in the [Snapshot vote](https://snapshot.org/#/lido-snapshot.eth/proposal/0xa478fa5518769096eda2b7403a1d4104ca47de3102e8a9abab8640ef1b50650c).
+
+"""
+
+def start_vote(tx_params: Dict[str, str], silent: bool) -> bool | list[int | TransactionReceipt | None]:
+    """Prepare and run voting."""
+
+    alliance_ops_topup_factory = interface.TopUpAllowedRecipients("0x79e0286a8f60276293a7D29f53979642CcBD4Fff")
+    alliance_ops_registry = interface.AllowedRecipientRegistry("0x94499410c47FF7572560Ef8CBAf68bCdd0F38D08")
+     
+    vote_desc_items, call_script_items = zip(
+        #
+        # I. Add Alliance Ops stablecoins top up EVM script factory
+        #
+        (
+            "1) Add Alliance Ops stablecoins top up EVM script factory 0x79e0286a8f60276293a7D29f53979642CcBD4Fff", 
+            add_evmscript_factory(
+                factory=alliance_ops_topup_factory,
+                permissions=create_permissions(contracts.finance, "newImmediatePayment")
+                + create_permissions(alliance_ops_registry, "updateSpentAmount")[2:],
+            ),
+        ),
+    )
+
+    vote_items = bake_vote_items(list(vote_desc_items), list(call_script_items))
+    
+    if silent:
+        desc_ipfs = calculate_vote_ipfs_description(description)
+    else:
+        desc_ipfs = upload_vote_ipfs_description(description)
+
+    return confirm_vote_script(vote_items, silent, desc_ipfs) and list(
+        create_vote(vote_items, tx_params, desc_ipfs=desc_ipfs)
+    )
+
+
+def main():
+    tx_params = {"from": get_deployer_account()}
+
+    if get_is_live():
+        tx_params["priority_fee"] = get_priority_fee()
+
+    vote_id, _ = start_vote(tx_params=tx_params, silent=False)
+
+    vote_id >= 0 and print(f"Vote created: {vote_id}.")
+
+    time.sleep(5)  # hack for waiting thread #2.
+
