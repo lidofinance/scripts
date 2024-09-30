b80febb32c93d31b9f59a3bd4d18cd60fa8070d0
Ivan Feofanov <feofanov@gmail.com>
2024-09-30 16:48:56 +0200

    fix: node operator 33 is not exited anymore

--- tests/acceptance/test_simple_dvt_module.py
+++ tests/acceptance/test_simple_dvt_module.py
@@ -110,7 +110,7 @@ def test_simple_dvt_state(contract):
     assert summary["depositableValidatorsCount"] >= 0
 
     deactivated_node_operators = []  # reserved for future use
-    exited_node_operators = [33]  # reserved for future use
+    exited_node_operators = []  # reserved for future use
 
     for id in range(node_operators_count):
         node_operator = contract.getNodeOperator(id, True)
@@ -152,7 +152,6 @@ def test_simple_dvt_state(contract):
 
         assert node_operator["totalExitedValidators"] == node_operator_summary["totalExitedValidators"]
         assert node_operator["totalDepositedValidators"] == node_operator_summary["totalDepositedValidators"]
-
 
         if node_operator_summary["isTargetLimitActive"] == False:
             no_depositable_validators_count = (
