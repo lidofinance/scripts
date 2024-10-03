75f85a8cd78763b9c87e6fcf4f3a5fe5e088a93c
Anton Kovalchuk <koval400k@gmail.com>
2024-10-03 17:57:58 +0100

    check all events

--- tests/test_2024_10_08.py
+++ tests/test_2024_10_08.py
@@ -5,6 +5,7 @@ from scripts.upgrade_2024_10_08 import start_vote, encode_l2_upgrade_call
 from brownie import interface
 from brownie.exceptions import VirtualMachineError
 from utils.test.tx_tracing_helpers import *
+from utils.test.event_validators.common import validate_events_chain
 from utils.config import (
     contracts,
     AGENT,
@@ -16,6 +17,7 @@ LIDO_LOCATOR_IMPL_NEW="0x39aFE23cE59e8Ef196b81F0DCb165E9aD38b9463"
 
 DEPOSITS_ENABLER_ROLE="0x4b43b36766bde12c5e9cbbc37d15f8d1f769f08f54720ab370faeb4ce893753a"
 L1_EMERGENCY_BRAKES_MULTISIG="0x73b047fe6337183A454c5217241D780a932777bD"
+L2_OPTIMISM_GOVERNANCE_EXECUTOR = "0xefa0db536d2c8089685630fafe88cf7805966fc3"
 
 L1_OPTIMISM_TOKENS_BRIDGE="0x76943C0D61395d8F2edF9060e1533529cAe05dE6"
 L1_OPTIMISM_TOKENS_BRIDGE_IMPL="0x29C5c51A031165CE62F964966A6399b81165EFA4"
@@ -99,12 +101,58 @@ def check_post_upgrade_state(vote_tx):
     # Multisig has been assigned as deposit enabler
     assert l1_token_bridge.hasRole(DEPOSITS_ENABLER_ROLE, L1_EMERGENCY_BRAKES_MULTISIG)
 
-    # Check bytecode that was sent to messenger to update L2 bridge and wstETH token
-    sentMessage = vote_tx.events["SentMessage"]["message"]
-    encoded_l2_upgrade_call = encode_l2_upgrade_call(
+    display_voting_events(vote_tx)
+    evs = group_voting_events(vote_tx)
+
+    validate_contract_upgrade(evs[0], L1_OPTIMISM_TOKENS_BRIDGE_IMPL_NEW)
+    validate_contract_finalization(evs[1], 2)
+    validate_contract_upgrade(evs[2], LIDO_LOCATOR_IMPL_NEW)
+    validate_contract_role_granting(evs[3], DEPOSITS_ENABLER_ROLE, L1_EMERGENCY_BRAKES_MULTISIG, AGENT)
+    validate_optimism_upgrade_call(
+        evs[4],
+        L2_OPTIMISM_GOVERNANCE_EXECUTOR,
+        AGENT,
         L2_OPTIMISM_TOKENS_BRIDGE,
         L2_OPTIMISM_TOKENS_BRIDGE_IMPL_NEW,
         L2_OPTIMISM_WSTETH_TOKEN,
-        L2_OPTIMISM_WSTETH_TOKEN_IMPL_NEW,
+        L2_OPTIMISM_WSTETH_TOKEN_IMPL_NEW
     )
-    assert sentMessage == encoded_l2_upgrade_call
+
+def validate_contract_upgrade(events: EventDict, implementation: str):
+    _events_chain = ["LogScriptCall", "LogScriptCall", "Upgraded", "ScriptResult"]
+    validate_events_chain([e.name for e in events], _events_chain)
+    assert events.count("Upgraded") == 1
+    assert events["Upgraded"]["implementation"] == implementation, "Wrong implementation"
+
+def validate_contract_finalization(events: EventDict, version: int):
+    _events_chain = ["LogScriptCall", "LogScriptCall", "ContractVersionSet", "ScriptResult"]
+    validate_events_chain([e.name for e in events], _events_chain)
+    assert events.count("ContractVersionSet") == 1
+    assert events["ContractVersionSet"]["version"] == version
+
+def validate_contract_role_granting(events: EventDict, role: str, account: str, sender: str):
+    _events_chain = ["LogScriptCall", "LogScriptCall", "RoleGranted", "ScriptResult"]
+    validate_events_chain([e.name for e in events], _events_chain)
+    assert events.count("RoleGranted") == 1
+    assert events["RoleGranted"]["role"] == role
+    assert events["RoleGranted"]["account"] == account
+    assert events["RoleGranted"]["sender"] == sender
+
+def validate_optimism_upgrade_call(
+        events: EventDict,
+        target: str,
+        sender: str,
+        l2_bridge_proxy: str,
+        l2_bridge_impl: str,
+        l2_wst_proxy: str,
+        l2_wst_impl: str):
+    _events_chain = ["LogScriptCall", "LogScriptCall", "TransactionDeposited", "SentMessage", "SentMessageExtension1", "ScriptResult"]
+    validate_events_chain([e.name for e in events], _events_chain)
+    assert events.count("SentMessage") == 1
+    # Check bytecode that was sent to messenger to update L2 bridge and wstETH token
+    encoded_l2_upgrade_call = encode_l2_upgrade_call(l2_bridge_proxy, l2_bridge_impl, l2_wst_proxy, l2_wst_impl)
+    assert events["SentMessage"]["message"] == encoded_l2_upgrade_call
+    assert events["SentMessage"]["target"] == target
+    assert events["SentMessage"]["sender"] == sender
+
+
