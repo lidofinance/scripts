7a33a05ec7f93efbc41e810879f5603e4a2862d1
Anna Mukharram <amuhar3@gmail.com>
2024-10-15 03:24:21 +0400

    fix: fix contract addresses and prameters in a voting test

--- tests/test_upgrade_sr_v2.py
+++ tests/test_upgrade_sr_v2.py
@@ -1,52 +1,7 @@
 import pytest
 
-from utils.config import (
-    contracts,
-    STAKING_ROUTER_IMPL,
-    LIDO_LOCATOR_IMPL,
-    NODE_OPERATORS_REGISTRY_IMPL,
-    NODE_OPERATORS_REGISTRY_ARAGON_APP_ID,
-    NODE_OPERATORS_REGISTRY,
-    SIMPLE_DVT,
-    SIMPLE_DVT_ARAGON_APP_ID,
-    SIMPLE_DVT_IMPL,
-    ACCOUNTING_ORACLE_IMPL,
-    CS_ACCOUNTING_ADDRESS,
-    CSM_ADDRESS,
-)
-from scripts.upgrade_sr_v2 import (
-    start_vote,
-    # oracles
-    AO_CONSENSUS_VERSION,
-    VEBO_CONSENSUS_VERSION,
-    # nor
-    nor_uri,
-    CURATED_PRIORITY_EXIT_SHARE_THRESHOLDS,
-    CURATED_MAX_DEPOSITS_PER_BLOCK,
-    CURATED_MIN_DEPOSIT_BLOCK_DISTANCES,
-    # sdvt
-    sdvt_uri,
-    SDVT_PRIORITY_EXIT_SHARE_THRESHOLDS,
-    SDVT_MAX_DEPOSITS_PER_BLOCK,
-    SDVT_MIN_DEPOSIT_BLOCK_DISTANCES,
-    ## easy track
-    OLD_TARGET_LIMIT__FACTORY,
-    NEW_TARGET_LIMIT_FACTORY,
-    # CSM
-    CS_MODULE_NAME,
-    CS_STAKE_SHARE_LIMIT,
-    CS_PRIORITY_EXIT_SHARE_THRESHOLD,
-    CS_STAKING_MODULE_FEE,
-    CS_TREASURY_FEE,
-    CS_MAX_DEPOSITS_PER_BLOCK,
-    CS_MIN_DEPOSIT_BLOCK_DISTANCE,
-    CS_ORACLE_INITIAL_EPOCH,
-    EASYTRACK_CSM_SETTLE_EL_REWARDS_STEALING_PENALTY_FACTORY,
-)
-from utils.config import (
-    contracts,
-    LDO_HOLDER_ADDRESS_FOR_TESTS,
-)
+from scripts.upgrade_sr_v2 import start_vote
+from utils.config import LDO_HOLDER_ADDRESS_FOR_TESTS
 from brownie import interface, Contract
 from utils.test.tx_tracing_helpers import *
 from utils.test.event_validators.permission import validate_grant_role_event, validate_revoke_role_event
@@ -61,12 +16,67 @@ from utils.test.event_validators.easy_track import (
 )
 from utils.easy_track import create_permissions, create_permissions_overloaded
 
-# Impl addresses before vote
+# Addresses that will not be changed
+
+NODE_OPERATORS_REGISTRY = "0x55032650b14df07b85bF18A3a3eC8E0Af2e028d5"
+SIMPLE_DVT = "0xaE7B191A31f627b4eB1d4DaC64eaB9976995b433"
+LIDO_LOCATOR = "0xC1d0b3DE6792Bf6b4b37EccdcC24e45978Cfd2Eb"
+STAKING_ROUTER = "0xFdDf38947aFB03C621C71b06C9C70bce73f12999"
+ACCOUNTING_ORACLE = "0x852deD011285fe67063a08005c71a85690503Cee"
+VALIDATORS_EXIT_BUS_ORACLE = "0x0De4Ea0184c2ad0BacA7183356Aea5B8d5Bf5c6e"
+AGENT = "0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c"
+BURNER = "0xD15a672319Cf0352560eE76d9e89eAB0889046D3"
+VOTING = "0x2e59A20f205bB85a89C53f1936454680651E618e"
+
+# aragon repo
+NODE_OPERATORS_REGISTRY_REPO = "0x0D97E876ad14DB2b183CFeEB8aa1A5C788eB1831"
+SIMPLE_DVT_REPO = "0x2325b0a607808dE42D918DB07F925FFcCfBb2968"
+
+# Addresses before vote
 OLD_LOCATOR_IMPL_ADDRESS = "0x39aFE23cE59e8Ef196b81F0DCb165E9aD38b9463"
 OLD_SR_IMPL_ADDRESS = "0xD8784e748f59Ba711fB5643191Ec3fAdD50Fb6df"
 OLD_NOR_IMPL = "0x8538930c385C0438A357d2c25CB3eAD95Ab6D8ed"
 OLD_SDVT_IMPL = "0x8538930c385C0438A357d2c25CB3eAD95Ab6D8ed"
 OLD_ACCOUNTING_ORACLE_IMPL = "0xF3c5E0A67f32CF1dc07a8817590efa102079a1aF"
+DEPOSIT_SECURITY_MODULE_V2 = "0xC77F8768774E1c9244BEed705C4354f2113CFc09"
+
+# Addresses after vote
+NEW_LIDO_LOCATOR_IMPL = "0x3ABc4764f0237923d52056CFba7E9AEBf87113D3"
+NEW_STAKING_ROUTER_IMPL = "0x89eDa99C0551d4320b56F82DDE8dF2f8D2eF81aA"
+NEW_NODE_OPERATORS_REGISTRY_IMPL = "0x1770044a38402e3CfCa2Fcfa0C84a093c9B42135"
+NEW_SIMPLE_DVT_IMPL = "0x1770044a38402e3CfCa2Fcfa0C84a093c9B42135"
+NEW_ACCOUNTING_ORACLE_IMPL = "0x0e65898527E77210fB0133D00dd4C0E86Dc29bC7"
+DEPOSIT_SECURITY_MODULE_V3 = "0xffa96d84def2ea035c7ab153d8b991128e3d72fd"
+
+# Easy track sdvt
+OLD_TARGET_LIMIT_FACTORY = "0x41CF3DbDc939c5115823Fba1432c4EC5E7bD226C"
+NEW_TARGET_LIMIT_FACTORY = "0x161a4552A625844c822954C5AcBac928ee0f399B"
+
+# CSM
+CSM_ADDRESS = "0xdA7dE2ECdDfccC6c3AF10108Db212ACBBf9EA83F"
+CS_ACCOUNTING_ADDRESS = "0x4d72BFF1BeaC69925F8Bd12526a39BAAb069e5Da"
+CS_ORACLE_HASH_CONSENSUS_ADDRESS = "0x71093efF8D8599b5fA340D665Ad60fA7C80688e4"
+
+# Staking Router parameters
+nor_uri = "0x697066733a516d54346a64693146684d454b5576575351316877786e33365748394b6a656743755a7441684a6b6368526b7a70"
+CURATED_PRIORITY_EXIT_SHARE_THRESHOLDS = 10_000
+CURATED_MAX_DEPOSITS_PER_BLOCK = 150
+CURATED_MIN_DEPOSIT_BLOCK_DISTANCES = 25
+
+sdvt_uri = "0x697066733a516d615353756a484347636e4675657441504777565735426567614d42766e355343736769334c5366767261536f"
+SDVT_PRIORITY_EXIT_SHARE_THRESHOLDS = 444
+SDVT_MAX_DEPOSITS_PER_BLOCK = 150
+SDVT_MIN_DEPOSIT_BLOCK_DISTANCES = 25
+
+# aragor id
+NODE_OPERATORS_REGISTRY_ARAGON_APP_ID = "0x7071f283424072341f856ac9e947e7ec0eb68719f757a7e785979b6b8717579d"
+SIMPLE_DVT_ARAGON_APP_ID = "0xe1635b63b5f7b5e545f2a637558a4029dea7905361a2f0fc28c66e9136cf86a4"
+
+
+# Accounting oracle
+AO_CONSENSUS_VERSION = 2
+# Vebo
+VEBO_CONSENSUS_VERSION = 2
 
 # NOR
 ### RewardDistributionState
@@ -77,6 +87,20 @@ SR_VERSION = 2
 NOR_VERSION = 3
 SDVT_VERSION = 3
 AO_VERSION = 2
+
+# CSM parameters
+
+CS_MODULE_NAME = "Community Staking"
+CS_STAKE_SHARE_LIMIT = 100
+CS_PRIORITY_EXIT_SHARE_THRESHOLD = 125
+CS_STAKING_MODULE_FEE = 600
+CS_TREASURY_FEE = 400
+CS_MAX_DEPOSITS_PER_BLOCK = 30
+CS_MIN_DEPOSIT_BLOCK_DISTANCE = 25
+CS_ORACLE_INITIAL_EPOCH = 326715
+
+# CSM steaking penalty factory
+EASYTRACK_CSM_SETTLE_EL_REWARDS_STEALING_PENALTY_FACTORY = "0xF6B6E7997338C48Ea3a8BCfa4BB64a315fDa76f4"
 
 # Roles
 ## SR
@@ -190,14 +214,18 @@ def test_vote(
     accounts,
     vote_ids_from_env,
 ):
-    staking_router = contracts.staking_router
-    sr_proxy = interface.OssifiableProxy(contracts.staking_router)
-    locator_proxy = interface.OssifiableProxy(contracts.lido_locator)
-    nor_proxy = interface.AppProxyUpgradeable(contracts.node_operators_registry)
-    sdvt_proxy = interface.AppProxyUpgradeable(contracts.simple_dvt)
-    ao_proxy = interface.OssifiableProxy(contracts.accounting_oracle)
-    vebo_proxy = interface.ValidatorsExitBusOracle(contracts.validators_exit_bus_oracle)
+    sr_proxy = interface.OssifiableProxy(STAKING_ROUTER)
+    locator_proxy = interface.OssifiableProxy(LIDO_LOCATOR)
+    nor_proxy = interface.AppProxyUpgradeable(NODE_OPERATORS_REGISTRY)
+    sdvt_proxy = interface.AppProxyUpgradeable(SIMPLE_DVT)
+    ao_proxy = interface.OssifiableProxy(ACCOUNTING_ORACLE)
+    vebo = get_vebo()
+    burner = get_burner()
+    csm = get_csm()
+    csm_hash_consensus = get_csm_hash_consensus()
 
+    # TODO: staking router change
+    staking_router = get_staking_router()
     assert staking_router.getStakingModulesCount() == 2
 
     # Before voting tests
@@ -210,11 +238,13 @@ def test_vote(
     check_module_before_vote(CURATED_MODULE_BEFORE_VOTE)
     check_module_before_vote(SDVT_MODULE_BEFORE_VOTE)
     # NOR
-    nor_old_app = contracts.nor_app_repo.getLatest()
+    nor_app_repo = interface.Repo(NODE_OPERATORS_REGISTRY_REPO)
+    nor_old_app = nor_app_repo.getLatest()
     assert nor_proxy.implementation() == OLD_NOR_IMPL
     assert_repo_before_vote(nor_old_app, 4, OLD_NOR_IMPL, nor_uri)
     # SDVT
-    sdvt_old_app = contracts.simple_dvt_app_repo.getLatest()
+    sdvt_app_repo = interface.Repo(SIMPLE_DVT_REPO)
+    sdvt_old_app = sdvt_app_repo.getLatest()
     assert sdvt_proxy.implementation() == OLD_SDVT_IMPL
     assert_repo_before_vote(sdvt_old_app, 1, OLD_SDVT_IMPL, sdvt_uri)
     # AO
@@ -223,9 +253,9 @@ def test_vote(
     check_manage_consensus_role()
 
     # VEBO consensus version
-    assert vebo_proxy.getConsensusVersion() == VEBO_CONSENSUS_VERSION - 1
-    assert not contracts.burner.hasRole(REQUEST_BURN_SHARES_ROLE, CS_ACCOUNTING_ADDRESS)
-    assert not contracts.csm.hasRole(RESUME_ROLE, contracts.agent.address)
+    assert vebo.getConsensusVersion() == VEBO_CONSENSUS_VERSION - 1
+    assert not burner.hasRole(REQUEST_BURN_SHARES_ROLE, CS_ACCOUNTING_ADDRESS)
+    assert not csm.hasRole(RESUME_ROLE, AGENT)
 
     # START VOTE
     if len(vote_ids_from_env) > 0:
@@ -234,44 +264,47 @@ def test_vote(
         tx_params = {"from": LDO_HOLDER_ADDRESS_FOR_TESTS}
         vote_id, _ = start_vote(tx_params, silent=True)
 
-    vote_tx = helpers.execute_vote(accounts, vote_id, contracts.voting)
+    # TODO: do we need instead use contracts.voting that read address from config?
+    voting = get_voting()
+
+    vote_tx = helpers.execute_vote(accounts, vote_id, voting)
 
     print(f"voteId = {vote_id}, gasUsed = {vote_tx.gas_used}")
 
     assert staking_router.getStakingModulesCount() == 3
 
     # locator
-    check_ossifiable_proxy_impl(locator_proxy, LIDO_LOCATOR_IMPL)
+    check_ossifiable_proxy_impl(locator_proxy, NEW_LIDO_LOCATOR_IMPL)
     # DSM
     check_dsm_roles_after_vote()
     # SR
-    check_ossifiable_proxy_impl(sr_proxy, STAKING_ROUTER_IMPL)
+    check_ossifiable_proxy_impl(sr_proxy, NEW_STAKING_ROUTER_IMPL)
     check_module_after_vote(CURATED_MODULE_AFTER_VOTE)
     check_module_after_vote(SDVT_MODULE_AFTER_VOTE)
     # AO
-    check_ossifiable_proxy_impl(ao_proxy, ACCOUNTING_ORACLE_IMPL)
+    check_ossifiable_proxy_impl(ao_proxy, NEW_ACCOUNTING_ORACLE_IMPL)
 
     # no permission to manage consensus version on agent
     check_manage_consensus_role()
     # VEBO consensus version
-    assert vebo_proxy.getConsensusVersion() == VEBO_CONSENSUS_VERSION
+    assert vebo.getConsensusVersion() == VEBO_CONSENSUS_VERSION
 
-    assert contracts.burner.hasRole(REQUEST_BURN_SHARES_ROLE, CS_ACCOUNTING_ADDRESS)
-    assert not contracts.csm.hasRole(RESUME_ROLE, contracts.agent.address)
+    assert burner.hasRole(REQUEST_BURN_SHARES_ROLE, CS_ACCOUNTING_ADDRESS)
+    assert not csm.hasRole(RESUME_ROLE, AGENT)
 
-    assert contracts.csm_hash_consensus.getFrameConfig()[0] == CS_ORACLE_INITIAL_EPOCH
+    assert csm_hash_consensus.getFrameConfig()[0] == CS_ORACLE_INITIAL_EPOCH
 
     check_csm()
 
     # Events check
-    # events = group_voting_events(vote_tx)
-    events = group_voting_events_from_receipt(vote_tx)
+    display_voting_events(vote_tx)
+    events = group_voting_events(vote_tx)
 
     assert len(events) == 26
 
-    validate_upgrade_events(events[0], LIDO_LOCATOR_IMPL)
+    validate_upgrade_events(events[0], NEW_LIDO_LOCATOR_IMPL)
     validate_dsm_roles_events(events)
-    validate_upgrade_events(events[4], STAKING_ROUTER_IMPL)
+    validate_upgrade_events(events[4], NEW_STAKING_ROUTER_IMPL)
 
     validate_staking_module_update(
         events[5],
@@ -301,42 +334,38 @@ def test_vote(
         ],
     )
 
-    nor_new_app = contracts.nor_app_repo.getLatest()
-    assert_repo_update(nor_new_app, nor_old_app, NODE_OPERATORS_REGISTRY_IMPL, nor_uri)
+    nor_new_app = nor_app_repo.getLatest()
+    assert_repo_update(nor_new_app, nor_old_app, NEW_NODE_OPERATORS_REGISTRY_IMPL, nor_uri)
     validate_repo_upgrade_event(events[6], RepoUpgrade(6, nor_new_app[0]))
-    validate_app_update_event(events[7], NODE_OPERATORS_REGISTRY_ARAGON_APP_ID, NODE_OPERATORS_REGISTRY_IMPL)
+    validate_app_update_event(events[7], NODE_OPERATORS_REGISTRY_ARAGON_APP_ID, NEW_NODE_OPERATORS_REGISTRY_IMPL)
     validate_nor_update(events[8], NOR_VERSION)
 
-    sdvt_new_app = contracts.simple_dvt_app_repo.getLatest()
-    assert_repo_update(sdvt_new_app, sdvt_old_app, SIMPLE_DVT_IMPL, sdvt_uri)
+    sdvt_new_app = sdvt_app_repo.getLatest()
+    assert_repo_update(sdvt_new_app, sdvt_old_app, NEW_SIMPLE_DVT_IMPL, sdvt_uri)
     validate_repo_upgrade_event(events[9], RepoUpgrade(2, sdvt_new_app[0]))
-    validate_app_update_event(events[10], SIMPLE_DVT_ARAGON_APP_ID, SIMPLE_DVT_IMPL)
+    validate_app_update_event(events[10], SIMPLE_DVT_ARAGON_APP_ID, NEW_SIMPLE_DVT_IMPL)
     validate_nor_update(events[11], SDVT_VERSION)
 
     # AO
-    validate_upgrade_events(events[12], ACCOUNTING_ORACLE_IMPL)
+    validate_upgrade_events(events[12], NEW_ACCOUNTING_ORACLE_IMPL)
     validate_ao_update(events[13], AO_VERSION, AO_CONSENSUS_VERSION)
 
-    validate_grant_role_event(
-        events[14], MANAGE_CONSENSUS_VERSION_ROLE, contracts.agent.address, contracts.agent.address
-    )
+    validate_grant_role_event(events[14], MANAGE_CONSENSUS_VERSION_ROLE, AGENT, AGENT)
 
     validate_vebo_consensus_version_set(events[15])
 
-    validate_revoke_role_event(
-        events[16], MANAGE_CONSENSUS_VERSION_ROLE, contracts.agent.address, contracts.agent.address
-    )
+    validate_revoke_role_event(events[16], MANAGE_CONSENSUS_VERSION_ROLE, AGENT, AGENT)
 
-    validate_evmscript_factory_removed_event(events[17], OLD_TARGET_LIMIT__FACTORY)
+    validate_evmscript_factory_removed_event(events[17], OLD_TARGET_LIMIT_FACTORY)
+
+    simple_dvt = get_simple_dvt()
 
     validate_evmscript_factory_added_event(
         events[18],
         EVMScriptFactoryAdded(
             factory_addr=NEW_TARGET_LIMIT_FACTORY,
             permissions=(
-                create_permissions_overloaded(
-                    contracts.simple_dvt, "updateTargetValidatorsLimits", "uint256,uint256,uint256"
-                )
+                create_permissions_overloaded(simple_dvt, "updateTargetValidatorsLimits", "uint256,uint256,uint256")
             ),
         ),
     )
@@ -355,21 +384,57 @@ def test_vote(
             CSM_AFTER_VOTE["minDepositBlockDistance"],
         ),
     )
-    validate_grant_role_event(events[20], REQUEST_BURN_SHARES_ROLE, CS_ACCOUNTING_ADDRESS, contracts.agent.address)
-    validate_grant_role_event(events[21], RESUME_ROLE, contracts.agent.address, contracts.agent.address)
+    validate_grant_role_event(events[20], REQUEST_BURN_SHARES_ROLE, CS_ACCOUNTING_ADDRESS, AGENT)
+    validate_grant_role_event(events[21], RESUME_ROLE, AGENT, AGENT)
 
     validate_resume_event(events[22])
-    validate_revoke_role_event(events[23], RESUME_ROLE, contracts.agent.address, contracts.agent.address)
+    validate_revoke_role_event(events[23], RESUME_ROLE, AGENT, AGENT)
     validate_updateInitial_epoch(events[24])
 
     validate_evmscript_factory_added_event(
         events[25],
         EVMScriptFactoryAdded(
             factory_addr=EASYTRACK_CSM_SETTLE_EL_REWARDS_STEALING_PENALTY_FACTORY,
-            permissions=(create_permissions(contracts.csm, "settleELRewardsStealingPenalty")),
+            permissions=(create_permissions(csm, "settleELRewardsStealingPenalty")),
         ),
         ["LogScriptCall", "EVMScriptFactoryAdded", "ScriptResult", "ExecuteVote"],
     )
+
+
+def get_staking_router():
+    return interface.StakingRouter(STAKING_ROUTER)
+
+
+def get_deposit_security_module_v3():
+    return interface.DepositSecurityModule(DEPOSIT_SECURITY_MODULE_V3)
+
+
+def get_deposit_security_module_v2():
+    return interface.DepositSecurityModule(DEPOSIT_SECURITY_MODULE_V2)
+
+
+def get_vebo():
+    return interface.ValidatorsExitBusOracle(VALIDATORS_EXIT_BUS_ORACLE)
+
+
+def get_simple_dvt():
+    return interface.SimpleDVT(SIMPLE_DVT)
+
+
+def get_burner():
+    return interface.Burner(BURNER)
+
+
+def get_csm():
+    return interface.CSModule(CSM_ADDRESS)
+
+
+def get_voting():
+    return interface.Voting(VOTING)
+
+
+def get_csm_hash_consensus():
+    return interface.CSHashConsensus(CS_ORACLE_HASH_CONSENSUS_ADDRESS)
 
 
 def check_ossifiable_proxy_impl(proxy, expected_impl):
@@ -378,47 +443,44 @@ def check_ossifiable_proxy_impl(proxy, expected_impl):
 
 
 def check_dsm_roles_before_vote():
-    new_dsm_doesnt_have_unvet_role = contracts.staking_router.hasRole(
-        STAKING_MODULE_UNVETTING_ROLE, contracts.deposit_security_module
-    )
+    staking_router = get_staking_router()
+    deposit_security_module_v2 = get_deposit_security_module_v2()
+    deposit_security_module_v3 = get_deposit_security_module_v3()
+    new_dsm_doesnt_have_unvet_role = staking_router.hasRole(STAKING_MODULE_UNVETTING_ROLE, deposit_security_module_v3)
     assert not new_dsm_doesnt_have_unvet_role
 
-    old_dsm_has_pause_role = contracts.staking_router.hasRole(PAUSE_ROLE, contracts.deposit_security_module_v2)
+    old_dsm_has_pause_role = staking_router.hasRole(PAUSE_ROLE, deposit_security_module_v2)
 
     assert old_dsm_has_pause_role
 
-    old_dsm_has_resume_role = contracts.staking_router.hasRole(
-        STAKING_MODULE_RESUME_ROLE, contracts.deposit_security_module_v2
-    )
+    old_dsm_has_resume_role = staking_router.hasRole(STAKING_MODULE_RESUME_ROLE, deposit_security_module_v2)
 
     assert old_dsm_has_resume_role
 
 
 def check_dsm_roles_after_vote():
-    new_dsm_has_unvet_role = contracts.staking_router.hasRole(
-        STAKING_MODULE_UNVETTING_ROLE, contracts.deposit_security_module
-    )
+    staking_router = get_staking_router()
+    deposit_security_module_v2 = get_deposit_security_module_v2()
+    deposit_security_module_v3 = get_deposit_security_module_v3()
+    new_dsm_has_unvet_role = staking_router.hasRole(STAKING_MODULE_UNVETTING_ROLE, deposit_security_module_v3)
     assert new_dsm_has_unvet_role
 
-    old_dsm_doesnt_have_pause_role = contracts.staking_router.hasRole(PAUSE_ROLE, contracts.deposit_security_module_v2)
+    old_dsm_doesnt_have_pause_role = staking_router.hasRole(PAUSE_ROLE, deposit_security_module_v2)
     assert not old_dsm_doesnt_have_pause_role
 
-    old_dsm_doesnt_have_resume_role = contracts.staking_router.hasRole(
-        STAKING_MODULE_RESUME_ROLE, contracts.deposit_security_module_v2
-    )
+    old_dsm_doesnt_have_resume_role = staking_router.hasRole(STAKING_MODULE_RESUME_ROLE, deposit_security_module_v2)
     assert not old_dsm_doesnt_have_resume_role
 
 
 def check_manage_consensus_role():
-    agent_has_manage_consensus_role = contracts.validators_exit_bus_oracle.hasRole(
-        MANAGE_CONSENSUS_VERSION_ROLE, contracts.agent.address
-    )
+    vebo = get_vebo()
+    agent_has_manage_consensus_role = vebo.hasRole(MANAGE_CONSENSUS_VERSION_ROLE, AGENT)
     assert not agent_has_manage_consensus_role
 
 
 def check_module_before_vote(expected_module_data: Dict):
-    sr_contract = Contract.from_abi("StakingContract", contracts.staking_router.address, OLD_SR_ABI)
-    module = sr_contract.getStakingModule(expected_module_data["id"])
+    staking_router = Contract.from_abi("StakingContract", STAKING_ROUTER, OLD_SR_ABI)
+    module = staking_router.getStakingModule(expected_module_data["id"])
     assert module["id"] == expected_module_data["id"]
     assert module["stakingModuleFee"] == expected_module_data["stakingModuleFee"]
     assert module["treasuryFee"] == expected_module_data["treasuryFee"]
@@ -429,7 +491,8 @@ def check_module_before_vote(expected_module_data: Dict):
 
 
 def check_module_after_vote(expected_module_data: Dict):
-    module = contracts.staking_router.getStakingModule(expected_module_data["id"])
+    staking_router = get_staking_router()
+    module = staking_router.getStakingModule(expected_module_data["id"])
     assert module["id"] == expected_module_data["id"]
     assert module["stakingModuleFee"] == expected_module_data["stakingModuleFee"]
     assert module["treasuryFee"] == expected_module_data["treasuryFee"]
@@ -465,20 +528,23 @@ def validate_upgrade_events(events: EventDict, implementation: str):
 
 def validate_dsm_roles_events(events: list[EventDict]):
     validate_revoke_role_event(
-        events[1], PAUSE_ROLE, contracts.deposit_security_module_v2.address, contracts.agent.address
+        events[1],
+        PAUSE_ROLE,
+        DEPOSIT_SECURITY_MODULE_V2,
+        AGENT,
     )
     validate_revoke_role_event(
         events[2],
         STAKING_MODULE_RESUME_ROLE,
-        contracts.deposit_security_module_v2.address,
-        contracts.agent.address,
+        DEPOSIT_SECURITY_MODULE_V2,
+        AGENT,
     )
 
     validate_grant_role_event(
         events[3],
         STAKING_MODULE_UNVETTING_ROLE,
-        contracts.deposit_security_module.address,
-        contracts.agent.address,
+        DEPOSIT_SECURITY_MODULE_V3,
+        AGENT,
     )
 
 
@@ -625,7 +691,8 @@ def validate_module_add(event, csm: StakingModuleItem):
 
 
 def check_csm():
-    cs = contracts.staking_router.getStakingModule(3)
+    staking_router = get_staking_router()
+    cs = staking_router.getStakingModule(3)
 
     assert cs["name"] == CSM_AFTER_VOTE["name"]
     assert cs["stakingModuleFee"] == CSM_AFTER_VOTE["stakingModuleFee"]
