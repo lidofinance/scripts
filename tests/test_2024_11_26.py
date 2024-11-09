b8c7d2bdcc57f530d68b5c80fbc610afb019882f
Nikita P <nikita.p@lido.fi>
2024-11-09 13:12:12 +0000

    feat: more tests

--- tests/test_2024_11_26.py
+++ tests/test_2024_11_26.py
@@ -3,30 +3,23 @@ Tests for voting 26/11/2024.
 """
 
 from scripts.vote_2024_11_26 import start_vote
-from brownie import interface, ZERO_ADDRESS, reverts, web3, accounts, convert
+from brownie import interface, reverts
 from utils.test.tx_tracing_helpers import *
-from utils.voting import find_metadata_by_vote_id
-from utils.ipfs import get_lido_vote_cid_from_str
-from utils.config import contracts, LDO_HOLDER_ADDRESS_FOR_TESTS, network_name
-from utils.easy_track import create_permissions
+from utils.config import contracts, LDO_HOLDER_ADDRESS_FOR_TESTS
 from utils.config import contracts
-from utils.test.easy_track_helpers import create_and_enact_payment_motion, check_add_and_remove_recipient_with_voting
-from utils.test.event_validators.easy_track import (
-    validate_evmscript_factory_added_event,
-    EVMScriptFactoryAdded,
-    validate_evmscript_factory_removed_event,
-)
+from utils.test.easy_track_helpers import create_and_enact_payment_motion
 from utils.test.event_validators.allowed_recipients_registry import (
-    validate_set_limit_parameter_event
+    validate_set_limit_parameter_event,
+    validate_set_spent_amount_event,
 )
-from configs.config_mainnet import (
-    DAI_TOKEN,
-    USDC_TOKEN,
-    USDT_TOKEN,
+from utils.test.event_validators.node_operators_registry import (
+    validate_node_operator_reward_address_set_event,
+    NodeOperatorRewardAddressSetItem
 )
+from configs.config_mainnet import ( USDC_TOKEN )
 
 
-def test_vote(helpers, accounts, vote_ids_from_env, stranger, ldo_holder, bypass_events_decoding):
+def test_vote(helpers, accounts, vote_ids_from_env, stranger):
 
     easy_track = interface.EasyTrack("0xF0211b7660680B49De1A7E9f25C65660F0a13Fea")
 
@@ -42,9 +35,19 @@ def test_vote(helpers, accounts, vote_ids_from_env, stranger, ldo_holder, bypass
 
     # Item 2
     pml_allowed_recipients_registry = interface.AllowedRecipientRegistry("0xDFfCD3BF14796a62a804c1B16F877Cf7120379dB")
+    pml_multisig_acc = accounts.at("0x17F6b2C738a63a8D3A113a228cfd0b373244633D", force=True)
+    pml_trusted_caller_acc = pml_multisig_acc
+    pml_top_up_evm_script_factory = interface.TopUpAllowedRecipients("0x92a27C4e5e35cFEa112ACaB53851Ec70e2D99a8D")
+    pmlBudgetLimitAfterExpected = 4_000_000 * 10**18
+    pmlSpendLimitAfterExpected = 3_000_000 * 10**18
 
     # Item 3, 4
     tmc_allowed_recipients_registry = interface.AllowedRecipientRegistry("0x1a7cFA9EFB4D5BfFDE87B0FaEb1fC65d653868C0")
+    tmc_multisig = accounts.at("0xa02FC823cCE0D016bD7e17ac684c9abAb2d6D647", force=True)
+    tmc_trusted_caller = tmc_multisig
+    tmc_top_up_evm_script_factory = interface.TopUpAllowedRecipients("0x6e04aED774B7c89BB43721AcDD7D03C872a51B69")
+    stETH_token = interface.ERC20("0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84")
+    stonks_steth_contract = accounts.at("0x3e2D251275A92a8169A3B17A2C49016e2de492a7", force=True)
 
     # Item 5
     # NO's data indexes
@@ -64,11 +67,13 @@ def test_vote(helpers, accounts, vote_ids_from_env, stranger, ldo_holder, bypass
     atcBudgetLimitBefore, atcPeriodDurationMonthsBefore = interface.AllowedRecipientRegistry(atc_allowed_recipients_registry).getLimitParameters()
     assert atcBudgetLimitBefore == 1_500_000 * 10 ** 18
     assert atcPeriodDurationMonthsBefore == 3
+    assert 0 == interface.AllowedRecipientRegistry(atc_allowed_recipients_registry).spendableBalance()
 
     # Item 2
     pmlBudgetLimitBefore, pmlPeriodDurationMonthsBefore = interface.AllowedRecipientRegistry(pml_allowed_recipients_registry).getLimitParameters()
     assert pmlBudgetLimitBefore == 6_000_000 * 10 ** 18
     assert pmlPeriodDurationMonthsBefore == 3
+    assert 5_000_000 * 10**18 == interface.AllowedRecipientRegistry(pml_allowed_recipients_registry).spendableBalance()
 
     # Item 3
     tmcBudgetLimitBefore, tmcPeriodDurationMonthsBefore = interface.AllowedRecipientRegistry(tmc_allowed_recipients_registry).getLimitParameters()
@@ -97,31 +102,69 @@ def test_vote(helpers, accounts, vote_ids_from_env, stranger, ldo_holder, bypass
 
     # Item 1
     atcBudgetLimitAfter, atcPeriodDurationMonthsAfter = interface.AllowedRecipientRegistry(atc_allowed_recipients_registry).getLimitParameters()
-    (already_spent, spend_limit, period_start, period_end) = interface.AllowedRecipientRegistry(atc_allowed_recipients_registry).getPeriodState()
+    atc_spend_limit_after = interface.AllowedRecipientRegistry(atc_allowed_recipients_registry).getPeriodState()[1]
     atcSpendableBalanceAfter = interface.AllowedRecipientRegistry(atc_allowed_recipients_registry).spendableBalance()
     assert atcBudgetLimitAfter == atcBudgetLimitAfterExpected
     assert atcPeriodDurationMonthsAfter == 3
-    assert spend_limit == atcSpendLimitAfterExpected
+    assert atc_spend_limit_after == atcSpendLimitAfterExpected
     assert interface.AllowedRecipientRegistry(atc_allowed_recipients_registry).isUnderSpendableBalance(atcSpendableBalanceAfter, 3)
     assert atcSpendableBalanceAfter == atcSpendLimitAfterExpected
     limit_test(easy_track, int(atcSpendableBalanceAfter / (10**18)), atc_trusted_caller_acc, atc_top_up_evm_script_factory, atc_multisig_acc, stranger)
 
     # Item 2
     pmlBudgetLimitAfter, pmlPeriodDurationMonthsAfter = interface.AllowedRecipientRegistry(pml_allowed_recipients_registry).getLimitParameters()
-    assert pmlBudgetLimitAfter == 4_000_000 * 10 ** 18
+    pml_spend_limit_after = interface.AllowedRecipientRegistry(pml_allowed_recipients_registry).getPeriodState()[1]
+    pmlSpendableBalanceAfter = interface.AllowedRecipientRegistry(pml_allowed_recipients_registry).spendableBalance()
+    assert pmlBudgetLimitAfter == pmlBudgetLimitAfterExpected
     assert pmlPeriodDurationMonthsAfter == 3
-    # TODO limit_test
+    assert pml_spend_limit_after == pmlSpendLimitAfterExpected
+    assert interface.AllowedRecipientRegistry(pml_allowed_recipients_registry).isUnderSpendableBalance(pmlSpendableBalanceAfter, 3)
+    assert pmlSpendableBalanceAfter == pmlSpendLimitAfterExpected
+    # TODO
+    #limit_test(easy_track, int(pmlSpendableBalanceAfter / (10**18)), pml_trusted_caller_acc, pml_top_up_evm_script_factory, pml_multisig_acc, stranger)
 
     # Item 3
     tmcBudgetLimitAfter, tmcPeriodDurationMonthsAfter = interface.AllowedRecipientRegistry(tmc_allowed_recipients_registry).getLimitParameters()
     assert tmcBudgetLimitAfter == 12_000 * 10 ** 18
     assert tmcPeriodDurationMonthsAfter == 6
-    # TODO limit_test
 
     # Item 4
     alreadySpentAmountAfter, spendableBalanceInPeriodAfter, periodStartTimestampAfter, periodEndTimestampAfter = tmc_allowed_recipients_registry.getPeriodState()
     assert alreadySpentAmountAfter == 0
-    # TODO limit_test
+    tmcSpendableBalanceAfter = interface.AllowedRecipientRegistry(tmc_allowed_recipients_registry).spendableBalance()
+    assert tmcSpendableBalanceAfter == 12_000 * 10 ** 18
+    with reverts("SUM_EXCEEDS_SPENDABLE_BALANCE"):
+        create_and_enact_payment_motion(
+            easy_track,
+            tmc_trusted_caller,
+            tmc_top_up_evm_script_factory,
+            stETH_token,
+            [stonks_steth_contract],
+            [12_000 * 10 ** 18 + 1],
+            stranger,
+        )
+
+    for i in range (0, 12):
+        create_and_enact_payment_motion(
+            easy_track,
+            tmc_trusted_caller,
+            tmc_top_up_evm_script_factory,
+            stETH_token,
+            [stonks_steth_contract],
+            [1_000 * 10 ** 18],
+            stranger,
+        )
+
+    with reverts("SUM_EXCEEDS_SPENDABLE_BALANCE"):
+        create_and_enact_payment_motion(
+            easy_track,
+            tmc_trusted_caller,
+            tmc_top_up_evm_script_factory,
+            stETH_token,
+            [stonks_steth_contract],
+            [1],
+            stranger,
+        )
 
     # Item 5
     SimplyStakingDataAfter = nor.getNodeOperator(SimplyStakingId, True)
@@ -130,7 +173,6 @@ def test_vote(helpers, accounts, vote_ids_from_env, stranger, ldo_holder, bypass
     assert SimplyStakingDataBefore[activeIndex] == SimplyStakingDataAfter[activeIndex]
     assert SimplyStakingDataBefore[stakingLimitIndex] == SimplyStakingDataAfter[stakingLimitIndex]
     assert SimplyStakingDataBefore[stoppedValidatorsIndex] == SimplyStakingDataAfter[stoppedValidatorsIndex]
-    # TODO limit_test
 
 
     # events
@@ -143,7 +185,29 @@ def test_vote(helpers, accounts, vote_ids_from_env, stranger, ldo_holder, bypass
         period_duration_month=3,
         period_start_timestamp=1727740800,
     )
-    # TODO more events
+    validate_set_limit_parameter_event(
+        evs[1],
+        limit=pmlBudgetLimitAfterExpected,
+        period_duration_month=3,
+        period_start_timestamp=1727740800,
+    )
+    validate_set_limit_parameter_event(
+        evs[2],
+        limit=12_000 * 10 ** 18,
+        period_duration_month=6,
+        period_start_timestamp=1719792000,
+    )
+    validate_set_spent_amount_event(
+        evs[3],
+        new_spent_amount=0,
+    )
+    validate_node_operator_reward_address_set_event(
+        evs[4],
+        NodeOperatorRewardAddressSetItem(
+            nodeOperatorId=SimplyStakingId,
+            reward_address=SimplyStakingNewRewardAddress
+        )
+    )
 
 def limit_test(easy_track, to_spend, trusted_caller_acc, top_up_evm_script_factory, multisig_acc, stranger):
 
@@ -162,7 +226,7 @@ def limit_test(easy_track, to_spend, trusted_caller_acc, top_up_evm_script_facto
             [multisig_acc],
             [to_spend_usdc + 1],
             stranger,
-        )    
+        )
     
     # spend all USDC step by step (at most 2M each time)
     while to_spend_usdc > 0:
@@ -175,7 +239,13 @@ def limit_test(easy_track, to_spend, trusted_caller_acc, top_up_evm_script_facto
             [min(max_usdc_spend_at_once, to_spend_usdc)],
             stranger,
         )
+        print("HERE1")
+        print (max_usdc_spend_at_once)
+        print (to_spend_usdc)
         to_spend_usdc -= min(max_usdc_spend_at_once, to_spend_usdc)
+
+
+    print ('NEXT')
 
     # make sure there is nothing left so that you can't spend anymore
     with reverts("SUM_EXCEEDS_SPENDABLE_BALANCE"):
