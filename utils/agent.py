5e966aa2b2b3e97c540623c1e093e97f1bab64bc
Roman Kolpakov <rkolpakov94@gmail.com>
2025-01-21 16:16:08 +0300

    feat: add option to agent_forward through dual governance

--- utils/agent.py
+++ utils/agent.py
@@ -1,10 +1,11 @@
 from utils.config import contracts
-from utils.config import AGENT
+from utils.config import AGENT, DUAL_GOVERNANCE
 from utils.evm_script import (
     encode_call_script,
 )
 from typing import (
     Tuple,
+    Optional,
     Sequence,
 )
 
@@ -18,3 +19,13 @@ def agent_forward(call_script: Sequence[Tuple[str, str]]) -> Tuple[str, str]:
 def agent_execute(target: str, value: str, data: str) -> Tuple[str, str]:
     agent = contracts.agent
     return (AGENT, agent.execute.encode_input(target, value, data))
+
+
+def dual_governance_agent_forward(
+    call_script: Sequence[Tuple[str, str]],
+    description: Optional[str] = "",
+) -> Tuple[str, str]:
+    dual_governance = contracts.dual_governance
+    (agent_address, agent_calldata) = agent_forward(call_script)
+
+    return (DUAL_GOVERNANCE, dual_governance.submitProposal.encode_input([(agent_address, 0, agent_calldata)], description))
