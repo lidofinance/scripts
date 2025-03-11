b93948421eea80a22db0a282ba61f6d63049b246
Roman Kolpakov <rkolpakov94@gmail.com>
2025-03-11 13:11:52 +0300

    feat: add voting script for holesky

--- tests/dual_governance_upgrade_test.py
+++ tests/dual_governance_upgrade_test.py
@@ -9,20 +9,19 @@ TIMELOCK = ""
 DUAL_GOVERNANCE_ADMIN_EXECUTOR = ""
 RESEAL_MANAGER = ""
 
-ACL = "0x9895F0F17cc1d1891b6f18ee0b483B6f221b37Bb"
-LIDO = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"
-KERNEL = "0xb8FFC3Cd6e7Cf5a098A1c92F48009765B24088Dc"
-VOTING = "0x2e59A20f205bB85a89C53f1936454680651E618e"
-TOKEN_MANAGER = "0xf73a1260d222f447210581DDf212D915c09a3249"
-FINANCE = "0xB9E5CBB9CA5b0d659238807E84D0176930753d86"
-AGENT = "0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c"
-EVM_SCRIPT_REGISTRY = "0x853cc0D5917f49B57B8e9F89e491F5E18919093A"
-CURATED_MODULE = "0x55032650b14df07b85bF18A3a3eC8E0Af2e028d5"
-SDVT_MODULE = "0xaE7B191A31f627b4eB1d4DaC64eaB9976995b433"
-ALLOWED_TOKENS_REGISTRY = "0x4AC40c34f8992bb1e5E856A448792158022551ca"
-WITHDRAWAL_VAULT = "0xB9D7934878B5FB9610B3fE8A5e441e8fad7E293f"
-WITHDRAWAL_QUEUE = "0x889edC2eDab5f40e902b864aD4d7AdE8E412F9B1"
-INSURANCE_FUND = "0x8B3f33234ABD88493c0Cd28De33D583B70beDe35"
+ACL = "0xfd1E42595CeC3E83239bf8dFc535250e7F48E0bC"
+LIDO = "0x3F1c547b21f65e10480dE3ad8E19fAAC46C95034"
+KERNEL = "0x3b03f75Ec541Ca11a223bB58621A3146246E1644"
+VOTING = "0xdA7d2573Df555002503F29aA4003e398d28cc00f"
+TOKEN_MANAGER = "0xFaa1692c6eea8eeF534e7819749aD93a1420379A"
+FINANCE = "0xf0F281E5d7FBc54EAFcE0dA225CDbde04173AB16"
+AGENT = "0xE92329EC7ddB11D25e25b3c21eeBf11f15eB325d"
+EVM_SCRIPT_REGISTRY = "0xE1200ae048163B67D69Bc0492bF5FddC3a2899C0"
+CURATED_MODULE = "0x595F64Ddc3856a3b5Ff4f4CC1d1fb4B46cFd2bAC"
+SDVT_MODULE = "0x11a93807078f8BB880c1BD0ee4C387537de4b4b6"
+ALLOWED_TOKENS_REGISTRY = "0x091C0eC8B4D54a9fcB36269B5D5E5AF43309e666"
+WITHDRAWAL_VAULT = "0xF0179dEC45a37423EAD4FaD5fCb136197872EAd9"
+WITHDRAWAL_QUEUE = "0xc7cc160b58F8Bb0baC94b80847E2CF2800565C50"
 
 def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_decoding, stranger):
     acl = interface.ACL(ACL)
@@ -65,6 +64,14 @@ def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_de
     assert not acl.hasPermission(VOTING, TOKEN_MANAGER, REVOKE_VESTINGS_ROLE)
     assert acl.getPermissionManager(TOKEN_MANAGER, REVOKE_VESTINGS_ROLE) == ZERO_ADDRESS
 
+    BURN_ROLE = web3.keccak(text="BURN_ROLE")
+    assert not acl.hasPermission(VOTING, TOKEN_MANAGER, BURN_ROLE)
+    assert acl.getPermissionManager(TOKEN_MANAGER, BURN_ROLE) == ZERO_ADDRESS
+
+    ISSUE_ROLE = web3.keccak(text="ISSUE_ROLE")
+    assert not acl.hasPermission(VOTING, TOKEN_MANAGER, ISSUE_ROLE)
+    assert acl.getPermissionManager(TOKEN_MANAGER, ISSUE_ROLE) == ZERO_ADDRESS
+
     # FINANCE
     CHANGE_PERIOD_ROLE = web3.keccak(text="CHANGE_PERIOD_ROLE")
     assert not acl.hasPermission(VOTING, FINANCE, CHANGE_PERIOD_ROLE)
@@ -91,28 +98,21 @@ def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_de
     assert acl.getPermissionManager(CURATED_MODULE, STAKING_ROUTER_ROLE) == VOTING
 
     MANAGE_NODE_OPERATOR_ROLE = web3.keccak(text="MANAGE_NODE_OPERATOR_ROLE")
-    assert acl.hasPermission(AGENT, CURATED_MODULE, MANAGE_NODE_OPERATOR_ROLE)
+    assert acl.hasPermission(VOTING, CURATED_MODULE, MANAGE_NODE_OPERATOR_ROLE)
     assert acl.getPermissionManager(CURATED_MODULE, MANAGE_NODE_OPERATOR_ROLE) == VOTING
 
     SET_NODE_OPERATOR_LIMIT_ROLE = web3.keccak(text="SET_NODE_OPERATOR_LIMIT_ROLE")
-    assert not acl.hasPermission(AGENT, CURATED_MODULE, SET_NODE_OPERATOR_LIMIT_ROLE)
     assert acl.hasPermission(VOTING, CURATED_MODULE, SET_NODE_OPERATOR_LIMIT_ROLE)
     assert acl.getPermissionManager(CURATED_MODULE, SET_NODE_OPERATOR_LIMIT_ROLE) == VOTING
 
     MANAGE_SIGNING_KEYS = web3.keccak(text="MANAGE_SIGNING_KEYS")
-    assert not acl.hasPermission(AGENT, CURATED_MODULE, MANAGE_SIGNING_KEYS)
     assert acl.hasPermission(VOTING, CURATED_MODULE, MANAGE_SIGNING_KEYS)
     assert acl.getPermissionManager(CURATED_MODULE, MANAGE_SIGNING_KEYS) == VOTING
 
     # SDVT MODULE
-    STAKING_ROUTER_ROLE = web3.keccak(text="STAKING_ROUTER_ROLE")
-    assert acl.getPermissionManager(CURATED_MODULE, MANAGE_SIGNING_KEYS) == VOTING
-
-    MANAGE_NODE_OPERATOR_ROLE = web3.keccak(text="MANAGE_NODE_OPERATOR_ROLE")
-    assert acl.getPermissionManager(CURATED_MODULE, MANAGE_NODE_OPERATOR_ROLE) == VOTING
-
-    SET_NODE_OPERATOR_LIMIT_ROLE = web3.keccak(text="SET_NODE_OPERATOR_LIMIT_ROLE")
-    assert acl.getPermissionManager(CURATED_MODULE, SET_NODE_OPERATOR_LIMIT_ROLE) == VOTING
+    assert acl.getPermissionManager(SDVT_MODULE, STAKING_ROUTER_ROLE) == VOTING
+    assert acl.getPermissionManager(SDVT_MODULE, MANAGE_NODE_OPERATOR_ROLE) == VOTING
+    assert acl.getPermissionManager(SDVT_MODULE, SET_NODE_OPERATOR_LIMIT_ROLE) == VOTING
 
     # AGENT
     RUN_SCRIPT_ROLE = web3.keccak(text="RUN_SCRIPT_ROLE")
@@ -128,7 +128,7 @@ def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_de
     assert acl.hasPermission(VOTING, ACL, CREATE_PERMISSIONS_ROLE)
 
     # WITHDRAWAL QUEUE
-    RESUME_ROLE = web3.keccak(text="RESUME_ROLE")
+    PAUSE_ROLE = web3.keccak(text="PAUSE_ROLE")
     withdrawal_queue = interface.WithdrawalQueue(WITHDRAWAL_QUEUE)
     assert not withdrawal_queue.hasRole(PAUSE_ROLE, RESEAL_MANAGER)
     assert not withdrawal_queue.hasRole(RESUME_ROLE, RESEAL_MANAGER)
@@ -152,10 +152,6 @@ def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_de
     withdrawal_vault = interface.WithdrawalContractProxy(WITHDRAWAL_VAULT)
     assert withdrawal_vault.proxy_getAdmin() == VOTING
 
-    # INSURANCE FUND
-    insurance_fund = interface.InsuranceFund(INSURANCE_FUND)
-    assert insurance_fund.owner() == AGENT
-
     # START VOTE
     vote_id = vote_ids_from_env[0] if vote_ids_from_env else start_vote({"from": ldo_holder}, silent=True)[0]
 
@@ -176,9 +172,9 @@ def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_de
     assert not acl.hasPermission(VOTING, LIDO, PAUSE_ROLE)
     assert acl.getPermissionManager(LIDO, PAUSE_ROLE) == AGENT
 
-    assert not acl.hasPermission(AGENT, LIDO, PAUSE_ROLE)
-    assert not acl.hasPermission(VOTING, LIDO, PAUSE_ROLE)
-    assert acl.getPermissionManager(LIDO, PAUSE_ROLE) == AGENT
+    assert not acl.hasPermission(AGENT, LIDO, STAKING_PAUSE_ROLE)
+    assert not acl.hasPermission(VOTING, LIDO, STAKING_PAUSE_ROLE)
+    assert acl.getPermissionManager(LIDO, STAKING_PAUSE_ROLE) == AGENT
 
     # KERNEL
     assert not acl.hasPermission(AGENT, KERNEL, APP_MANAGER_ROLE)
@@ -191,6 +187,12 @@ def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_de
 
     assert acl.hasPermission(VOTING, TOKEN_MANAGER, REVOKE_VESTINGS_ROLE)
     assert acl.getPermissionManager(TOKEN_MANAGER, REVOKE_VESTINGS_ROLE) == VOTING
+
+    assert acl.hasPermission(VOTING, TOKEN_MANAGER, BURN_ROLE)
+    assert acl.getPermissionManager(TOKEN_MANAGER, BURN_ROLE) == VOTING
+
+    assert acl.hasPermission(VOTING, TOKEN_MANAGER, ISSUE_ROLE)
+    assert acl.getPermissionManager(TOKEN_MANAGER, ISSUE_ROLE) == VOTING
 
     # FINANCE
     assert acl.hasPermission(VOTING, FINANCE, CHANGE_PERIOD_ROLE)
@@ -211,7 +213,7 @@ def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_de
     # CURATED MODULE
     assert acl.getPermissionManager(CURATED_MODULE, STAKING_ROUTER_ROLE) == AGENT
 
-    assert acl.hasPermission(AGENT, CURATED_MODULE, MANAGE_NODE_OPERATOR_ROLE)
+    assert not acl.hasPermission(VOTING, CURATED_MODULE, MANAGE_NODE_OPERATOR_ROLE)
     assert acl.getPermissionManager(CURATED_MODULE, MANAGE_NODE_OPERATOR_ROLE) == AGENT
 
     assert not acl.hasPermission(VOTING, CURATED_MODULE, SET_NODE_OPERATOR_LIMIT_ROLE)
@@ -221,20 +223,18 @@ def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_de
     assert acl.getPermissionManager(CURATED_MODULE, MANAGE_SIGNING_KEYS) == AGENT
 
     # SDVT MODULE
-    assert acl.getPermissionManager(CURATED_MODULE, MANAGE_SIGNING_KEYS) == AGENT
-    assert acl.getPermissionManager(CURATED_MODULE, MANAGE_NODE_OPERATOR_ROLE) == AGENT
-    assert acl.getPermissionManager(CURATED_MODULE, SET_NODE_OPERATOR_LIMIT_ROLE) == AGENT
+    assert acl.getPermissionManager(SDVT_MODULE, STAKING_ROUTER_ROLE) == AGENT
+    assert acl.getPermissionManager(SDVT_MODULE, MANAGE_NODE_OPERATOR_ROLE) == AGENT
+    assert acl.getPermissionManager(SDVT_MODULE, SET_NODE_OPERATOR_LIMIT_ROLE) == AGENT
 
     # AGENT
     assert acl.getPermissionManager(AGENT, RUN_SCRIPT_ROLE) == AGENT
-    assert acl.hasPermission(DUAL_GOVERNANCE_ADMIN_EXECUTOR, AGENT, RUN_SCRIPT_ROLE)
     assert acl.getPermissionManager(AGENT, EXECUTE_ROLE) == AGENT
-    assert acl.hasPermission(DUAL_GOVERNANCE_ADMIN_EXECUTOR, AGENT, EXECUTE_ROLE)
 
     # ACL
     assert not acl.hasPermission(VOTING, ACL, CREATE_PERMISSIONS_ROLE)
-    assert acl.getPermissionManager(ACL, CREATE_PERMISSIONS_ROLE) == AGENT
     assert acl.hasPermission(AGENT, ACL, CREATE_PERMISSIONS_ROLE)
+    assert acl.getPermissionManager(ACL, CREATE_PERMISSIONS_ROLE) == AGENT
 
     # WITHDRAWAL QUEUE
     assert withdrawal_queue.hasRole(PAUSE_ROLE, RESEAL_MANAGER)
@@ -252,9 +252,6 @@ def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, bypass_events_de
 
     # WITHDRAWAL VAULT
     assert withdrawal_vault.proxy_getAdmin() == AGENT
-
-    # INSURANCE FUND
-    assert insurance_fund.owner() == VOTING
 
     chain.sleep(7 * 24 * 60)
 
