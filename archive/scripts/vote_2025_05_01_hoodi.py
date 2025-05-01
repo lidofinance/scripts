a37f1d8b629e89ede6ae38feae23379dd6b587a8
Vitaly Galaichuk <vitaly.galaichuk@gmail.com>
2025-05-01 19:23:48 +0700

    chore: add vote to enable sandbox et factories

---
+++ archive/scripts/vote_2025_05_01_hoodi.py
@@ -0,0 +1,111 @@
+"""
+Voting may slot '25
+
+I. Deploy sandbox EVM script factories for EasyTrack:
+
+1. Add `RemoveAllowedRecipients` EVM script factory 
+2. Add `AddAllowedRecipient` EVM script factory
+3. Add `TopUpAllowedRecipient` EVM script factory
+4. Add `CREATE_PAYMENTS_ROLE` permission to EasyTrackEVMScriptExecutor
+"""
+
+import time
+from typing import Dict
+from brownie import interface
+from brownie.network.transaction import TransactionReceipt
+from utils.permissions import encode_permission_grant
+
+from utils.agent import agent_forward
+from utils.voting import bake_vote_items, confirm_vote_script, create_vote
+from utils.ipfs import calculate_vote_ipfs_description, upload_vote_ipfs_description
+from utils.config import (
+    contracts,
+    EASYTRACK_EVMSCRIPT_EXECUTOR,
+    get_deployer_account,
+    get_is_live,
+    get_priority_fee,
+)
+from utils.easy_track import (
+    add_evmscript_factory,
+    create_permissions,
+)
+
+DESCRIPTION = """
+Voting may slot '25
+
+I. Deploy sandbox EVM script factories for EasyTrack:
+
+1. Add `RemoveAllowedRecipients` EVM script factory 
+2. Add `AddAllowedRecipient` EVM script factory
+3. Add `TopUpAllowedRecipient` EVM script factory
+4. Add `CREATE_PAYMENTS_ROLE` permission to EasyTrackEVMScriptExecutor
+
+II. ...
+"""
+
+
+def start_vote(tx_params: Dict[str, str], silent: bool) -> bool | list[int | TransactionReceipt | None]:
+    """Prepare and run voting"""
+
+    removeAllowed = "0xc84251D2959E976AfE95201E1e2B88dB56Bc0a69"
+    addAllowed = "0x056561d0F1314CB3932180b3f0B3C03174F2642B"
+    topUp = "0x8D9Fd9cD208f57c6735174B848180B53A0F7F560"
+
+    registry = interface.AllowedRecipientRegistry("0xd57FF1ce54F572F4E8DaF0cB7038F1Bd6049cAa8")
+
+    vote_desc_items, call_script_items = zip(
+        (
+            "1) Add `RemoveAllowedRecipients` EVM script factory with address {removeAllowed}",
+            add_evmscript_factory(
+                factory=removeAllowed,
+                permissions=(create_permissions(registry, "add_relay"),),
+            ),
+        ),
+        (
+            "2) Add `AddAllowedRecipient` EVM script factory with address {addAllowed}",
+            add_evmscript_factory(
+                factory=addAllowed,
+                permissions=(create_permissions(registry, "remove_relay"),),
+            ),
+        ),
+        (
+            "3) Add `TopUpAllowedRecipient` EVM script factory with address {topUp}",
+            add_evmscript_factory(
+                factory=topUp,
+                permissions=create_permissions(contracts.finance, "newImmediatePayment")
+                + create_permissions(registry, "updateSpentAmount")[2:],
+            ),
+        ),
+        (
+            "4) Add CREATE_PAYMENTS_ROLE permission to EasyTrackEVMScriptExecutor",
+            encode_permission_grant(
+                acl=contracts.acl,
+                target_app=contracts.finance,
+                permission_name="CREATE_PAYMENTS_ROLE",
+                grant_to=EASYTRACK_EVMSCRIPT_EXECUTOR,
+            ),
+        ),
+    )
+
+    vote_items = bake_vote_items(list(vote_desc_items), list(call_script_items))
+
+    if silent:
+        desc_ipfs = calculate_vote_ipfs_description(DESCRIPTION)
+    else:
+        desc_ipfs = upload_vote_ipfs_description(DESCRIPTION)
+
+    return confirm_vote_script(vote_items, silent, desc_ipfs) and list(
+        create_vote(vote_items, tx_params, desc_ipfs=desc_ipfs)
+    )
+
+
+def main():
+    tx_params = {"from": get_deployer_account()}
+    if get_is_live():
+        tx_params["priority_fee"] = get_priority_fee()
+
+    vote_id, _ = start_vote(tx_params=tx_params, silent=False)
+
+    vote_id >= 0 and print(f"Vote created: {vote_id}.")
+
+    time.sleep(5)  # hack for waiting thread #2.
