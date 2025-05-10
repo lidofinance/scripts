44a595aff442db501a420dc9dae5fde73bf88f34
Nikita Panin <iamnp@yandex.ru>
2025-05-10 21:09:48 +0300

    Update test_2025_05_11.py

--- tests/test_2025_05_11.py
+++ tests/test_2025_05_11.py
@@ -5,6 +5,8 @@ from utils.config import LDO_HOLDER_ADDRESS_FOR_TESTS
 from brownie import interface
 from utils.test.tx_tracing_helpers import *
 from typing import NamedTuple
+from utils.voting import find_metadata_by_vote_id
+from utils.ipfs import get_lido_vote_cid_from_str
 
 from utils.test.event_validators.hash_consensus import (
     validate_hash_consensus_member_removed,
@@ -18,12 +20,7 @@ VOTING = "0x2e59A20f205bB85a89C53f1936454680651E618e"
 
 HASH_CONSENSUS_FOR_AO = "0xD624B08C83bAECF0807Dd2c6880C3154a5F0B288"
 HASH_CONSENSUS_FOR_VEBO = "0x7FaDB6358950c5fAA66Cb5EB8eE5147De3df355a"
-
-
-# CSM
-CSM_ADDRESS = "0xdA7dE2ECdDfccC6c3AF10108Db212ACBBf9EA83F"
 CS_ORACLE_HASH_CONSENSUS_ADDRESS = "0x71093efF8D8599b5fA340D665Ad60fA7C80688e4"
-
 
 # Oracles members
 old_oracle_member_to_remove = "0x140bd8fbdc884f48da7cb1c09be8a2fadfea776e"
@@ -42,15 +39,20 @@ def test_vote(
     assert ao_hash_consensus.getIsMember(old_oracle_member_to_remove)
     assert not ao_hash_consensus.getIsMember(new_oracle_member_to_add)
     assert ao_hash_consensus.getQuorum() == 5
+    assert len(ao_hash_consensus.getMembers()[0]) == 9
+
     # 2), 5) before vote old member is still in the quorum of vebo hash consensus, new member is not in the quorum
     vebo_hash_consensus = get_vebo_hash_consensus()
     assert vebo_hash_consensus.getIsMember(old_oracle_member_to_remove)
     assert not vebo_hash_consensus.getIsMember(new_oracle_member_to_add)
     assert vebo_hash_consensus.getQuorum() == 5
+    assert len(vebo_hash_consensus.getMembers()[0]) == 9
+
     # 4), 6) before vote old member is still in the quorum of cs hash consensus, new member is not in the quorum
     assert csm_hash_consensus.getIsMember(old_oracle_member_to_remove)
     assert not csm_hash_consensus.getIsMember(new_oracle_member_to_add)
     assert csm_hash_consensus.getQuorum() == 5
+    assert len(csm_hash_consensus.getMembers()[0]) == 9
 
     # START VOTE
     if len(vote_ids_from_env) > 0:
@@ -65,20 +67,28 @@ def test_vote(
 
     print(f"voteId = {vote_id}, gasUsed = {vote_tx.gas_used}")
 
+    metadata = find_metadata_by_vote_id(vote_id)
+    print('ipfs id:', get_lido_vote_cid_from_str(metadata))
+    assert get_lido_vote_cid_from_str(metadata) == "bafkreihy2o5c653ykrqcvsruootiwi7i4el26apnvriocfgxlosejdnt6i"
+
     # 1), 4) after vote old member is not in the quorum of ao hash consensus, new member is in the quorum
     assert not ao_hash_consensus.getIsMember(old_oracle_member_to_remove)
     assert ao_hash_consensus.getIsMember(new_oracle_member_to_add)
     assert ao_hash_consensus.getQuorum() == 5
+    assert len(ao_hash_consensus.getMembers()[0]) == 9
+
     # 2), 5) after vote old member is not in the quorum of vebo hash consensus, new member is in the quorum
     vebo_hash_consensus = get_vebo_hash_consensus()
     assert not vebo_hash_consensus.getIsMember(old_oracle_member_to_remove)
     assert vebo_hash_consensus.getIsMember(new_oracle_member_to_add)
     assert vebo_hash_consensus.getQuorum() == 5
+    assert len(vebo_hash_consensus.getMembers()[0]) == 9
 
     # 3), 6) after vote old member is not in the quorum of cs hash consensus, new member is in the quorum
     assert not csm_hash_consensus.getIsMember(old_oracle_member_to_remove)
     assert csm_hash_consensus.getIsMember(new_oracle_member_to_add)
     assert csm_hash_consensus.getQuorum() == 5
+    assert len(csm_hash_consensus.getMembers()[0]) == 9
 
     # Events check
     display_voting_events(vote_tx)
@@ -86,26 +96,23 @@ def test_vote(
 
     assert len(events) == 6
 
-    validate_hash_consensus_member_removed(events[0], old_oracle_member_to_remove, 5, new_total_members=8)
-    validate_hash_consensus_member_removed(events[1], old_oracle_member_to_remove, 5, new_total_members=8)
+    validate_hash_consensus_member_removed(events[0], old_oracle_member_to_remove, 5, new_total_members=8, emitted_by=HASH_CONSENSUS_FOR_AO)
+    validate_hash_consensus_member_removed(events[1], old_oracle_member_to_remove, 5, new_total_members=8, emitted_by=HASH_CONSENSUS_FOR_VEBO)
     validate_hash_consensus_member_removed(
         events[2],
         old_oracle_member_to_remove,
         5,
         new_total_members=8,
+        emitted_by=CS_ORACLE_HASH_CONSENSUS_ADDRESS
     )
-    validate_hash_consensus_member_added(events[3], new_oracle_member_to_add, 5, new_total_members=9)
-    validate_hash_consensus_member_added(
-        events[4],
-        new_oracle_member_to_add,
-        5,
-        new_total_members=9,
-    )
+    validate_hash_consensus_member_added(events[3], new_oracle_member_to_add, 5, new_total_members=9, emitted_by=HASH_CONSENSUS_FOR_AO)
+    validate_hash_consensus_member_added(events[4], new_oracle_member_to_add,5, new_total_members=9, emitted_by=HASH_CONSENSUS_FOR_VEBO)
     validate_hash_consensus_member_added(
         events[5],
         new_oracle_member_to_add,
         5,
         new_total_members=9,
+        emitted_by=CS_ORACLE_HASH_CONSENSUS_ADDRESS
     )
 
 def get_voting():
