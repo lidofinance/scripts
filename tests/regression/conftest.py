aa9c2a83b3df27c522aad10ee9f14d540869d090
Roman Kolpakov <rkolpakov94@gmail.com>
2025-06-02 15:42:10 +0300

    Add helpers to process dg proposals

--- tests/regression/conftest.py
+++ tests/regression/conftest.py
@@ -1,33 +1,15 @@
 import pytest
-import os
-from brownie import web3
-
-from utils.config import contracts
-from utils.import_current_votes import is_there_any_vote_scripts, is_there_any_upgrade_scripts, start_and_execute_votes
+from utils.import_current_votes import is_there_any_vote_scripts, is_there_any_upgrade_scripts
 from utils.test.extra_data import ExtraDataService
 
-from utils.test.helpers import ETH
-from utils.test.oracle_report_helpers import oracle_report
-from utils.test.simple_dvt_helpers import fill_simple_dvt_ops_vetted_keys
+from utils.test.governance_helpers import execute_vote_and_process_dg_proposals
 
 ENV_REPORT_AFTER_VOTE = "REPORT_AFTER_VOTE"
 ENV_FILL_SIMPLE_DVT = "FILL_SIMPLE_DVT"
 
-
 @pytest.fixture(scope="module", autouse=is_there_any_vote_scripts() or is_there_any_upgrade_scripts())
-def autoexecute_vote(request, helpers, vote_ids_from_env, accounts, stranger, module_isolation):
-    if vote_ids_from_env:
-        helpers.execute_votes(accounts, vote_ids_from_env, contracts.voting)
-    else:
-        start_and_execute_votes(contracts.voting, helpers)
-
-    if os.getenv(ENV_FILL_SIMPLE_DVT):
-        print(f"Prefilling SimpleDVT...")
-        fill_simple_dvt_ops_vetted_keys(stranger)
-
-    if os.getenv(ENV_REPORT_AFTER_VOTE):
-        oracle_report(cl_diff=ETH(523), exclude_vaults_balances=False)
-
+def autoexecute_vote(request, module_isolation, helpers, vote_ids_from_env, accounts, stranger):
+    execute_vote_and_process_dg_proposals(helpers, vote_ids_from_env, accounts, stranger)
 
 @pytest.fixture()
 def extra_data_service() -> ExtraDataService:
