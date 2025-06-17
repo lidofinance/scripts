64c79c1505703013fdb8341e0a001f3040184f63
Eddort <webisforce@gmail.com>
2025-06-17 14:29:20 +0200

    feat: update holesky config and refactoring

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-export DEPLOYER=0x8943545177806ED17B9F23F0a21ee5948eCaa776
-poetry run brownie run scripts/tw_vote.py --network=hoodi-devnet-remote
--- fork.sh
+++
@@ -1,2 +0,0 @@
-# ssh -L 33486:localhost:33486 -L 33480:localhost:33480 -L 33479:localhost:33479 -L 33497:localhost:33497 -L 33491:localhost:33491 -L 33494:localhost:33494 devnet
-anvil --fork-url http://localhost:33529 --auto-impersonate
