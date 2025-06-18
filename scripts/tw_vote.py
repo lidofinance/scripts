f5c2e33a41556b8c5eb12bd4f2a1d93a45b47fdd
vgorkavenko <vladimir.g@lido.fi>
2025-06-18 10:39:26 +0200

    feat: add CSM items

--- scripts/tw_vote.py
+++ scripts/tw_vote.py
@@ -1,7 +1,7 @@
 from itertools import count
 from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple
 from typing import Tuple, Optional, Sequence
-from brownie import accounts, web3, convert,ZERO_ADDRESS
+from brownie import accounts, web3, convert, ZERO_ADDRESS
 from utils.config import (
     CS_ACCOUNTING_IMPL_V2_ADDRESS,
     CS_CURVES,
@@ -11,6 +11,7 @@ from utils.config import (
     CS_GATE_SEAL_V2_ADDRESS,
     CSM_COMMITTEE_MS,
     CSM_IMPL_V2_ADDRESS,
+    CSM_SET_VETTED_GATE_TREE_FACTORY,
     NODE_OPERATORS_REGISTRY_ARAGON_APP_ID,
     NODE_OPERATORS_REGISTRY_IMPL,
     ACCOUNTING_ORACLE_IMPL,
@@ -29,10 +30,13 @@ from utils.config import (
 from utils.evm_script import encode_call_script
 from utils.ipfs import upload_vote_ipfs_description, calculate_vote_ipfs_description
 from utils.permissions import encode_oz_grant_role, encode_oz_revoke_role
+from utils.easy_track import (
+    add_evmscript_factory,
+    create_permissions,
+)
 from utils.agent import dual_governance_agent_forward
 from utils.voting import bake_vote_items, confirm_vote_script, create_vote
 from utils.config import get_deployer_account, get_priority_fee
-from utils.agent import dual_governance_submit_proposal, dual_governance_agent_forward
 from utils.kernel import update_app_implementation
 from utils.agent import agent_forward
 from tests.conftest import Helpers
@@ -64,6 +68,15 @@ TW_DAILY_LIMIT = 10
 EXIT_EVENTS_LOOKBACK_WINDOW_IN_SLOTS = 7200
 
 NOR_EXIT_DEADLINE_IN_SEC = 30 * 60
+
+# CSM Module share and EasyTrack constants
+CSM_MODULE_ID = 3
+CSM_NEW_TARGET_SHARE_BP = 300  # 3%
+CSM_NEW_PRIORITY_EXIT_THRESHOLD_BP = 375  # 3.75%
+CSM_OLD_STAKING_MODULE_FEE_BP = 600
+CSM_OLD_TREASURY_FEE_BP = 400
+CSM_OLD_MAX_DEPOSITS_PER_BLOCK = 30
+CSM_OLD_MIN_DEPOSIT_BLOCK_DISTANCE = 25
 
 NOR_VERSION = ["6", "0", "0"]
 SDVT_VERSION = ["6", "0", "0"]
@@ -114,6 +127,21 @@ def encode_oracle_upgrade_consensus(proxy: Any, consensus_version: int) -> Tuple
     oracle = interface.BaseOracle(proxy)
     return oracle.address, oracle.setConsensusVersion.encode_input(consensus_version)
 
+def encode_staking_router_update_csm_module_share() -> Tuple[str, str]:
+    """Encode call to update CSM share limit"""
+    return (
+        contracts.staking_router.address,
+        contracts.staking_router.updateStakingModule.encode_input(
+            CSM_MODULE_ID,
+            CSM_NEW_TARGET_SHARE_BP,
+            CSM_NEW_PRIORITY_EXIT_THRESHOLD_BP,
+            CSM_OLD_STAKING_MODULE_FEE_BP,
+            CSM_OLD_TREASURY_FEE_BP,
+            CSM_OLD_MAX_DEPOSITS_PER_BLOCK,
+            CSM_OLD_MIN_DEPOSIT_BLOCK_DISTANCE,
+        )
+    )
+
 def create_tw_vote(tx_params: Dict[str, str], silent: bool) -> Tuple[int, Optional[Any]]:
     """
         Triggerable withdrawals voting baking and sending.
@@ -156,28 +184,30 @@ def create_tw_vote(tx_params: Dict[str, str], silent: bool) -> Tuple[int, Option
             26. Remove VALIDATOR_DELINQUENT_TIMEOUT_IN_SLOTS variable from OracleDaemonConfig
             27. Add EXIT_EVENTS_LOOKBACK_WINDOW_IN_SLOTS variable to OracleDaemonConfig
             --- CSM ---
-            32. Upgrade CSM implementation on proxy
-            33. Call `finalizeUpgradeV2()` on CSM contract
-            34. Upgrade CSAccounting implementation on proxy
-            35. Call `finalizeUpgradeV2(bondCurves)` on CSAccounting contract
-            36. Upgrade CSFeeOracle implementation on proxy
-            37. Call `finalizeUpgradeV2(consensusVersion)` on CSFeeOracle contract
-            38. Upgrade CSFeeDistributor implementation on proxy
-            39. Call `finalizeUpgradeV2(admin)` on CSFeeDistributor contract
-            40. Revoke CSAccounting role SET_BOND_CURVE_ROLE from the CSM contract
-            41. Revoke CSAccounting role RESET_BOND_CURVE_ROLE from the CSM contract
-            42. Revoke CSAccounting role RESET_BOND_CURVE_ROLE from the CSM committee
-            43. Grant CSM role CREATE_NODE_OPERATOR_ROLE for the permissionless gate
-            44. Grant CSM role CREATE_NODE_OPERATOR_ROLE for the vetted gate
-            45. Grant CSAccounting role SET_BOND_CURVE_ROLE for the vetted gate
-            46. Revoke role VERIFIER_ROLE from the previous instance of the Verifier contract
-            47. Grant role VERIFIER_ROLE to the new instance of the Verifier contract
-            48. Revoke CSM role PAUSE_ROLE from the previous GateSeal instance
-            49. Revoke CSAccounting role PAUSE_ROLE from the previous GateSeal instance
-            50. Revoke CSFeeOracle role PAUSE_ROLE from the previous GateSeal instance
-            51. Grant CSM role PAUSE_ROLE for the new GateSeal instance
-            52. Grant CSAccounting role PAUSE_ROLE for the new GateSeal instance
-            53. Grant CSFeeOracle role PAUSE_ROLE for the new GateSeal instance
+            28. Upgrade CSM implementation on proxy
+            29. Call `finalizeUpgradeV2()` on CSM contract
+            30. Upgrade CSAccounting implementation on proxy
+            31. Call `finalizeUpgradeV2(bondCurves)` on CSAccounting contract
+            32. Upgrade CSFeeOracle implementation on proxy
+            33. Call `finalizeUpgradeV2(consensusVersion)` on CSFeeOracle contract
+            34. Upgrade CSFeeDistributor implementation on proxy
+            35. Call `finalizeUpgradeV2(admin)` on CSFeeDistributor contract
+            36. Revoke CSAccounting role SET_BOND_CURVE_ROLE from the CSM contract
+            37. Revoke CSAccounting role RESET_BOND_CURVE_ROLE from the CSM contract
+            38. Revoke CSAccounting role RESET_BOND_CURVE_ROLE from the CSM committee
+            39. Grant CSM role CREATE_NODE_OPERATOR_ROLE for the permissionless gate
+            40. Grant CSM role CREATE_NODE_OPERATOR_ROLE for the vetted gate
+            41. Grant CSAccounting role SET_BOND_CURVE_ROLE for the vetted gate
+            42. Revoke role VERIFIER_ROLE from the previous instance of the Verifier contract
+            43. Grant role VERIFIER_ROLE to the new instance of the Verifier contract
+            44. Revoke CSM role PAUSE_ROLE from the previous GateSeal instance
+            45. Revoke CSAccounting role PAUSE_ROLE from the previous GateSeal instance
+            46. Revoke CSFeeOracle role PAUSE_ROLE from the previous GateSeal instance
+            47. Grant CSM role PAUSE_ROLE for the new GateSeal instance
+            48. Grant CSAccounting role PAUSE_ROLE for the new GateSeal instance
+            49. Grant CSFeeOracle role PAUSE_ROLE for the new GateSeal instance
+            50. Increase CSM share in Staking Router from 2% to 3%
+            51. Add CSMSetVettedGateTree factory to EasyTrack with permissions
     """
 
     item_idx = count(1)
@@ -578,6 +608,19 @@ def create_tw_vote(tx_params: Dict[str, str], silent: bool) -> Tuple[int, Option
                 role_name="PAUSE_ROLE",
                 grant_to=CS_GATE_SEAL_V2_ADDRESS,
             )
+        ),
+        (
+            f"50. Increase CSM share in Staking Router from 2% to 3%",
+            agent_forward([
+                encode_staking_router_update_csm_module_share()
+            ])
+        ),
+        (
+            f"51. Add CSMSetVettedGateTree factory to EasyTrack with permissions",
+            add_evmscript_factory(
+                factory=CSM_SET_VETTED_GATE_TREE_FACTORY,
+                permissions=(create_permissions(contracts.cs_vetted_gate, "setTreeParams")),
+            ),
         ),
     )
 
