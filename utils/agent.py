a6cdc9cd72d6795453edba3db45380ba4da6ac0c
Eddort <webisforce@gmail.com>
2025-07-18 10:34:49 +0200

    refactor: remove dual governance related functions from agent and dg_decorators modules

--- utils/agent.py
+++ utils/agent.py
@@ -14,29 +14,6 @@ def agent_forward(call_script: Sequence[Tuple[str, str]]) -> Tuple[str, str]:
     print(f"Forwarding call script to agent: {call_script}")
     return (AGENT, agent.forward.encode_input(encode_call_script(call_script)))
 
-def extract_as_dg_admin_call(call_script: Sequence[Tuple[str, str]]) -> Tuple[str, str]:
-    print(f"Extracting call script for dual governance admin: {call_script}")
-    return (call_script[0][0], call_script[0][1])
-
-def dual_governance_agent_forward(
-    call_script: Sequence[Tuple],
-    description: Optional[str] = "",
-) -> Tuple[str, str]:
-    dual_governance = contracts.dual_governance
-    forwarded = []
-
-    for _call in call_script:
-        if len(_call) > 2 and _call[2] == True:
-            forwarded.append(extract_as_dg_admin_call([_call]))
-        else:
-            forwarded.append(agent_forward([_call]))
-
-    print(f"Forwarding call script to dual governance: {forwarded}")
-    return (
-        contracts.dual_governance.address,
-        dual_governance.submitProposal.encode_input([(_call[0], 0, _call[1]) for _call in forwarded], description),
-    )
-
 
 def agent_execute(target: str, value: str, data: str) -> Tuple[str, str]:
     agent = contracts.agent
