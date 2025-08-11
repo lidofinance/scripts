b2a3a9b72b84a3b52c903191db44fb5495f3ec2d
Roman Kolpakov <rkolpakov94@gmail.com>
2025-08-11 17:44:07 +0300

    Add a template for omnibus script & test

---
+++ tests/_test_template.py
@@ -0,0 +1,126 @@
+"""
+TEMPLATE TEST — Aragon vote + optional DG proposal execution
+По одному примеру проверки на блок.
+"""
+
+from brownie import chain, interface
+from brownie.network.transaction import TransactionReceipt
+
+from utils.test.tx_tracing_helpers import (
+    group_voting_events_from_receipt,
+    group_dg_events_from_receipt,
+)
+from utils.evm_script import encode_call_script
+from utils.voting import find_metadata_by_vote_id
+from utils.ipfs import get_lido_vote_cid_from_str
+from utils.dual_governance import PROPOSAL_STATUS
+
+
+# ============================================================================
+# ============================== Import vote =================================
+# ============================================================================
+# TODO: import voting script
+# from scripts.vote_* import start_vote, get_vote_items
+
+
+# ============================================================================
+# ============================== Constants ===================================
+# ============================================================================
+# TODO: list all contract addresses used in tests - do not use imports from config!
+# NOTE: these addresses might have a different value on other chains
+
+VOTING = "0x2e59A20f205bB85a89C53f1936454680651E618e"
+EMERGENCY_PROTECTED_TIMELOCK = "0xCE0425301C85c5Ea2A0873A2dEe44d78E02D2316"
+DUAL_GOVERNANCE = "0xcdF49b058D606AD34c5789FD8c3BF8B3E54bA2db"
+
+# Set variable to None if item is not presented
+EXPECTED_VOTE_ID = 1
+EXPECTED_DG_PROPOSAL_ID = 1
+EXPECTED_VOTE_EVENTS_COUNT = 1
+EXPECTED_DG_EVENTS_COUNT = 1
+IPFS_DESCRIPTION_HASH = ""
+
+
+def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env, stranger):
+
+    # =======================================================================
+    # ========================= Arrange variables ===========================
+    # =======================================================================
+    voting = interface.Voting(VOTING)
+    timelock = interface.EmergencyProtectedTimelock(EMERGENCY_PROTECTED_TIMELOCK)
+    dual_governance = interface.DualGovernance(DUAL_GOVERNANCE)
+
+
+    # =========================================================================
+    # ======================== Identify or Create vote ========================
+    # =========================================================================
+    if vote_ids_from_env:
+        vote_id = vote_ids_from_env[0]
+        if EXPECTED_VOTE_ID is not None:
+            assert vote_id == EXPECTED_VOTE_ID
+    elif EXPECTED_VOTE_ID is not None and voting.votesLength() > EXPECTED_VOTE_ID:
+        vote_id = EXPECTED_VOTE_ID
+    else:
+        vote_id, _ = start_vote({"from": ldo_holder}, silent=True)
+
+    _, call_script_items = get_vote_items()
+    onchain_script = voting.getVote(vote_id)["script"]
+    assert onchain_script == encode_call_script(call_script_items)
+
+
+    # =========================================================================
+    # ============================= Execute Vote ==============================
+    # =========================================================================
+    is_executed = voting.getVote(vote_id)["executed"]
+    if not is_executed:
+        # =======================================================================
+        # ========================= Before voting checks ========================
+        # =======================================================================
+        # TODO: add before voting checks
+
+
+        assert get_lido_vote_cid_from_str(find_metadata_by_vote_id(vote_id)) == IPFS_DESCRIPTION_HASH
+
+        vote_tx: TransactionReceipt = helpers.execute_vote(vote_id=vote_id, accounts=accounts, dao_voting=voting)
+        vote_events = group_voting_events_from_receipt(vote_tx)
+
+
+        # =======================================================================
+        # ========================= After voting checks =========================
+        # =======================================================================
+        # TODO: add after voting tests
+
+
+        assert len(vote_events) == EXPECTED_VOTE_EVENTS_COUNT
+        if EXPECTED_DG_PROPOSAL_ID is not None:
+            assert EXPECTED_DG_PROPOSAL_ID == timelock.getProposalsCount()
+
+
+    if EXPECTED_DG_PROPOSAL_ID is not None:
+        details = timelock.getProposalDetails(EXPECTED_DG_PROPOSAL_ID)
+        if details["status"] != PROPOSAL_STATUS["executed"]:
+            # =========================================================================
+            # ================== DG before proposal executed checks ===================
+            # =========================================================================
+            # TODO: add DG before proposal executed checks
+
+
+            if details["status"] == PROPOSAL_STATUS["submitted"]:
+                chain.sleep(timelock.getAfterSubmitDelay() + 1)
+                dual_governance.scheduleProposal(EXPECTED_DG_PROPOSAL_ID, {"from": stranger})
+
+            if timelock.getProposalDetails(EXPECTED_DG_PROPOSAL_ID)["status"] == PROPOSAL_STATUS["scheduled"]:
+                chain.sleep(timelock.getAfterScheduleDelay() + 1)
+                dg_tx: TransactionReceipt = timelock.execute(EXPECTED_DG_PROPOSAL_ID, {"from": stranger})
+                dg_events = group_dg_events_from_receipt(
+                    dg_tx,
+                    timelock=EMERGENCY_PROTECTED_TIMELOCK,
+                    admin_executor=DUAL_GOVERNANCE_ADMIN_EXECUTOR,
+                )
+                assert len(dg_events) == EXPECTED_DG_EVENTS_COUNT
+
+
+        # =========================================================================
+        # ==================== After DG proposal executed checks ==================
+        # =========================================================================
+        # TODO: add DG after proposal executed checks
