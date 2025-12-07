69f00cf703df18411e76f3d8b1cc31643c777752
Nikita P <iamnp@yandex.ru>
2025-12-07 11:16:19 +0000

    fix reset

--- tests/test_2025_12_10.py
+++ tests/test_2025_12_10.py
@@ -1,10 +1,5 @@
-import pytest
-import brownie
 import tests.utils_test_2025_12_10_lidov3 as lidov3
 
-@pytest.fixture(autouse=True)
-def isolation():
-    brownie.chain.reset()
 
 def test_vote_v1_dg1(helpers, accounts, ldo_holder, vote_ids_from_env, stranger):
 
