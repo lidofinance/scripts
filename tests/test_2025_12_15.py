92817b98acebdff1fa8eb198b33209aede4afa8a
Nikita P <iamnp@yandex.ru>
2025-12-10 18:41:28 +0000

    fix vote name

--- tests/test_2025_12_15.py
+++ tests/test_2025_12_15.py
@@ -1,4 +1,4 @@
-import tests.utils_test_2025_12_10_lidov3 as lidov3
+import tests.utils_test_2025_12_15_lidov3 as lidov3
 
 
 def test_vote_v1_dg1(helpers, accounts, ldo_holder, vote_ids_from_env, stranger):
