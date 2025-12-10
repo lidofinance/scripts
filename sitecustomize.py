bcbbde4b36584fb427b2834db2a83ca3f7f36c51
Nikita P <iamnp@yandex.ru>
2025-12-10 13:42:37 +0000

    fix: download solc for CI

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,11 +1,9 @@
 # sitecustomize.py
-
-print("======================================================================applied")
 try:
     import solcx.install as solcx_install
 except ImportError:
     # py-solc-x not installed in this environment; nothing to patch
     pass
 else:
-    # change this to your mirror / URL
+    # replace outdated solc binaries URL with the current one
     solcx_install.BINARY_DOWNLOAD_BASE = "https://binaries.soliditylang.org/{}-amd64/{}"
