a57fb768014b74dd48c5cf667af22a0b920d1bac
Nikita P <iamnp@yandex.ru>
2025-12-19 19:13:48 +0000

    initial voting

---
+++ scripts/vote_2025_12_19.py
@@ -0,0 +1,94 @@
+"""
+Vote 2025_12_19
+
+1. Grant role BURN_ROLE on TokenManager 0xf73a1260d222f447210581DDf212D915c09a3249 for contract `Y` TODO
+2. Grant role MINT_ROLE on TokenManager 0xf73a1260d222f447210581DDf212D915c09a3249 for contract `Y` TODO
+3. Grant role ASSIGN_ROLE on TokenManager 0xf73a1260d222f447210581DDf212D915c09a3249 for contract `Y` TODO
+
+# TODO (after vote) Vote #{vote number} passed & executed on ${date+time}, block ${blockNumber}.
+"""
+
+from typing import Dict, List, Tuple
+
+from utils.voting import bake_vote_items, confirm_vote_script, create_vote
+from utils.ipfs import upload_vote_ipfs_description, calculate_vote_ipfs_description
+from utils.config import get_deployer_account, get_is_live, get_priority_fee
+from utils.mainnet_fork import pass_and_exec_dao_vote
+from utils.permissions import encode_permission_grant
+
+
+# ============================== Addresses ===================================
+TOKEN_MANAGER = "0xf73a1260d222f447210581DDf212D915c09a3249"
+VESTING_CONTRACT = "0xb9d7934878b5fb9610b3fe8a5e441e8fad7e293f" # TODO replace with actual address
+
+
+# ============================= Description ==================================
+IPFS_DESCRIPTION = "Enable a dedicated vesting contract to apply vesting schedules to LDO tokens held on contributor addresses, where the contract can only remint the same amount of tokens that were previously held on the address, but under vesting."
+
+
+# ================================ Main ======================================
+def get_vote_items() -> Tuple[List[str], List[Tuple[str, str]]]:
+    
+    vote_desc_items, call_script_items = zip(
+        (
+            "1. Grant role BURN_ROLE on TokenManager 0xf73a1260d222f447210581DDf212D915c09a3249 for contract `Y` TODO",
+            encode_permission_grant(
+                target_app=TOKEN_MANAGER,
+                permission_name="BURN_ROLE",
+                grant_to=VESTING_CONTRACT,
+            ),
+        ),
+        (
+            "2. Grant role MINT_ROLE on TokenManager 0xf73a1260d222f447210581DDf212D915c09a3249 for contract `Y` TODO",
+            encode_permission_grant(
+                target_app=TOKEN_MANAGER,
+                permission_name="MINT_ROLE",
+                grant_to=VESTING_CONTRACT,
+            ),
+        ),
+        (
+            "3. Grant role ASSIGN_ROLE on TokenManager 0xf73a1260d222f447210581DDf212D915c09a3249 for contract `Y` TODO",
+            encode_permission_grant(
+                target_app=TOKEN_MANAGER,
+                permission_name="ASSIGN_ROLE",
+                grant_to=VESTING_CONTRACT,
+            ),
+        ),
+    )
+
+    return vote_desc_items, call_script_items
+
+
+def start_vote(tx_params: Dict[str, str], silent: bool = False):
+    vote_desc_items, call_script_items = get_vote_items()
+    vote_items = bake_vote_items(list(vote_desc_items), list(call_script_items))
+
+    desc_ipfs = (
+        calculate_vote_ipfs_description(IPFS_DESCRIPTION)
+        if silent else upload_vote_ipfs_description(IPFS_DESCRIPTION)
+    )
+
+    vote_id, tx = confirm_vote_script(vote_items, silent, desc_ipfs) and list(
+        create_vote(vote_items, tx_params, desc_ipfs=desc_ipfs)
+    )
+
+    return vote_id, tx
+
+
+def main():
+    tx_params: Dict[str, str] = {"from": get_deployer_account().address}
+    if get_is_live():
+        tx_params["priority_fee"] = get_priority_fee()
+
+    vote_id, _ = start_vote(tx_params=tx_params, silent=False)
+    vote_id >= 0 and print(f"Vote created: {vote_id}.")
+
+
+def start_and_execute_vote_on_fork_manual():
+    if get_is_live():
+        raise Exception("This script is for local testing only.")
+
+    tx_params = {"from": get_deployer_account()}
+    vote_id, _ = start_vote(tx_params=tx_params, silent=True)
+    print(f"Vote created: {vote_id}.")
+    pass_and_exec_dao_vote(int(vote_id), step_by_step=True)
---
+++ tests/test_2025_12_19.py
@@ -0,0 +1,130 @@
+from brownie import web3, interface
+from brownie.network.transaction import TransactionReceipt
+
+from utils.test.tx_tracing_helpers import group_voting_events_from_receipt
+from utils.evm_script import encode_call_script
+from utils.voting import find_metadata_by_vote_id
+from utils.ipfs import get_lido_vote_cid_from_str
+from utils.test.tx_tracing_helpers import (
+    group_voting_events_from_receipt,
+    count_vote_items_by_events,
+    display_voting_events,
+)
+from utils.test.event_validators.permission import (
+    Permission,
+    validate_permission_grant_event,
+)
+
+
+# ============================================================================
+# ============================== Import vote =================================
+# ============================================================================
+from scripts.vote_2025_12_19 import start_vote, get_vote_items
+
+
+# ============================================================================
+# ============================== Constants ===================================
+# ============================================================================
+
+VOTING = "0x2e59A20f205bB85a89C53f1936454680651E618e"
+TOKEN_MANAGER = "0xf73a1260d222f447210581DDf212D915c09a3249"
+ACL = "0x9895f0f17cc1d1891b6f18ee0b483b6f221b37bb"
+VESTING_MANAGER = "0xb9d7934878b5fb9610b3fe8a5e441e8fad7e293f" # TODO replace with actual address
+
+BURN_ROLE = "BURN_ROLE"
+MINT_ROLE = "MINT_ROLE"
+ASSIGN_ROLE = "ASSIGN_ROLE"
+
+EXPECTED_VOTE_ID = 196
+EXPECTED_VOTE_EVENTS_COUNT = 3
+IPFS_DESCRIPTION_HASH = "bafkreic2vwidwtcp3vx2zcz2ye2vadw245jlo26x3fhhqmcu2o34y55mgm"
+
+
+def test_vote(helpers, accounts, ldo_holder, vote_ids_from_env):
+
+    # =======================================================================
+    # ========================= Arrange variables ===========================
+    # =======================================================================
+    voting = interface.Voting(VOTING)
+    acl = interface.ACL(ACL)
+
+
+    # =========================================================================
+    # ======================== Identify or Create vote ========================
+    # =========================================================================
+    if vote_ids_from_env:
+        vote_id = vote_ids_from_env[0]
+        if EXPECTED_VOTE_ID is not None:
+            assert vote_id == EXPECTED_VOTE_ID
+    elif EXPECTED_VOTE_ID is not None and voting.votesLength() > EXPECTED_VOTE_ID:
+        vote_id = EXPECTED_VOTE_ID
+    else:
+        vote_id, _ = start_vote({"from": ldo_holder}, silent=True)
+
+    _, call_script_items = get_vote_items()
+    onchain_script = voting.getVote(vote_id)["script"]
+    assert onchain_script == encode_call_script(call_script_items)
+
+
+    # =========================================================================
+    # ============================= Execute Vote ==============================
+    # =========================================================================
+    is_executed = voting.getVote(vote_id)["executed"]
+    if not is_executed:
+        # =======================================================================
+        # ========================= Before voting checks ========================
+        # =======================================================================
+        assert not acl.hasPermission(VESTING_MANAGER, TOKEN_MANAGER, web3.keccak(text=BURN_ROLE).hex())
+        assert not acl.hasPermission(VESTING_MANAGER, TOKEN_MANAGER, web3.keccak(text=MINT_ROLE).hex())
+        assert not acl.hasPermission(VESTING_MANAGER, TOKEN_MANAGER, web3.keccak(text=ASSIGN_ROLE).hex())
+
+        # TODO more before tests
+
+
+        assert get_lido_vote_cid_from_str(find_metadata_by_vote_id(vote_id)) == IPFS_DESCRIPTION_HASH
+
+        vote_tx: TransactionReceipt = helpers.execute_vote(vote_id=vote_id, accounts=accounts, dao_voting=voting)
+        display_voting_events(vote_tx)
+        vote_events = group_voting_events_from_receipt(vote_tx)
+
+
+        # =======================================================================
+        # ========================= After voting checks =========================
+        # =======================================================================
+        assert acl.hasPermission(VESTING_MANAGER, TOKEN_MANAGER, web3.keccak(text=BURN_ROLE).hex())
+        assert acl.hasPermission(VESTING_MANAGER, TOKEN_MANAGER, web3.keccak(text=MINT_ROLE).hex())
+        assert acl.hasPermission(VESTING_MANAGER, TOKEN_MANAGER, web3.keccak(text=ASSIGN_ROLE).hex())
+
+        # TODO more after tests
+
+
+        assert len(vote_events) == EXPECTED_VOTE_EVENTS_COUNT
+        assert count_vote_items_by_events(vote_tx, voting.address) == EXPECTED_VOTE_EVENTS_COUNT
+
+        validate_permission_grant_event(
+            event=vote_events[0],
+            p=Permission(
+                app=TOKEN_MANAGER,
+                entity=VESTING_MANAGER,
+                role=web3.keccak(text=BURN_ROLE).hex(),
+            ),
+            emitted_by=ACL,
+        )
+        validate_permission_grant_event(
+            event=vote_events[1],
+            p=Permission(
+                app=TOKEN_MANAGER,
+                entity=VESTING_MANAGER,
+                role=web3.keccak(text=MINT_ROLE).hex(),
+            ),
+            emitted_by=ACL,
+        )
+        validate_permission_grant_event(
+            event=vote_events[2],
+            p=Permission(
+                app=TOKEN_MANAGER,
+                entity=VESTING_MANAGER,
+                role=web3.keccak(text=ASSIGN_ROLE).hex(),
+            ),
+            emitted_by=ACL,
+        )
