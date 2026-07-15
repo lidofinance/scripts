e8e5e1339b7c5122b458364ba9f1163abea5f834
Nikita P <iamnp@yandex.ru>
2026-07-15 07:04:15 +0000

    update wording

--- tests/test_upgrade_2026_07_15_srv3_cmv2.py
+++ tests/test_upgrade_2026_07_15_srv3_cmv2.py
@@ -356,7 +356,7 @@ EXPECTED_DG_PROPOSAL_ID = 12
 EXPECTED_VOTE_EVENTS_COUNT = 12  # 1 DG submission + 11 Easy Track items
 EXPECTED_DG_EVENTS_FROM_AGENT = 69
 EXPECTED_DG_EVENTS_COUNT = 69
-IPFS_DESCRIPTION_HASH = "bafkreidp36yft3tllk4rqnx2zp6h2mxbuowzpqfsxgxaxjhk6tgu3acz5u"
+IPFS_DESCRIPTION_HASH = "bafkreihbfbtivfk4cebscu6xz4fhhe3bkiugjaaatrfqtqncvr2thb4npu"
 
 
 class StakingModuleItem(NamedTuple):
